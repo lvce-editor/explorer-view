aa0933061339ebc9d8d85bfdb502c61d921b47fb
Le Vivilet <levivlet@pm.me>
2024-11-27 22:03:05 +0100

    tsc

--- packages/explorer-view/src/parts/GetFileIconVirtualDom/GetFileIconVirtualDom.ts
+++ packages/explorer-view/src/parts/GetFileIconVirtualDom/GetFileIconVirtualDom.ts
@@ -2,7 +2,7 @@ import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getFileIconVirtualDom = (icon) => {
+export const getFileIconVirtualDom = (icon: string): any => {
   return {
     type: VirtualDomElements.Img,
     className: ClassNames.FileIcon,
