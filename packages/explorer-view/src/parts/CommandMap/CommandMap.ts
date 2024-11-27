ff87141963b950fcae1a44259ce31341c8176c69
Le Vivilet <levivlet@pm.me>
2024-11-27 22:31:04 +0100

    tsc

--- packages/explorer-view/src/parts/CommandMap/CommandMap.ts
+++ packages/explorer-view/src/parts/CommandMap/CommandMap.ts
@@ -1,12 +1,15 @@
+import * as ExpandAll from '../ExpandAll/ExpandAll.ts'
 import * as GetExplorerVirtualDom from '../GetExplorerVirtualDom/GetExplorerVirtualDom.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as GetVisibleExplorerItems from '../GetVisibleExplorerItems/GetVisibleExplorerItems.ts'
 import * as HandleClick from '../HandleClick/HandleClick.ts'
+import * as LoadContent from '../LoadContent/LoadContent.ts'
 
 export const commandMap = {
   'Explorer.cancelEdit': HandleClick.cancelEdit,
   'Explorer.copyPath': HandleClick.copyPath,
   'Explorer.copyRelativePath': HandleClick.copyRelativePath,
+  'Explorer.expandAll': ExpandAll.expandAll,
   'Explorer.getKeyBindings': GetKeyBindings.getKeyBindings,
   'Explorer.getVirtualDom': GetExplorerVirtualDom.getExplorerVirtualDom,
   'Explorer.getVisibleItems': GetVisibleExplorerItems.getVisibleExplorerItems,
@@ -19,6 +22,7 @@ export const commandMap = {
   'Explorer.handleClickOpenFolder': HandleClick.handleClickOpenFolder,
   'Explorer.handleIconThemeChange': HandleClick.handleIconThemeChange,
   'Explorer.handleWheel': HandleClick.handleWheel,
+  'Explorer.loadContent': LoadContent.loadContent,
   'Explorer.removeDirent': HandleClick.removeDirent,
   'Explorer.renameDirent': HandleClick.renameDirent,
 }
