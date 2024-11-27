1ec81e5650b4e25a012b0e6277e3cc458fd0d37a
Le Vivilet <levivlet@pm.me>
2024-11-27 22:36:27 +0100

    tsc

--- packages/explorer-view/src/parts/HandleClick/HandleClick.ts
+++ packages/explorer-view/src/parts/HandleClick/HandleClick.ts
@@ -1,15 +1,12 @@
-import * as Assert from '../Assert/Assert.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import { focusIndex } from '../FocusIndex/FocusIndex.ts'
 import { getChildDirents, getIndexFromPosition, getParentEndIndex, getParentStartIndex } from '../GetChildDirents/GetChildDirents.ts'
 import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
-import * as GetFileExtension from '../GetFileExtension/GetFileExtension.ts'
 import * as IconTheme from '../IconTheme/IconTheme.ts'
 import * as MouseEventType from '../MouseEventType/MouseEventType.ts'
 import * as OpenFolder from '../OpenFolder/OpenFolder.ts'
-import * as SortExplorerItems from '../SortExplorerItems/SortExplorerItems.ts'
 // TODO viewlet should only have create and refresh functions
 // every thing else can be in a separate module <viewlet>.lazy.js
 // and  <viewlet>.ipc.js
