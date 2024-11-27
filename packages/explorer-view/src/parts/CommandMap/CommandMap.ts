2e5e8371c7caf3e315aa720b148f2e5b69a71765
Le Vivilet <levivlet@pm.me>
2024-11-27 20:17:42 +0100

    feature: add getVisibleExplorerItems function

--- packages/explorer-view/src/parts/CommandMap/CommandMap.ts
+++ packages/explorer-view/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
+import * as GetVisibleExplorerItems from '../GetVisibleExplorerItems/GetVisibleExplorerItems.ts'
 
 export const commandMap = {
   'Explorer.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'Explorer.getVisibleItems': GetVisibleExplorerItems.getVisibleExplorerItems,
 }
