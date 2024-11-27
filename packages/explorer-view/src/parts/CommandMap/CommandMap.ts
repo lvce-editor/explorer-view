0997564d1c8b1f198023489b566381f3044aaa9b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-27 20:04:45 +0100

    feature: register keybindings command (#3)
    * feature: register keybindings command
    
    * coverage
    
    * tsc

--- packages/explorer-view/src/parts/CommandMap/CommandMap.ts
+++ packages/explorer-view/src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,5 @@
-export const commandMap = {}
+import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
+
+export const commandMap = {
+  'Explorer.getKeyBindings': GetKeyBindings.getKeyBindings,
+}
