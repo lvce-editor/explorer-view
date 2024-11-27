aabbe0902c80a32635ff44e2a70b102398e67c12
Le Vivilet <levivlet@pm.me>
2024-11-27 20:03:31 +0100

    lint

--- packages/explorer-view/src/parts/ExplorerStrings/ExplorerStrings.ts
+++ packages/explorer-view/src/parts/ExplorerStrings/ExplorerStrings.ts
@@ -24,74 +24,74 @@ const UiStrings = {
   NoFolderOpen: 'No Folder Open',
 }
 
-export const newFile = () => {
+export const newFile = (): string => {
   return I18nString.i18nString(UiStrings.NewFile)
 }
 
-export const newFolder = () => {
+export const newFolder = (): string => {
   return I18nString.i18nString(UiStrings.NewFolder)
 }
 
-export const openContainingFolder = () => {
+export const openContainingFolder = (): string => {
   return I18nString.i18nString(UiStrings.OpenContainingFolder)
 }
 
-export const openInIntegratedTerminal = () => {
+export const openInIntegratedTerminal = (): string => {
   return I18nString.i18nString(UiStrings.OpenInIntegratedTerminal)
 }
 
-export const cut = () => {
+export const cut = (): string => {
   return I18nString.i18nString(UiStrings.Cut)
 }
 
-export const copy = () => {
+export const copy = (): string => {
   return I18nString.i18nString(UiStrings.Copy)
 }
 
-export const paste = () => {
+export const paste = (): string => {
   return I18nString.i18nString(UiStrings.Paste)
 }
 
-export const copyPath = () => {
+export const copyPath = (): string => {
   return I18nString.i18nString(UiStrings.CopyPath)
 }
 
-export const copyRelativePath = () => {
+export const copyRelativePath = (): string => {
   return I18nString.i18nString(UiStrings.CopyRelativePath)
 }
 
-export const rename = () => {
+export const rename = (): string => {
   return I18nString.i18nString(UiStrings.Rename)
 }
 
-export const deleteItem = () => {
+export const deleteItem = (): string => {
   return I18nString.i18nString(UiStrings.Delete)
 }
 
-export const refresh = () => {
+export const refresh = (): string => {
   return I18nString.i18nString(UiStrings.RefreshExplorer)
 }
 
-export const collapseAll = () => {
+export const collapseAll = (): string => {
   return I18nString.i18nString(UiStrings.CollapseAllFoldersInExplorer)
 }
 
-export const explorer = () => {
+export const explorer = (): string => {
   return I18nString.i18nString(UiStrings.Explorer)
 }
 
-export const filesExplorer = () => {
+export const filesExplorer = (): string => {
   return I18nString.i18nString(UiStrings.FilesExplorer)
 }
 
-export const youHaveNotYetOpenedAFolder = () => {
+export const youHaveNotYetOpenedAFolder = (): string => {
   return I18nString.i18nString(UiStrings.YouHaveNotYetOpenedAFolder)
 }
 
-export const openFolder = () => {
+export const openFolder = (): string => {
   return I18nString.i18nString(UiStrings.OpenFolder)
 }
 
-export const noFolderOpen = () => {
+export const noFolderOpen = (): string => {
   return I18nString.i18nString(UiStrings.NoFolderOpen)
 }
