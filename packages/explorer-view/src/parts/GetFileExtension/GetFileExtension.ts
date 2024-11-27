bbe8b6fdbe474eb7dd5b2cde54abfb64e9be04d3
Le Vivilet <levivlet@pm.me>
2024-11-27 22:35:52 +0100

    tsc

--- packages/explorer-view/src/parts/GetFileExtension/GetFileExtension.ts
+++ packages/explorer-view/src/parts/GetFileExtension/GetFileExtension.ts
@@ -1,12 +1,12 @@
 import * as Character from '../Character/Character.ts'
 import * as Assert from '../Assert/Assert.ts'
 
-const getFileExtensionIndex = (file) => {
+const getFileExtensionIndex = (file: any): any => {
   Assert.string(file)
   return file.lastIndexOf(Character.Dot)
 }
 
-export const getFileExtension = (file) => {
+export const getFileExtension = (file: any): any => {
   const index = getFileExtensionIndex(file)
   return file.slice(index)
 }
