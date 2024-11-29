15bb5a466e0db74471f07786280506a0c87919e2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-29 18:01:14 +0100

    feature: move compareDirent function into separate file (#18)

---
+++ packages/explorer-view/src/parts/CompareDirent/CompareDirent.ts
@@ -0,0 +1,23 @@
+import * as Compare from '../Compare/Compare.ts'
+import * as DirentType from '../DirentType/DirentType.ts'
+
+const priorityMapFoldersFirst: any = {
+  [DirentType.Directory]: 1,
+  [DirentType.SymLinkFolder]: 1,
+  [DirentType.File]: 0,
+  [DirentType.SymLinkFile]: 0,
+  [DirentType.Unknown]: 0,
+  [DirentType.Socket]: 0,
+}
+
+const compareDirentType = (direntA: any, direntB: any): any => {
+  return priorityMapFoldersFirst[direntB.type] - priorityMapFoldersFirst[direntA.type]
+}
+
+const compareDirentName = (direntA: any, direntB: any): number => {
+  return Compare.compareStringNumeric(direntA.name, direntB.name)
+}
+
+export const compareDirent = (direntA: any, direntB: any): number => {
+  return compareDirentType(direntA, direntB) || compareDirentName(direntA, direntB)
+}
