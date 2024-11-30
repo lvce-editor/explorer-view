7bc58381e9a227e661e74fc2741b3f2f37909fbd
Le Vivilet <levivlet@pm.me>
2024-11-30 09:00:06 +0100

    feature: add getTopLevelDirents function

---
+++ packages/explorer-view/src/parts/GetTopLevelDirents/GetTopLevelDirents.ts
@@ -0,0 +1,17 @@
+import * as DirentType from '../DirentType/DirentType.js'
+import { getChildDirents } from '../GetChildDirents/GetChildDirents.ts'
+
+export const getTopLevelDirents = (root: string, pathSeparator: string, excluded: any[]): any => {
+  if (!root) {
+    return []
+  }
+  return getChildDirents(
+    pathSeparator,
+    {
+      depth: 0,
+      path: root,
+      type: DirentType.Directory,
+    },
+    excluded,
+  )
+}
