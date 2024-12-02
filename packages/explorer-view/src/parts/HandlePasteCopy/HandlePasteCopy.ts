2b05d3ef8f579477b2518f6d37ab5a3338d6ec84
Le Vivilet <levivlet@pm.me>
2024-12-02 22:30:41 +0100

    tsc

--- packages/explorer-view/src/parts/HandlePasteCopy/HandlePasteCopy.ts
+++ packages/explorer-view/src/parts/HandlePasteCopy/HandlePasteCopy.ts
@@ -2,7 +2,6 @@ import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as Path from '../Path/Path.ts'
 import { getBaseName } from '../Path/Path.ts'
 import { updateRoot } from '../UpdateRoot/UpdateRoot.ts'
-import * as Viewlet from '../Viewlet/Viewlet.ts' // TODO should not import viewlet manager -> avoid cyclic dependency
 
 export const handlePasteCopy = async (state: any, nativeFiles: any): Promise<any> => {
   // TODO handle pasting files into nested folder
@@ -13,10 +12,6 @@ export const handlePasteCopy = async (state: any, nativeFiles: any): Promise<any
   for (const source of nativeFiles.files) {
     const target = Path.join(state.pathSeperator, state.root, getBaseName(state.pathSeparator, source))
     await FileSystem.copy(source, target)
-  }
-  const stateNow = Viewlet.getState('Explorer')
-  if (stateNow.disposed) {
-    return
   }
   // TODO only update folder at which level it changed
   return updateRoot(state)
