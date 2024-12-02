5e68426e4f9ef879899d075121ba5aaac60325c0
Le Vivilet <levivlet@pm.me>
2024-12-02 23:23:23 +0100

    feature: use namespace imports

--- packages/explorer-view/src/parts/HandleDrop/HandleDrop.ts
+++ packages/explorer-view/src/parts/HandleDrop/HandleDrop.ts
@@ -1,16 +1,16 @@
-import { getIndexFromPosition } from '../GetIndexFromPosition/GetIndexFromPosition.ts'
-import { handleDropIndex } from '../HandleDropIndex/HandleDropIndex.ts'
-import { handleDropRoot } from '../HandleDropRoot/HandleDropRoot.ts'
+import * as GetIndexFromPosition from '../GetIndexFromPosition/GetIndexFromPosition.ts'
+import * as HandleDropIndex from '../HandleDropIndex/HandleDropIndex.ts'
+import * as HandleDropRoot from '../HandleDropRoot/HandleDropRoot.ts'
 import { VError } from '../VError/VError.ts'
 
 export const handleDrop = async (state: any, x: number, y: number, files: any): Promise<any> => {
   try {
-    const index = getIndexFromPosition(state, x, y)
+    const index = GetIndexFromPosition.getIndexFromPosition(state, x, y)
     switch (index) {
       case -1:
-        return await handleDropRoot(state, files)
+        return await HandleDropRoot.handleDropRoot(state, files)
       default:
-        return await handleDropIndex(state, index, files)
+        return await HandleDropIndex.handleDropIndex(state, index, files)
     }
   } catch (error) {
     throw new VError(error, 'Failed to drop files')
