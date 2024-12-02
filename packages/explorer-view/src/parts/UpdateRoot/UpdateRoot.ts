38dc2e46ddd94ad18cb05f065ba0d4eb84a89fbe
Le Vivilet <levivlet@pm.me>
2024-12-02 22:32:43 +0100

    tsc

--- packages/explorer-view/src/parts/UpdateRoot/UpdateRoot.ts
+++ packages/explorer-view/src/parts/UpdateRoot/UpdateRoot.ts
@@ -1,5 +1,5 @@
-import * as Viewlet from '../Viewlet/Viewlet.ts'
 import { getTopLevelDirents } from '../GetTopLevelDirents/GetTopLevelDirents.ts'
+import { mergeDirents } from '../MergeDirents/MergeDirents.ts'
 
 // TODO add lots of tests for this
 export const updateRoot = async (state1: any): Promise<any> => {
@@ -9,14 +9,14 @@ export const updateRoot = async (state1: any): Promise<any> => {
   // const file = nativeFiles.files[0]
   // @ts-ignore
   const topLevelDirents = await getTopLevelDirents(state1.root, state1.pathSeparator)
-  const state2 = Viewlet.getState('Explorer')
-  // TODO what if root changes while reading directories?
-  if (state2.disposed || state2.root !== state1.root) {
-    return state2
-  }
-  const newDirents = mergeDirents(state2.items, topLevelDirents)
+  // const state2 = Viewlet.getState('Explorer')
+  // // TODO what if root changes while reading directories?
+  // if (state2.disposed || state2.root !== state1.root) {
+  //   return state2
+  // }
+  const newDirents = mergeDirents(state1.items, topLevelDirents)
   const state3 = {
-    ...state2,
+    ...state1,
     items: newDirents,
   }
   return state3
