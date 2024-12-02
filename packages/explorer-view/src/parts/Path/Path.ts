01a863855400ed3f45291b2a0259d3f97cdde3bc
Le Vivilet <levivlet@pm.me>
2024-12-02 22:31:13 +0100

    tsc

--- packages/explorer-view/src/parts/Path/Path.ts
+++ packages/explorer-view/src/parts/Path/Path.ts
@@ -5,3 +5,7 @@ export const dirname = (pathSeparator: string, path: string): string => {
   }
   return path.slice(0, index)
 }
+
+export const getBaseName = (pathSeparator: string, path: string): string => {
+  return path.slice(path.lastIndexOf(pathSeparator) + 1)
+}
