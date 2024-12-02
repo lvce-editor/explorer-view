d756d083ebc89572ad71052854027780342cf623
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-02 23:07:44 +0100

    feature: move handleBlur function into separate file (#39)
    * feature: move handleBlur function into separate file
    
    * import

--- packages/explorer-view/src/parts/HandleClick/HandleClick.ts
+++ packages/explorer-view/src/parts/HandleClick/HandleClick.ts
@@ -358,19 +358,6 @@ export const handleArrowLeft = (state: any): any => {
 // TODO what happens when mouse leave and anther mouse enter event occur?
 // should update preview instead of closing and reopening
 
-export const handleBlur = (state: any): any => {
-  // TODO when blur event occurs because of context menu, focused index should stay the same
-  // but focus outline should be removed
-  const { editingType } = state
-  if (editingType !== ExplorerEditingType.None) {
-    return state
-  }
-  return {
-    ...state,
-    focused: false,
-  }
-}
-
 // TODO maybe just insert items into explorer and refresh whole explorer
 
 export const handleClickOpenFolder = async (state: any): Promise<any> => {
