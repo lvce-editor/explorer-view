7ec01711b3c29c901765f6247328a20fc162034c
Le Vivilet <levivlet@pm.me>
2024-12-15 11:50:07 +0100

    feature: implement file system

--- packages/explorer-view/src/parts/FileSystem/FileSystem.ts
+++ packages/explorer-view/src/parts/FileSystem/FileSystem.ts
@@ -1,17 +1,37 @@
-export const remove = async (diren: any): Promise<void> => {}
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
-export const readDirWithFileTypes = async (uri: string): Promise<any> => {}
+export const remove = async (dirent: any): Promise<void> => {
+  return ParentRpc.invoke('FileSystem.remove', dirent)
+}
 
-export const getPathSeparator = async (root: string): Promise<any> => {}
+export const readDirWithFileTypes = async (uri: string): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.readDirWithFileTypes', uri)
+}
 
-export const getRealPath = async (path: any): Promise<any> => {}
+export const getPathSeparator = async (root: string): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.getPathSeparator', root)
+}
 
-export const stat = async (dirent: any): Promise<any> => {}
+export const getRealPath = async (path: any): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.getRealPath', path)
+}
 
-export const createFile = async (uri: string): Promise<any> => {}
+export const stat = async (dirent: any): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.stat', dirent)
+}
 
-export const mkdir = async (uri: string): Promise<any> => {}
+export const createFile = async (uri: string): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.createFile', uri)
+}
 
-export const rename = async (oldUri: string, newUri: string): Promise<any> => {}
+export const mkdir = async (uri: string): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.mkdir', uri)
+}
 
-export const copy = async (oldUri: string, newUri: string): Promise<any> => {}
+export const rename = async (oldUri: string, newUri: string): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.rename', oldUri, newUri)
+}
+
+export const copy = async (oldUri: string, newUri: string): Promise<any> => {
+  return ParentRpc.invoke('FileSystem.copy', oldUri, newUri)
+}
