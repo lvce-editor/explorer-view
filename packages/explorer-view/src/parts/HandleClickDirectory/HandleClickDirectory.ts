3e764fc02d2caf87398b9d9b0f9dca460aa0ab94
Le Vivilet <levivlet@pm.me>
2024-12-16 00:10:59 +0100

    feature: update file icons when expanding folder

--- packages/explorer-view/src/parts/HandleClickDirectory/HandleClickDirectory.ts
+++ packages/explorer-view/src/parts/HandleClickDirectory/HandleClickDirectory.ts
@@ -2,6 +2,7 @@ import * as DirentType from '../DirentType/DirentType.ts'
 import * as GetChildDirents from '../GetChildDirents/GetChildDirents.ts'
 import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
 import * as IconTheme from '../IconTheme/IconTheme.ts'
+import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 
 export const handleClickDirectory = async (state: any, dirent: any, index: any, keepFocus: boolean): Promise<any> => {
   dirent.type = DirentType.DirectoryExpanding
@@ -24,9 +25,13 @@ export const handleClickDirectory = async (state: any, dirent: any, index: any, 
   const { height, itemHeight, minLineY } = state2
   // TODO when focused index has changed while expanding, don't update it
   const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
+
+  const parts = newDirents.slice(minLineY, maxLineY)
+  const icons = await GetFileIcons.getFileIcons(parts)
   return {
     ...state,
     items: newDirents,
+    icons,
     focusedIndex: newIndex,
     focused: keepFocus,
     maxLineY,
