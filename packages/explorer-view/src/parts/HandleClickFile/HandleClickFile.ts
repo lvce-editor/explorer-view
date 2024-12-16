fb97612bb273810377e1df1a823db8e76fccbf23
Le Vivilet <levivlet@pm.me>
2024-12-16 21:35:42 +0100

    fix: open file on click

--- packages/explorer-view/src/parts/HandleClickFile/HandleClickFile.ts
+++ packages/explorer-view/src/parts/HandleClickFile/HandleClickFile.ts
@@ -1,5 +1,7 @@
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+
 export const handleClickFile = async (state: any, dirent: any, index: any, keepFocus = false): Promise<any> => {
-  // await Command.execute(/* Main.openAbsolutePath */ 'Main.openUri', /* absolutePath */ dirent.path, /* focus */ !keepFocus)
+  await ParentRpc.invoke(/* Main.openAbsolutePath */ 'Main.openUri', /* absolutePath */ dirent.path, /* focus */ !keepFocus)
   return {
     ...state,
     focusedIndex: index,
