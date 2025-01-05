659d9450f1e48e21c28e734b50d855ac60c2275d
Le Vivilet <levivlet@pm.me>
2025-01-05 22:52:23 +0100

    feature: add isTopLevel test

---
+++ packages/explorer-view/test/IsTopLevel.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@jest/globals'
+import * as IsTopLevel from '../src/parts/IsTopLevel/IsTopLevel.ts'
+
+test('isTopLevel - depth 0', () => {
+  const dirent = {
+    depth: 0,
+  }
+  expect(IsTopLevel.isTopLevel(dirent)).toBe(true)
+})
+
+test('isTopLevel - depth 1', () => {
+  const dirent = {
+    depth: 1,
+  }
+  expect(IsTopLevel.isTopLevel(dirent)).toBe(false)
+})
+
+test('isTopLevel - depth 2', () => {
+  const dirent = {
+    depth: 2,
+  }
+  expect(IsTopLevel.isTopLevel(dirent)).toBe(false)
+})
