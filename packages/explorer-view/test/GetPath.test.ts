f176bee9808fd8821b9bd78e1e5379b0712d77d4
Le Vivilet <levivlet@pm.me>
2025-01-05 22:43:01 +0100

    feature: add getPath test

---
+++ packages/explorer-view/test/GetPath.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@jest/globals'
+import * as GetPath from '../src/parts/GetPath/GetPath.ts'
+
+test('getPath - file dirent', () => {
+  const dirent = {
+    name: 'test.txt',
+    path: '/test/test.txt',
+    type: 1,
+  }
+  expect(GetPath.getPath(dirent)).toBe('/test/test.txt')
+})
+
+test('getPath - directory dirent', () => {
+  const dirent = {
+    name: 'folder',
+    path: '/test/folder',
+    type: 2,
+  }
+  expect(GetPath.getPath(dirent)).toBe('/test/folder')
+})
+
+test('getPath - nested path', () => {
+  const dirent = {
+    name: 'file.js',
+    path: '/test/folder/subfolder/file.js',
+    type: 1,
+  }
+  expect(GetPath.getPath(dirent)).toBe('/test/folder/subfolder/file.js')
+})
