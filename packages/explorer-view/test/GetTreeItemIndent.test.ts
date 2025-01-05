96eb34ef4d926133527ad2353fec2fcc2fa1ffdc
Le Vivilet <levivlet@pm.me>
2025-01-05 23:00:05 +0100

    test

--- packages/explorer-view/test/GetTreeItemIndent.test.ts
+++ packages/explorer-view/test/GetTreeItemIndent.test.ts
@@ -2,17 +2,17 @@ import { expect, test } from '@jest/globals'
 import * as GetTreeItemIndent from '../src/parts/GetTreeItemIndent/GetTreeItemIndent.ts'
 
 test('getTreeItemIndent - depth 0', () => {
-  expect(GetTreeItemIndent.getTreeItemIndent(0)).toBe(0)
+  expect(GetTreeItemIndent.getTreeItemIndent(0)).toBe('0rem')
 })
 
 test('getTreeItemIndent - depth 1', () => {
-  expect(GetTreeItemIndent.getTreeItemIndent(1)).toBe(16)
+  expect(GetTreeItemIndent.getTreeItemIndent(1)).toBe('1rem')
 })
 
 test('getTreeItemIndent - depth 2', () => {
-  expect(GetTreeItemIndent.getTreeItemIndent(2)).toBe(32)
+  expect(GetTreeItemIndent.getTreeItemIndent(2)).toBe('2rem')
 })
 
 test('getTreeItemIndent - depth 3', () => {
-  expect(GetTreeItemIndent.getTreeItemIndent(3)).toBe(48)
+  expect(GetTreeItemIndent.getTreeItemIndent(3)).toBe('3rem')
 })
