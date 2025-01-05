206d83227be15dd8dd5eff09e3fcfbf40dad2246
Le Vivilet <levivlet@pm.me>
2025-01-05 22:56:30 +0100

    feature: add file icon test

---
+++ packages/explorer-view/test/GetFileIconVirtualDom.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@jest/globals'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import * as GetFileIconVirtualDom from '../src/parts/GetFileIconVirtualDom/GetFileIconVirtualDom.ts'
+
+test('getFileIconVirtualDom - with icon path', () => {
+  expect(GetFileIconVirtualDom.getFileIconVirtualDom('/icons/file.svg')).toEqual({
+    type: VirtualDomElements.Img,
+    className: ClassNames.FileIcon,
+    src: '/icons/file.svg',
+    role: AriaRoles.None,
+    childCount: 0,
+  })
+})
+
+test('getFileIconVirtualDom - empty icon path', () => {
+  expect(GetFileIconVirtualDom.getFileIconVirtualDom('')).toEqual({
+    type: VirtualDomElements.Img,
+    className: ClassNames.FileIcon,
+    src: '',
+    role: AriaRoles.None,
+    childCount: 0,
+  })
+})
