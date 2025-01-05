98bf07dcbc994bee1ca2b346ed750170db762b19
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-05 23:03:34 +0100

    feature: add icon test (#121)
    * feature: add icon test
    
    * lint

---
+++ packages/explorer-view/test/GetIconVirtualDom.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@jest/globals'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
+import * as GetIconVirtualDom from '../src/parts/GetIconVirtualDom/GetIconVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getIconVirtualDom - with icon', () => {
+  expect(GetIconVirtualDom.getIconVirtualDom('File')).toEqual({
+    type: VirtualDomElements.Div,
+    className: 'MaskIcon MaskIconFile',
+    role: AriaRoles.None,
+    childCount: 0,
+  })
+})
+
+test('getIconVirtualDom - with custom element type', () => {
+  expect(GetIconVirtualDom.getIconVirtualDom('Folder', VirtualDomElements.Div)).toEqual({
+    type: VirtualDomElements.Div,
+    className: 'MaskIcon MaskIconFolder',
+    role: AriaRoles.None,
+    childCount: 0,
+  })
+})
