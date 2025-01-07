c31ea08539a3c6898c3e397aa50d35a3246dc9d5
Le Vivilet <levivlet@pm.me>
2025-01-07 22:33:44 +0100

    fix

--- packages/explorer-view/src/parts/HandleClickDirectoryExpanding/HandleClickDirectoryExpanding.ts
+++ packages/explorer-view/src/parts/HandleClickDirectoryExpanding/HandleClickDirectoryExpanding.ts
@@ -2,7 +2,7 @@ import type { ExplorerState } from '../EXplorerState/ExplorerState.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as IconTheme from '../IconTheme/IconTheme.ts'
 
-export const handleClickDirectoryExpanding = (state: ExplorerState, dirent: any, index: number, keepFocus: boolean): ExplorerState => {
+export const handleClickDirectoryExpanding = async (state: ExplorerState, dirent: any, index: number, keepFocus: boolean): Promise<ExplorerState> => {
   dirent.type = DirentType.Directory
   dirent.icon = IconTheme.getIcon(dirent)
   return {
