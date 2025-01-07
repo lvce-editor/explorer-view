aebd483fe2afd89bf6caf77c177eef06111ca513
Le Vivilet <levivlet@pm.me>
2025-01-07 19:18:03 +0100

    feature: add more types

--- packages/explorer-view/src/parts/GetParentStartIndex/GetParentStartIndex.ts
+++ packages/explorer-view/src/parts/GetParentStartIndex/GetParentStartIndex.ts
@@ -1,4 +1,4 @@
-export const getParentStartIndex = (dirents: any, index: any): any => {
+export const getParentStartIndex = (dirents: any, index: number): number => {
   const dirent = dirents[index]
   let startIndex = index - 1
   while (startIndex >= 0 && dirents[startIndex].depth >= dirent.depth) {
