27edeb0df80312d1634ac43d79d36851ab733f0e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-07 18:55:56 +0100

    feature: add more types (#130)

--- packages/explorer-view/src/parts/VirtualDomHelpers/VirtualDomHelpers.ts
+++ packages/explorer-view/src/parts/VirtualDomHelpers/VirtualDomHelpers.ts
@@ -1,6 +1,7 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const text = (data: string): any => {
+export const text = (data: string): VirtualDomNode => {
   return {
     type: VirtualDomElements.Text,
     text: data,
