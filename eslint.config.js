7a6797ec9e36b4821bbe637e10d1887bf2a587d0
Le Vivilet <levivlet@pm.me>
2025-01-07 19:44:41 +0100

    feature: add rpc registry

--- eslint.config.js
+++ eslint.config.js
@@ -50,6 +50,7 @@ export default tseslint.config(
       '@typescript-eslint/restrict-plus-operands': 'off',
       '@typescript-eslint/no-unnecessary-condition': 'off',
       '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-dynamic-delete': 'off',
     },
   },
   {
