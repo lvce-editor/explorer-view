1d6abde287a3c295921663107b9e47c01341da0e
Le Vivilet <levivlet@pm.me>
2025-01-07 19:09:55 +0100

    feature: remove chevron extraClassName parameter

--- packages/explorer-view/src/parts/GetChevronVirtualDom/GetChevronVirtualDom.ts
+++ packages/explorer-view/src/parts/GetChevronVirtualDom/GetChevronVirtualDom.ts
@@ -2,18 +2,18 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getChevronDownVirtualDom = (extraClassName = ''): VirtualDomNode => {
+export const getChevronDownVirtualDom = (): VirtualDomNode => {
   return {
     type: VirtualDomElements.Div,
-    className: `${ClassNames.Chevron} MaskIconChevronDown ${extraClassName}`,
+    className: `${ClassNames.Chevron} MaskIconChevronDown`,
     childCount: 0,
   }
 }
 
-export const getChevronRightVirtualDom = (extraClassName = ''): VirtualDomNode => {
+export const getChevronRightVirtualDom = (): VirtualDomNode => {
   return {
     type: VirtualDomElements.Div,
-    className: `${ClassNames.Chevron} MaskIconChevronRight ${extraClassName}`,
+    className: `${ClassNames.Chevron} MaskIconChevronRight`,
     childCount: 0,
   }
 }
