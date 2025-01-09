0c449733ded6f89ba9f6f043f961757cba21b195
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-09 22:22:17 +0100

    feature: add saveState tests (#185)

--- packages/explorer-view/src/parts/SaveState/SaveState.ts
+++ packages/explorer-view/src/parts/SaveState/SaveState.ts
@@ -1,7 +1,9 @@
+import type { ExplorerState } from '../EXplorerState/ExplorerState.ts'
+import type { SavedState } from '../SavedState/SavedState.ts'
 import * as GetPath from '../GetPath/GetPath.ts'
 import * as IsExpandedDirectory from '../IsExpandedDirectory/IsExpandedDirectory.ts'
 
-export const saveState = (state: any): any => {
+export const saveState = (state: ExplorerState): SavedState => {
   const { items, root, deltaY, minLineY, maxLineY } = state
   const expandedPaths = items.filter(IsExpandedDirectory.isExpandedDirectory).map(GetPath.getPath)
   return {
