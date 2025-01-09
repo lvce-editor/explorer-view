8c95eee3a6948ad354ad0da94b3de1503a20ae88
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-09 19:01:58 +0100

    feature: merge file and folder virtual dom functions (#155)

--- packages/explorer-view/src/parts/GetExplorerItemVirtualDom/GetExplorerItemVirtualDom.ts
+++ packages/explorer-view/src/parts/GetExplorerItemVirtualDom/GetExplorerItemVirtualDom.ts
@@ -1,17 +1,7 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { VisibleExplorerItem } from '../VisibleExplorerItem/VisibleExplorerItem.ts'
-import * as DirentType from '../DirentType/DirentType.ts'
-import * as GetVirtualDomFile from '../GetVirtualDomFile/GetVirtualDomFile.ts'
 import * as GetVirtualDomFolder from '../GetVirtualDomFolder/GetVirtualDomFolder.ts'
 
 export const getExplorerItemVirtualDom = (item: VisibleExplorerItem): readonly VirtualDomNode[] => {
-  const { type } = item
-  switch (type) {
-    case DirentType.Directory:
-    case DirentType.DirectoryExpanding:
-    case DirentType.DirectoryExpanded:
-      return GetVirtualDomFolder.getItemVirtualDomFolder(item)
-    default:
-      return GetVirtualDomFile.getItemVirtualDomFile(item)
-  }
+  return GetVirtualDomFolder.getItemVirtualDomFolder(item)
 }
