efe21eb358e6a3cc4b9d38c031355f114aedd8b1
Le Vivilet <levivlet@pm.me>
2025-01-09 22:01:05 +0100

    test

--- packages/explorer-view/test/GetExpandedType.test.ts
+++ packages/explorer-view/test/GetExpandedType.test.ts
@@ -1,27 +1,28 @@
 import { expect, test } from '@jest/globals'
 import * as DirentType from '../src/parts/DirentType/DirentType.ts'
+import * as ExpandedType from '../src/parts/ExpandedType/ExpandedType.ts'
 import * as GetExpandedType from '../src/parts/GetExpandedType/GetExpandedType.ts'
 
 test('getExpandedType - directory', () => {
-  expect(GetExpandedType.getExpandedType(DirentType.Directory)).toBe(DirentType.DirectoryExpanded)
+  expect(GetExpandedType.getExpandedType(DirentType.Directory)).toBe(ExpandedType.Collapsed)
+})
+
+test('getExpandedType - directory expanding', () => {
+  expect(GetExpandedType.getExpandedType(DirentType.DirectoryExpanding)).toBe(ExpandedType.Expanded)
+})
+
+test('getExpandedType - directory expanded', () => {
+  expect(GetExpandedType.getExpandedType(DirentType.DirectoryExpanded)).toBe(ExpandedType.Expanded)
 })
 
 test('getExpandedType - file', () => {
-  expect(() => GetExpandedType.getExpandedType(DirentType.File)).toThrow('cannot expand file')
+  expect(GetExpandedType.getExpandedType(DirentType.File)).toBe(ExpandedType.None)
 })
 
 test('getExpandedType - symlink file', () => {
-  expect(() => GetExpandedType.getExpandedType(DirentType.SymLinkFile)).toThrow('cannot expand symlink file')
-})
-
-test('getExpandedType - already expanded directory', () => {
-  expect(() => GetExpandedType.getExpandedType(DirentType.DirectoryExpanded)).toThrow('directory already expanded')
-})
-
-test('getExpandedType - expanding directory', () => {
-  expect(() => GetExpandedType.getExpandedType(DirentType.DirectoryExpanding)).toThrow('directory is expanding')
+  expect(GetExpandedType.getExpandedType(DirentType.SymLinkFile)).toBe(ExpandedType.None)
 })
 
 test('getExpandedType - unknown type', () => {
-  expect(() => GetExpandedType.getExpandedType(999999)).toThrow('unsupported dirent type')
+  expect(GetExpandedType.getExpandedType(999999)).toBe(ExpandedType.None)
 })
