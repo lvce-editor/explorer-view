6a702f5fddffb4756c2dad229e6ce6a31174a6c1
Le Vivilet <levivlet@pm.me>
2025-01-09 21:24:13 +0100

    feature: add getMissingIconRequests

---
+++ packages/explorer-view/src/parts/GetMissingIconRequests/GetMissingIcoRequests.ts
@@ -0,0 +1,19 @@
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
+import type { FileIconCache } from '../FileIconCache/FileIconCache.ts'
+import type { IconRequest } from '../IconRequest/IconRequest.ts'
+
+export const getMissingIconRequests = (dirents: readonly ExplorerItem[], fileIconCache: FileIconCache): readonly IconRequest[] => {
+  const missingRequests: IconRequest[] = []
+
+  for (const dirent of dirents) {
+    const cacheKey = `${dirent.type}:${dirent.name}`
+    if (!(cacheKey in fileIconCache)) {
+      missingRequests.push({
+        type: dirent.type,
+        name: dirent.name,
+      })
+    }
+  }
+
+  return missingRequests
+}
