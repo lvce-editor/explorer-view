b43d65d3c72788147ea35997b80d753300a3f695
Le Vivilet <levivlet@pm.me>
2025-01-09 19:10:04 +0100

    simplify code

--- packages/explorer-view/src/parts/GetExplorerItemVirtualDom/GetExplorerItemVirtualDom.ts
+++ packages/explorer-view/src/parts/GetExplorerItemVirtualDom/GetExplorerItemVirtualDom.ts
@@ -1,7 +1,34 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { VisibleExplorerItem } from '../VisibleExplorerItem/VisibleExplorerItem.ts'
-import * as GetVirtualDomFolder from '../GetVirtualDomFolder/GetVirtualDomFolder.ts'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as GetChevronVirtualDom from '../GetChevronVirtualDom/GetChevronVirtualDom.ts'
+import * as GetFileIconVirtualDom from '../GetFileIconVirtualDom/GetFileIconVirtualDom.ts'
+import * as GetInputOrLabelDom from '../GetInputOrLabelDom/GetInputOrLabelDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getExplorerItemVirtualDom = (item: VisibleExplorerItem): readonly VirtualDomNode[] => {
-  return GetVirtualDomFolder.getItemVirtualDomFolder(item)
+  const { posInSet, setSize, icon, name, path, depth, indent, chevron, id, className, isEditing, ariaExpanded } = item
+  const chevronDom = GetChevronVirtualDom.getChevronVirtualDom(chevron)
+  const dom: readonly VirtualDomNode[] = [
+    {
+      type: VirtualDomElements.Div,
+      role: AriaRoles.TreeItem,
+      className,
+      draggable: true,
+      title: path,
+      ariaPosInSet: posInSet,
+      ariaSetSize: setSize,
+      ariaLevel: depth,
+      childCount: 2 + chevronDom.length,
+      paddingLeft: indent,
+      ariaLabel: name,
+      ariaExpanded,
+      ariaDescription: '',
+      id,
+    },
+    ...chevronDom,
+    GetFileIconVirtualDom.getFileIconVirtualDom(icon),
+    ...GetInputOrLabelDom.getInputOrLabelDom(isEditing, name),
+  ]
+  return dom
 }
--- packages/explorer-view/src/parts/GetVirtualDomFile/GetVirtualDomFile.ts
+++
@@ -1,6 +0,0 @@
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import type { VisibleExplorerItem } from '../VisibleExplorerItem/VisibleExplorerItem.ts'
-
-export const getItemVirtualDomFile = (item: VisibleExplorerItem): readonly VirtualDomNode[] => {
-  throw new Error('deprecated')
-}
