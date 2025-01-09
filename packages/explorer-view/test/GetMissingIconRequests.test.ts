207109b17a9b357af028b1c94919de32817fed2f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-09 21:42:23 +0100

    feature: add file icon tests (#167)

---
+++ packages/explorer-view/test/GetMissingIconRequests.test.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@jest/globals'
+import type { ExplorerItem } from '../src/parts/ExplorerItem/ExplorerItem.ts'
+import type { FileIconCache } from '../src/parts/FileIconCache/FileIconCache.ts'
+import * as DirentType from '../src/parts/DirentType/DirentType.ts'
+import * as GetMissingIconRequests from '../src/parts/GetMissingIconRequests/GetMissingIconRequests.ts'
+
+test('getMissingIconRequests - empty list', () => {
+  const dirents: readonly ExplorerItem[] = []
+  const cache: FileIconCache = {}
+  expect(GetMissingIconRequests.getMissingIconRequests(dirents, cache)).toEqual([])
+})
+
+test('getMissingIconRequests - all in cache', () => {
+  const dirents: readonly ExplorerItem[] = [{ type: DirentType.File, path: '/test/file.txt', name: 'file.txt', depth: 0 }]
+  const cache: FileIconCache = {
+    '/test/file.txt': 'icon',
+  }
+  expect(GetMissingIconRequests.getMissingIconRequests(dirents, cache)).toEqual([])
+})
+
+test('getMissingIconRequests - some missing', () => {
+  const dirents: readonly ExplorerItem[] = [
+    { type: DirentType.File, path: '/test/file1.txt', name: 'file1.txt', depth: 0 },
+    { type: DirentType.File, path: '/test/file2.txt', name: 'file2.txt', depth: 0 },
+  ]
+  const cache: FileIconCache = {
+    '/test/file1.txt': 'icon',
+  }
+  expect(GetMissingIconRequests.getMissingIconRequests(dirents, cache)).toEqual([
+    { type: DirentType.File, name: 'file2.txt', path: '/test/file2.txt' },
+  ])
+})
