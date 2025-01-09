731855a9a4b614ea9ad85a6973642e61e7a2b44b
Le Vivilet <levivlet@pm.me>
2025-01-09 21:59:41 +0100

    test

--- packages/explorer-view/test/GetExplorerWelcomeVirtualDom.test.ts
+++ packages/explorer-view/test/GetExplorerWelcomeVirtualDom.test.ts
@@ -2,7 +2,40 @@ import { expect, test } from '@jest/globals'
 import * as GetExplorerWelcomeVirtualDom from '../src/parts/GetExplorerWelcomeVirtualDom/GetExplorerWelcomeVirtualDom.ts'
 
 test('getExplorerWelcomeVirtualDom - wide', () => {
-  expect(GetExplorerWelcomeVirtualDom.getExplorerWelcomeVirtualDom(true)).toEqual([])
+  expect(GetExplorerWelcomeVirtualDom.getExplorerWelcomeVirtualDom(true)).toEqual([
+    {
+      childCount: 1,
+      className: 'Viewlet Explorer',
+      tabIndex: 0,
+      type: 4,
+    },
+    {
+      childCount: 2,
+      className: 'Welcome',
+      type: 4,
+    },
+    {
+      childCount: 1,
+      className: 'WelcomeMessage',
+      type: 50,
+    },
+    {
+      childCount: 0,
+      text: 'You have not yet opened a folder',
+      type: 12,
+    },
+    {
+      childCount: 1,
+      className: 'Button ButtonPrimary ButtonWide',
+      onClick: 'handleClickOpenFolder',
+      type: 1,
+    },
+    {
+      childCount: 0,
+      text: 'Open folder',
+      type: 12,
+    },
+  ])
 })
 
 test('getExplorerWelcomeVirtualDom - narrow', () => {
