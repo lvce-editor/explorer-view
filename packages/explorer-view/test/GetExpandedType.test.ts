fe30bff718faa5f8d6cd75189c4ff992a0f50504
Le Vivilet <levivlet@pm.me>
2025-01-09 21:58:04 +0100

    feature: add getExpandedType test

---
+++ packages/explorer-view/test/GetExpandedType.test.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@jest/globals'
+import * as DirentType from '../src/parts/DirentType/DirentType.ts'
+import * as GetExpandedType from '../src/parts/GetExpandedType/GetExpandedType.ts'
+
+test('getExpandedType - directory', () => {
+  expect(GetExpandedType.getExpandedType(DirentType.Directory)).toBe(DirentType.DirectoryExpanded)
+})
+
+test('getExpandedType - symlink folder', () => {
+  expect(GetExpandedType.getExpandedType(DirentType.SymLinkFolder)).toBe(DirentType.SymLinkFolderExpanded)
+})
+
+test('getExpandedType - file', () => {
+  expect(() => GetExpandedType.getExpandedType(DirentType.File)).toThrow('cannot expand file')
+})
+
+test('getExpandedType - symlink file', () => {
+  expect(() => GetExpandedType.getExpandedType(DirentType.SymLinkFile)).toThrow('cannot expand symlink file')
+})
+
+test('getExpandedType - already expanded directory', () => {
+  expect(() => GetExpandedType.getExpandedType(DirentType.DirectoryExpanded)).toThrow('directory already expanded')
+})
+
+test('getExpandedType - expanding directory', () => {
+  expect(() => GetExpandedType.getExpandedType(DirentType.DirectoryExpanding)).toThrow('directory is expanding')
+})
+
+test('getExpandedType - unknown type', () => {
+  expect(() => GetExpandedType.getExpandedType(999999)).toThrow('unsupported dirent type')
+})
