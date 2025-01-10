00c3dcee20526baac2a05d54861a55ddff35b5a1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-10 23:22:25 +0100

    feature: add todo (#199)

--- packages/explorer-view/src/parts/ClassNames/ClassNames.ts
+++ packages/explorer-view/src/parts/ClassNames/ClassNames.ts
@@ -17,3 +17,15 @@ export const TreeItemActive = 'TreeItemActive'
 export const Viewlet = 'Viewlet'
 export const Welcome = 'Welcome'
 export const WelcomeMessage = 'WelcomeMessage'
+
+// TODO add option to make classnames numeric
+// once at start, send all classnames to renderer process
+// then, components uses numeric classname
+// when a component uses multiple classnames, it is a new number
+// representing the concatenated strings for example
+// 0 = 'Button'
+// 1 = 'IconButton'
+// 2 = 'Button IconButton'
+// it could make dom diffing faster, since for classname,
+// only numbers are compared. it could also make rendering faster,
+// since less data is transferred to renderer process
