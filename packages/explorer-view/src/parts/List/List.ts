182952b209e43b60e4bd90ad366e19c4986ad621
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-18 17:33:44 +0100

    feature: remove unused code (#214)

--- packages/explorer-view/src/parts/List/List.ts
+++
@@ -1,9 +0,0 @@
-export interface List<T> {
-  readonly items: readonly T[]
-  readonly focusedIndex: number
-  readonly minLineY: number
-  readonly maxLineY: number
-  readonly itemHeight: number
-  readonly headerHeight: number
-  readonly height: number
-}
