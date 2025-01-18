804a6dbf6d9bb4d5997b14430b59442b2f341f18
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-18 12:50:06 +0100

    feature: wrap saveState command to keep state in explorer worker (#208)

--- packages/explorer-view/src/parts/SaveState/SaveState.ts
+++ packages/explorer-view/src/parts/SaveState/SaveState.ts
@@ -1,10 +1,19 @@
 import type { ExplorerState } from '../EXplorerState/ExplorerState.ts'
 import type { SavedState } from '../SavedState/SavedState.ts'
+import * as ExplorerStates from '../ExplorerStates/ExplorerStates.ts'
 import * as GetPath from '../GetPath/GetPath.ts'
 import * as IsExpandedDirectory from '../IsExpandedDirectory/IsExpandedDirectory.ts'
 
-export const saveState = (state: ExplorerState): SavedState => {
-  const { items, root, deltaY, minLineY, maxLineY } = state
+export const saveState = (uid: number): SavedState => {
+  let newState: ExplorerState
+  if (typeof uid === 'number') {
+    const value = ExplorerStates.get(uid)
+    newState = value.newState
+  } else {
+    // deprecated
+    newState = uid
+  }
+  const { items, root, deltaY, minLineY, maxLineY } = newState
   const expandedPaths = items.filter(IsExpandedDirectory.isExpandedDirectory).map(GetPath.getPath)
   return {
     expandedPaths,
