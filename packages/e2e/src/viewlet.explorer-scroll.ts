159e8e3ec4ef0b2136ebff5f7158d0f4df67fb1f
Le Vivilet <levivlet@pm.me>
2025-02-28 18:28:21 +0100

    feature: add scroll e2e test

---
+++ packages/e2e/src/viewlet.explorer-scroll.ts
@@ -0,0 +1,17 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-scroll'
+
+export const test: Test = async ({ FileSystem, Workspace, Explorer, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  for (let i = 0; i < 100; i++) {
+    const extension = i % 2 === 0 ? '.json' : '.txt'
+    await FileSystem.writeFile(`${tmpDir}/file-${i}${extension}`, '')
+  }
+  await Workspace.setPath(tmpDir)
+  await Explorer.focusFirst()
+
+  // act
+  // TODO scroll
+}
