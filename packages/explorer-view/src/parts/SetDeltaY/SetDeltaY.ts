dc10040a75c7f0e21b5f1f81ee3fcb810d0293ad
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-28 18:32:03 +0100

    fix: update icons when scrolling (#237)

--- packages/explorer-view/src/parts/SetDeltaY/SetDeltaY.ts
+++ packages/explorer-view/src/parts/SetDeltaY/SetDeltaY.ts
@@ -1,6 +1,7 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 
-export const setDeltaY = (state: ExplorerState, deltaY: number): ExplorerState => {
+export const setDeltaY = async (state: ExplorerState, deltaY: number): Promise<ExplorerState> => {
   const { itemHeight, height, items } = state
   if (deltaY < 0) {
     deltaY = 0
@@ -12,10 +13,14 @@ export const setDeltaY = (state: ExplorerState, deltaY: number): ExplorerState =
   }
   const minLineY = Math.round(deltaY / itemHeight)
   const maxLineY = minLineY + Math.round(height / itemHeight)
+  const visible = items.slice(minLineY, maxLineY)
+  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, state.fileIconCache)
   return {
     ...state,
     deltaY,
     minLineY,
     maxLineY,
+    icons,
+    fileIconCache: newFileIconCache,
   }
 }
