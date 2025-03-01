5dc06aa1f00d910e875834af50efca6fc64f2a58
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-01 18:17:14 +0100

    feature: move getChildDirentsRecursively into separate file (#239)

--- packages/explorer-view/src/parts/ExpandRecursively/ExpandRecursively.ts
+++ packages/explorer-view/src/parts/ExpandRecursively/ExpandRecursively.ts
@@ -1,10 +1,8 @@
-import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
-import * as GetChildDirents from '../GetChildDirents/GetChildDirents.ts'
+import * as GetChildDirentsRecursively from '../GetChildDirentsRecursively/GetChildDirentsRecursively.ts'
 import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
 import * as GetParentEndIndex from '../GetParentEndIndex/GetParentEndIndex.ts'
-import * as MakeExpanded from '../MakeExpanded/MakeExpanded.ts'
 
 export const expandRecursively = async (state: ExplorerState): Promise<ExplorerState> => {
   const { items, focusedIndex, pathSeparator, root, height, itemHeight, minLineY } = state
@@ -19,30 +17,11 @@ export const expandRecursively = async (state: ExplorerState): Promise<ExplorerS
   if (dirent.type !== DirentType.Directory && dirent.type !== DirentType.DirectoryExpanding && dirent.type !== DirentType.DirectoryExpanded) {
     return state
   }
-  // TODO this is very inefficient
-  const getChildDirentsRecursively = async (dirent: ExplorerItem): Promise<readonly ExplorerItem[]> => {
-    switch (dirent.type) {
-      case DirentType.File:
-        return [dirent]
-      case DirentType.Directory:
-      case DirentType.DirectoryExpanding:
-      case DirentType.DirectoryExpanded:
-        const childDirents = await GetChildDirents.getChildDirents(pathSeparator, dirent)
-        const all = [MakeExpanded.makeExpanded(dirent)]
-        for (const childDirent of childDirents) {
-          const childAll = await getChildDirentsRecursively(childDirent)
-          all.push(...childAll)
-        }
-        return all
-      default:
-        return []
-    }
-  }
   // TODO race condition: what if folder is being collapse while it is recursively expanding?
   // TODO race condition: what if folder is being deleted while it is recursively expanding?
   // TODO race condition: what if a new file/folder is created while the folder is recursively expanding?
   // @ts-ignore
-  const childDirents = await getChildDirentsRecursively(dirent)
+  const childDirents = await GetChildDirentsRecursively.getChildDirentsRecursively(dirent, pathSeparator)
   const startIndex = focusedIndex
   if (focusedIndex >= 0) {
     const endIndex = GetParentEndIndex.getParentEndIndex(items, focusedIndex)
