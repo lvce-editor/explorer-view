db4683dbd16d962966f941a72eea9d669de8bc87
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-01 18:19:02 +0100

    feature: update file icons when expanding recursively (#240)

--- packages/explorer-view/src/parts/ExpandRecursively/ExpandRecursively.ts
+++ packages/explorer-view/src/parts/ExpandRecursively/ExpandRecursively.ts
@@ -2,6 +2,7 @@ import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as GetChildDirentsRecursively from '../GetChildDirentsRecursively/GetChildDirentsRecursively.ts'
 import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
+import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 import * as GetParentEndIndex from '../GetParentEndIndex/GetParentEndIndex.ts'
 
 export const expandRecursively = async (state: ExplorerState): Promise<ExplorerState> => {
@@ -27,17 +28,25 @@ export const expandRecursively = async (state: ExplorerState): Promise<ExplorerS
     const endIndex = GetParentEndIndex.getParentEndIndex(items, focusedIndex)
     const newDirents = [...items.slice(0, startIndex), ...childDirents, ...items.slice(endIndex)]
     const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
+    const visible = newDirents.slice(minLineY, maxLineY)
+    const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, state.fileIconCache)
     return {
       ...state,
       items: newDirents,
       maxLineY,
+      icons,
+      fileIconCache: newFileIconCache,
     }
   }
   const newDirents = childDirents.slice(1)
   const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
+  const visible = newDirents.slice(minLineY, maxLineY)
+  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, state.fileIconCache)
   return {
     ...state,
     items: newDirents,
     maxLineY,
+    icons,
+    fileIconCache: newFileIconCache,
   }
 }
