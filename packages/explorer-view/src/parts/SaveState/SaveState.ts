d45fdc690c8c661f4dac74d4412e1e2b50407709
Le Vivilet <levivlet@pm.me>
2025-03-09 09:43:46 +0100

    feature: enforce saveState uid to be number

--- packages/explorer-view/src/parts/SaveState/SaveState.ts
+++ packages/explorer-view/src/parts/SaveState/SaveState.ts
@@ -1,18 +1,13 @@
-import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import type { SavedState } from '../SavedState/SavedState.ts'
+import * as Assert from '../Assert/Assert.ts'
 import * as ExplorerStates from '../ExplorerStates/ExplorerStates.ts'
 import * as GetPath from '../GetPath/GetPath.ts'
 import * as IsExpandedDirectory from '../IsExpandedDirectory/IsExpandedDirectory.ts'
 
 export const saveState = (uid: number): SavedState => {
-  let newState: ExplorerState
-  if (typeof uid === 'number') {
-    const value = ExplorerStates.get(uid)
-    newState = value.newState
-  } else {
-    // deprecated
-    newState = uid
-  }
+  Assert.number(uid)
+  const value = ExplorerStates.get(uid)
+  const newState = value.newState
   const { items, root, deltaY, minLineY, maxLineY } = newState
   const expandedPaths = items.filter(IsExpandedDirectory.isExpandedDirectory).map(GetPath.getPath)
   return {
