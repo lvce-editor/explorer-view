8cea969ca5259352bd6f54bc4699933bd084923c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 19:19:52 +0200

    feature: simplify chevron dom (#276)

--- packages/explorer-view/src/parts/ChevronRightVirtualDom/ChevronRightVirtualDom.ts
+++ packages/explorer-view/src/parts/ChevronRightVirtualDom/ChevronRightVirtualDom.ts
@@ -1,9 +1,10 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const chevronRightVirtualDom: VirtualDomNode = {
   type: VirtualDomElements.Div,
-  className: `${ClassNames.Chevron} ${ClassNames.MaskIconChevronRight}`,
+  className: MergeClassNames.mergeClassNames(ClassNames.Chevron, ClassNames.MaskIconChevronRight),
   childCount: 0,
 }
