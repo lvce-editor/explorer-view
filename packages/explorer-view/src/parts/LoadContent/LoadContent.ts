7c394319363316dd2875d67201a3ba2aec7b3907
Le Vivilet <levivlet@pm.me>
2025-04-08 19:03:45 +0200

    feature: move getSavedChildDirents into separate file

--- packages/explorer-view/src/parts/LoadContent/LoadContent.ts
+++ packages/explorer-view/src/parts/LoadContent/LoadContent.ts
@@ -1,12 +1,10 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import * as DirentType from '../DirentType/DirentType.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
 import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 import * as GetSettings from '../GetSettings/GetSettings.ts'
 import * as GetWorkspacePath from '../GetWorkspacePath/GetWorkspacePath.ts'
 import * as RestoreExpandedState from '../RestoreExpandedState/RestoreExpandedState.ts'
-import * as SortExplorerItems from '../SortExplorerItems/SortExplorerItems.ts'
 // TODO viewlet should only have create and refresh functions
 // every thing else can be in a separate module <viewlet>.lazy.js
 // and  <viewlet>.ipc.js
@@ -18,52 +16,6 @@ import * as SortExplorerItems from '../SortExplorerItems/SortExplorerItems.ts'
 
 const getPathSeparator = (root: any): any => {
   return FileSystem.getPathSeparator(root)
-}
-
-const getSavedChildDirents = (map: any, path: any, depth: any, excluded: any, pathSeparator: any): any => {
-  const children = map[path]
-  if (!children) {
-    return []
-  }
-  const dirents = []
-  SortExplorerItems.sortExplorerItems(children)
-  const visible = []
-  const displayRoot = path.endsWith(pathSeparator) ? path : path + pathSeparator
-  for (const child of children) {
-    if (excluded.includes(child.name)) {
-      continue
-    }
-    visible.push(child)
-  }
-  const visibleLength = visible.length
-  for (let i = 0; i < visibleLength; i++) {
-    const child = visible[i]
-    const { name, type } = child
-    const childPath = displayRoot + name
-    if ((child.type === DirentType.Directory || child.type === DirentType.SymLinkFolder) && childPath in map) {
-      dirents.push({
-        depth,
-        posInSet: i + 1,
-        setSize: visibleLength,
-        icon: '',
-        name,
-        path: childPath,
-        type: DirentType.DirectoryExpanded,
-      })
-      dirents.push(...getSavedChildDirents(map, childPath, depth + 1, excluded, pathSeparator))
-    } else {
-      dirents.push({
-        depth,
-        posInSet: i + 1,
-        setSize: visibleLength,
-        icon: '',
-        name,
-        path: childPath,
-        type,
-      })
-    }
-  }
-  return dirents
 }
 
 const getExcluded = (): any => {
