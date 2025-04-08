6b4c84057c8e44dd55670acfe9f723fe3a825cb2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 19:33:06 +0200

    feature: add diffFocus module (#269)
    * feature: add diffFocus module
    
    * feature: ignore empty array result (#270)
    
    * feature: add more exact renderer type (#268)
    
    * feature: add focus renderer (#271)
    
    * feature: add diff2 command (#273)
    
    * feature: add render2 command (#274)
    
    * feature: add focusId module (#272)
    
    * feature: simplify chevron dom (#276)
    
    * feature: add virtual dom dependency (#275)
    
    * memory

--- packages/explorer-view/src/parts/DiffModules/DiffModules.ts
+++ packages/explorer-view/src/parts/DiffModules/DiffModules.ts
@@ -1,4 +1,5 @@
 import * as DiffEditingIndex from '../DiffEditingIndex/DiffEditingIndex.ts'
+import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 
-export const modules = [DiffItems, DiffEditingIndex]
+export const modules = [DiffItems, DiffEditingIndex, DiffFocus]
