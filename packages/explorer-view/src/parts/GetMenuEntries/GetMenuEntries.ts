5b6798240fea268f0e71da7e7799d9d4e003b751
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 19:42:38 +0200

    feature: add menu entry type (#281)

--- packages/explorer-view/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/explorer-view/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,87 +1,89 @@
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as ViewletExplorerStrings from '../ExplorerStrings/ExplorerStrings.ts'
 import * as MenuEntrySeparator from '../MenuEntrySeparator/MenuEntrySeparator.ts'
 import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
 
-const menuEntryNewFile = {
+const menuEntryNewFile: MenuEntry = {
   id: 'newFile',
   label: ViewletExplorerStrings.newFile(),
   flags: MenuItemFlags.None,
   command: 'Explorer.newFile',
 }
 
-const menuEntryNewFolder = {
+const menuEntryNewFolder: MenuEntry = {
   id: 'newFolder',
   label: ViewletExplorerStrings.newFolder(),
   flags: MenuItemFlags.None,
   command: 'Explorer.newFolder',
 }
 
-const menuEntryOpenContainingFolder = {
+const menuEntryOpenContainingFolder: MenuEntry = {
   id: 'openContainingFolder',
   label: ViewletExplorerStrings.openContainingFolder(),
   flags: MenuItemFlags.RestoreFocus,
   command: 'Explorer.openContainingFolder',
 }
 
-const menuEntryOpenInIntegratedTerminal = {
+const menuEntryOpenInIntegratedTerminal: MenuEntry = {
   id: 'openInIntegratedTerminal',
   label: ViewletExplorerStrings.openInIntegratedTerminal(),
   flags: MenuItemFlags.None,
-  command: /* TODO */ -1,
+  command: /* TODO */ '-1',
 }
 
-const menuEntryCut = {
+const menuEntryCut: MenuEntry = {
   id: 'cut',
   label: ViewletExplorerStrings.cut(),
   flags: MenuItemFlags.RestoreFocus,
-  command: /* TODO */ -1,
+  command: /* TODO */ '-1',
 }
 
-const menuEntryCopy = {
+const menuEntryCopy: MenuEntry = {
   id: 'copy',
   label: ViewletExplorerStrings.copy(),
   flags: MenuItemFlags.RestoreFocus,
   command: 'Explorer.handleCopy',
 }
 
-const menuEntryPaste = {
+const menuEntryPaste: MenuEntry = {
   id: 'paste',
   label: ViewletExplorerStrings.paste(),
   flags: MenuItemFlags.None,
   command: 'Explorer.handlePaste',
 }
 
-const menuEntryCopyPath = {
+const menuEntryCopyPath: MenuEntry = {
   id: 'copyPath',
   label: ViewletExplorerStrings.copyPath(),
   flags: MenuItemFlags.RestoreFocus,
   command: 'Explorer.copyPath',
 }
 
-const menuEntryCopyRelativePath = {
+const menuEntryCopyRelativePath: MenuEntry = {
   id: 'copyRelativePath',
   label: ViewletExplorerStrings.copyRelativePath(),
   flags: MenuItemFlags.RestoreFocus,
   command: 'Explorer.copyRelativePath',
 }
 
-const menuEntryRename = {
+const menuEntryRename: MenuEntry = {
   id: 'rename',
   label: ViewletExplorerStrings.rename(),
   flags: MenuItemFlags.None,
   command: 'Explorer.renameDirent',
 }
 
-const menuEntryDelete = {
+const menuEntryDelete: MenuEntry = {
   id: 'delete',
   label: ViewletExplorerStrings.deleteItem(),
   flags: MenuItemFlags.None,
   command: 'Explorer.removeDirent',
 }
 
-const ALL_ENTRIES = [
+const ALL_ENTRIES: readonly MenuEntry[] = [
   menuEntryNewFile,
   menuEntryNewFolder,
   menuEntryOpenContainingFolder,
@@ -101,18 +103,18 @@ const ALL_ENTRIES = [
 // TODO there are two possible ways of getting the focused dirent of explorer
 // 1. directly access state of explorer (bad because it directly accesses state of another component)
 // 2. expose getFocusedDirent method in explorer (bad because explorer code should not know about for menuEntriesExplorer, which needs that method)
-const getFocusedDirent = (explorerState: ExplorerState): any => {
+const getFocusedDirent = (explorerState: ExplorerState): ExplorerItem | undefined => {
   if (!explorerState || explorerState.focusedIndex < 0) {
     return undefined
   }
   return explorerState.items[explorerState.focusedIndex]
 }
 
-const getMenuEntriesDirectory = (): any => {
+const getMenuEntriesDirectory = (): readonly MenuEntry[] => {
   return ALL_ENTRIES
 }
 
-const getMenuEntriesFile = (): any => {
+const getMenuEntriesFile = (): readonly MenuEntry[] => {
   return [
     menuEntryOpenContainingFolder,
     menuEntryOpenInIntegratedTerminal,
@@ -129,11 +131,11 @@ const getMenuEntriesFile = (): any => {
   ]
 }
 
-const getMenuEntriesDefault = (): any => {
+const getMenuEntriesDefault = (): readonly MenuEntry[] => {
   return ALL_ENTRIES
 }
 
-const getMenuEntriesRoot = (): any => {
+const getMenuEntriesRoot = (): readonly MenuEntry[] => {
   return [
     menuEntryNewFile,
     menuEntryNewFolder,
@@ -147,7 +149,7 @@ const getMenuEntriesRoot = (): any => {
   ]
 }
 
-export const getMenuEntries = (state: ExplorerState): any => {
+export const getMenuEntries = (state: ExplorerState): readonly MenuEntry[] => {
   const focusedDirent = getFocusedDirent(state)
   if (!focusedDirent) {
     return getMenuEntriesRoot()
