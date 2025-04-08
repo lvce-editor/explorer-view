4aab74c9f98953e65d2e58c955d5c8f117f188bb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 19:10:40 +0200

    feature: move getSavedChildDirents into separate file (#267)

--- packages/explorer-view/src/parts/RestoreExpandedState/RestoreExpandedState.ts
+++ packages/explorer-view/src/parts/RestoreExpandedState/RestoreExpandedState.ts
@@ -1,54 +1,7 @@
 import * as Character from '../Character/Character.ts'
-import * as DirentType from '../DirentType/DirentType.ts'
 import * as GetChildDirents from '../GetChildDirents/GetChildDirents.ts'
+import { getSavedChildDirents } from '../GetSavedChildDirents/GetSavedChildDirents.ts'
 import * as PromiseStatus from '../PromiseStatus/PromiseStatus.ts'
-import * as SortExplorerItems from '../SortExplorerItems/SortExplorerItems.ts'
-
-const getSavedChildDirents = (map: any, path: any, depth: any, excluded: any, pathSeparator: any): any => {
-  const children = map[path]
-  if (!children) {
-    return []
-  }
-  const dirents = []
-  SortExplorerItems.sortExplorerItems(children)
-  const visible = []
-  const displayRoot = path.endsWith(pathSeparator) ? path : path + pathSeparator
-  for (const child of children) {
-    if (excluded.includes(child.name)) {
-      continue
-    }
-    visible.push(child)
-  }
-  const visibleLength = visible.length
-  for (let i = 0; i < visibleLength; i++) {
-    const child = visible[i]
-    const { name, type } = child
-    const childPath = displayRoot + name
-    if ((child.type === DirentType.Directory || child.type === DirentType.SymLinkFolder) && childPath in map) {
-      dirents.push({
-        depth,
-        posInSet: i + 1,
-        setSize: visibleLength,
-        icon: '',
-        name,
-        path: childPath,
-        type: DirentType.DirectoryExpanded,
-      })
-      dirents.push(...getSavedChildDirents(map, childPath, depth + 1, excluded, pathSeparator))
-    } else {
-      dirents.push({
-        depth,
-        posInSet: i + 1,
-        setSize: visibleLength,
-        icon: '',
-        name,
-        path: childPath,
-        type,
-      })
-    }
-  }
-  return dirents
-}
 
 const createDirents = (root: any, expandedDirentPaths: any, expandedDirentChildren: any, excluded: any, pathSeparator: any): any => {
   const dirents = []
