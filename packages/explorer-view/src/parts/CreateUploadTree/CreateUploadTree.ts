c5c5cb7b1d0a6c916fbef2f664a46e25deeb84a3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 18:49:11 +0200

    feature: use relative paths in uploadTree (#339)
    * feature: use relative paths in uploadTree
    
    * lint

--- packages/explorer-view/src/parts/CreateUploadTree/CreateUploadTree.ts
+++ packages/explorer-view/src/parts/CreateUploadTree/CreateUploadTree.ts
@@ -6,14 +6,14 @@ import { isFileHandle } from '../IsFileHandle/IsFileHandle.ts'
 export const createUploadTree = async (root: string, fileHandles: readonly FileSystemHandle[]): Promise<any> => {
   const uploadTree = Object.create(null)
   for (const fileHandle of fileHandles) {
-    const path = `${root}/${fileHandle.name}`
+    const name = fileHandle.name
     if (isDirectoryHandle(fileHandle)) {
       const children = await getChildHandles(fileHandle)
-      const childTree = await createUploadTree(path, children)
-      uploadTree[path] = childTree
+      const childTree = await createUploadTree(name, children)
+      uploadTree[name] = childTree
     } else if (isFileHandle(fileHandle)) {
       const text = await getFileHandleText(fileHandle)
-      uploadTree[path] = text
+      uploadTree[name] = text
     }
   }
   return uploadTree
--- packages/explorer-view/test/CreateUploadTree.test.ts
+++ packages/explorer-view/test/CreateUploadTree.test.ts
@@ -1,118 +1,106 @@
 import { test, expect } from '@jest/globals'
 import { createUploadTree } from '../src/parts/CreateUploadTree/CreateUploadTree.js'
 
-test('createUploadTree with files', async () => {
+test('createUploadTree with files', async (): Promise<void> => {
   const fileHandle = {
     kind: 'file',
     name: 'test.txt',
-    async getFile(): Promise<{ text: () => Promise<string> }> {
+    async getFile(): Promise<{ text(): Promise<string> }> {
       return {
         async text(): Promise<string> {
           return 'file content'
         },
       }
     },
-    async isSameEntry(): Promise<boolean> {
-      return false
-    },
+    isSameEntry: async (): Promise<boolean> => false,
   } as FileSystemHandle
 
   const result = await createUploadTree('root', [fileHandle])
   expect(result).toEqual({
-    'root/test.txt': 'file content',
+    'test.txt': 'file content',
   })
 })
 
-test('createUploadTree with directories', async () => {
+test('createUploadTree with directories', async (): Promise<void> => {
   const fileHandle = {
     kind: 'file',
     name: 'test.txt',
-    async getFile(): Promise<{ text: () => Promise<string> }> {
+    async getFile(): Promise<{ text(): Promise<string> }> {
       return {
         async text(): Promise<string> {
           return 'file content'
         },
       }
     },
-    async isSameEntry(): Promise<boolean> {
-      return false
-    },
+    isSameEntry: async (): Promise<boolean> => false,
   } as FileSystemHandle
 
   const directoryHandle = {
     kind: 'directory',
     name: 'dir',
-    values(): AsyncIterable<FileSystemHandle> {
+    values(): { [Symbol.asyncIterator](): AsyncGenerator<FileSystemHandle> } {
       return {
         [Symbol.asyncIterator]: async function* (): AsyncGenerator<FileSystemHandle> {
           yield fileHandle
         },
       }
     },
-    async isSameEntry(): Promise<boolean> {
-      return false
-    },
+    isSameEntry: async (): Promise<boolean> => false,
   } as FileSystemHandle
 
   const result = await createUploadTree('root', [directoryHandle])
   expect(result).toEqual({
-    'root/dir': {
-      'root/dir/test.txt': 'file content',
+    dir: {
+      'test.txt': 'file content',
     },
   })
 })
 
-test('createUploadTree with mixed content', async () => {
+test('createUploadTree with mixed content', async (): Promise<void> => {
   const fileHandle1 = {
     kind: 'file',
     name: 'test1.txt',
-    async getFile(): Promise<{ text: () => Promise<string> }> {
+    async getFile(): Promise<{ text(): Promise<string> }> {
       return {
         async text(): Promise<string> {
           return 'file content 1'
         },
       }
     },
-    async isSameEntry(): Promise<boolean> {
-      return false
-    },
+    isSameEntry: async (): Promise<boolean> => false,
   } as FileSystemHandle
 
   const fileHandle2 = {
     kind: 'file',
     name: 'test2.txt',
-    async getFile(): Promise<{ text: () => Promise<string> }> {
+    async getFile(): Promise<{ text(): Promise<string> }> {
       return {
         async text(): Promise<string> {
           return 'file content 2'
         },
       }
     },
-    async isSameEntry(): Promise<boolean> {
-      return false
-    },
+    isSameEntry: async (): Promise<boolean> => false,
   } as FileSystemHandle
 
   const directoryHandle = {
     kind: 'directory',
     name: 'dir',
-    values(): AsyncIterable<FileSystemHandle> {
+    values(): { [Symbol.asyncIterator](): AsyncGenerator<FileSystemHandle> } {
       return {
         [Symbol.asyncIterator]: async function* (): AsyncGenerator<FileSystemHandle> {
           yield fileHandle2
         },
       }
     },
-    async isSameEntry(): Promise<boolean> {
-      return false
-    },
+    isSameEntry: async (): Promise<boolean> => false,
   } as FileSystemHandle
 
   const result = await createUploadTree('root', [fileHandle1, directoryHandle])
   expect(result).toEqual({
-    'root/test1.txt': 'file content 1',
-    'root/dir': {
-      'root/dir/test2.txt': 'file content 2',
+    'test1.txt': 'file content 1',
+    dir: {
+      'test2.txt': 'file content 2',
     },
   })
 })
