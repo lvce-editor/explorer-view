8944d37fea177debef7bed38d11442f5d11702ed
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 11:35:25 +0200

    feature: improve file handle upload (#326)
    * feature: improve file handle upload
    
    * lint
    
    * lint

--- packages/explorer-view/src/parts/UploadFileSystemHandles/UploadFileSystemHandles.ts
+++ packages/explorer-view/src/parts/UploadFileSystemHandles/UploadFileSystemHandles.ts
@@ -1,3 +1,23 @@
-export const uploadFileSystemHandles = async (root: string, pathSeparator: string, fileSystemHandles: any): Promise<any> => {
-  // TODO send to renderer worker
+export const uploadFileSystemHandles = async (root: string, pathSeparator: string, fileSystemHandles: FileList): Promise<boolean> => {
+  // console.log({ fileSystemHandles })
+  // @ts-ignore
+  const files: readonly FileSystemHandle[] = [...fileSystemHandles]
+
+  // @ts-ignore
+  for (const file of files) {
+    // console.log({
+    //   file,
+    //   kind: file.kind,
+    //   name: file.name,
+    //   // text: await file.text(),
+    // })
+  }
+  // console.log({ files })
+
+  // TODO
+  // 1. in electron, use webutils.getPathForFile to see if a path is available
+  // 2. else, walk all files and folders recursively and upload all of them (if there are many, show a progress bar)
+
+  // TODO send file system operations to renderer worker
+  return true
 }
