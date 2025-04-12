1a413e88253779d051568bbf5b360f468183e33f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 09:56:07 +0200

    feature: use mergeClassNames helper function (#319)
    * feature: use mergeClassNames helper function
    
    * lint

--- packages/explorer-view/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.ts
+++ packages/explorer-view/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.ts
@@ -5,6 +5,7 @@ import * as GetChevronType from '../GetChevronType/GetChevronType.ts'
 import * as GetExpandedType from '../GetExpandedType/GetExpandedType.ts'
 import * as GetTreeItemIndent from '../GetTreeItemIndent/GetTreeItemIndent.ts'
 import * as GetTreeItemIndentWithChevron from '../GetTreeItemIndentWithChevron/GetTreeItemIndentWithChevron.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
 const ariaExpandedValues: (string | undefined)[] = [undefined, 'true', 'false']
 
@@ -30,7 +31,7 @@ export const getVisibleExplorerItems = (
     const indent = indentFn(item.depth, chevron)
     const isFocused = i === focusedIndex
     const id = isFocused ? 'TreeItemActive' : undefined
-    const className = isFocused ? ClassNames.TreeItem + ' ' + ClassNames.TreeItemActive : ClassNames.TreeItem
+    const className = isFocused ? MergeClassNames.mergeClassNames(ClassNames.TreeItem, ClassNames.TreeItemActive) : ClassNames.TreeItem
     const expanded = GetExpandedType.getExpandedType(item.type)
     const ariaExpanded = ariaExpandedValues[expanded]
 
