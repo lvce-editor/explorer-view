8e599f8240189df99c5e1eb8467e7ec25ae6a15e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 22:59:26 +0200

    Discard changes to packages/explorer-view/src/parts/CopyFilesElectron/CopyFilesElectron.ts

--- packages/explorer-view/src/parts/CopyFilesElectron/CopyFilesElectron.ts
+++ packages/explorer-view/src/parts/CopyFilesElectron/CopyFilesElectron.ts
@@ -1,11 +1,14 @@
-import { getFilePaths } from '../GetFilePaths/GetFilePaths.ts'
+import * as FileSystem from '../FileSystem/FileSystem.ts'
+import * as GetFilePathElectron from '../GetFilePathElectron/GetFilePathElectron.ts'
+import * as Path from '../Path/Path.ts'
 
 // TODO copy files in parallel
 export const copyFilesElectron = async (root: string, pathSeparator: string, files: readonly FileSystemHandle[]): Promise<void> => {
-  const paths = await getFilePaths(files)
-  console.log({ paths })
-  // for (const path of paths) {
-  // const to = Path.join(pathSeparator, root, file.name)
-  // await FileSystem.copy(from, to)
-  // }
+  for (const file of files) {
+    // @ts-ignore
+    const from = await GetFilePathElectron.getFilePathElectron(file)
+    // const from = file.path
+    const to = Path.join(pathSeparator, root, file.name)
+    await FileSystem.copy(from, to)
+  }
 }
