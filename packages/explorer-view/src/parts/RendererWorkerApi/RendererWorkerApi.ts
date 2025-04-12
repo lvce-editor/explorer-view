25f691f299ca42d24ee27dc31bd09511ce843a6a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 17:35:45 +0200

    feature: update renderer worker api (#329)
    * feature: update renderer worker api
    
    * feature: update renderer worker api
    
    * sort api

--- packages/explorer-view/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++ packages/explorer-view/src/parts/RendererWorkerApi/RendererWorkerApi.ts
@@ -1,11 +1,24 @@
 export interface RendererWorkerApi {
+  readonly 'ClipBoard.readNativeFiles': () => Promise<readonly string[]>
+  readonly 'ClipBoard.writeNativeFiles': (type: string, files: readonly string[]) => Promise<void>
   readonly 'ClipBoard.writeText': (text: string) => Promise<void>
-  readonly 'ElectronDialog.showMessageBox': (options: any) => Promise<any>
-  readonly 'GetWindowId.getWindowId': () => Promise<number>
-  readonly 'Process.getChromeVersion': () => Promise<string>
-  readonly 'Process.getNodeVersion': () => Promise<string>
-  readonly 'Process.getElectronVersion': () => Promise<string>
-  readonly 'Viewlet.closeWidget': (widgetId: string) => Promise<string>
-  readonly 'Viewlet.openWidget': (widgetId: string) => Promise<string>
-  readonly 'Focus.setFocus': (focusId: number) => Promise<string>
+  readonly 'ContextMenu.show': (x: number, y: number, id: number, ...args: readonly any[]) => Promise<void>
+  readonly 'FileSystem.copy': (oldUri: string, newUri: string) => Promise<void>
+  readonly 'FileSystem.createFile': (uri: string) => Promise<void>
+  readonly 'FileSystem.getPathSeparator': (root: string) => Promise<string>
+  readonly 'FileSystem.getRealPath': (root: string) => Promise<string>
+  readonly 'FileSystem.mkdir': (uri: string) => Promise<void>
+  readonly 'FileSystem.readDirWithFileTypes': (uri: string) => Promise<readonly any[]>
+  readonly 'FileSystem.remove': (uri: string) => Promise<void>
+  readonly 'FileSystem.rename': (oldUri: string, newUri: string) => Promise<void>
+  readonly 'FileSystem.stat': (root: string) => Promise<any>
+  readonly 'FileSystem.writeFile': (uri: string, content: string) => Promise<void>
+  readonly 'Focus.setFocus': (focusId: number) => Promise<void>
+  readonly 'GetFilePathElectron.getFilePathElectron': (file: File) => Promise<string>
+  readonly 'IconTheme.getFileIcon': (options: { readonly name: string }) => Promise<string>
+  readonly 'IconTheme.getFolderIcon': (options: { readonly name: string }) => Promise<string>
+  readonly 'Main.openUri': (uri: string, focus: boolean) => Promise<string>
+  readonly 'OpenNativeFolder.openNativeFolder': (path: string) => Promise<void>
+  readonly 'Preferences.get': (key: string) => Promise<any>
+  readonly 'Workspace.getPath': () => Promise<string>
 }
