171f49c32f2c1daabff9589cdaf552f3d6863c10
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 18:32:31 +0200

    feature: simplify code (#340)

--- packages/explorer-view/src/parts/CreateUploadTree/CreateUploadTree.ts
+++ packages/explorer-view/src/parts/CreateUploadTree/CreateUploadTree.ts
@@ -1,4 +1,4 @@
-import * as Arrays from '../Arrays/Arrays.ts'
+import { getChildHandles } from '../GetChildHandles/GetChildHandles.ts'
 import { isDirectoryHandle } from '../IsDirectoryHandle/IsDirectoryHandle.ts'
 import { isFileHandle } from '../IsFileHandle/IsFileHandle.ts'
 
@@ -7,9 +7,7 @@ export const createUploadTree = async (root: string, fileHandles: readonly FileS
   for (const fileHandle of fileHandles) {
     const path = `${root}/${fileHandle.name}`
     if (isDirectoryHandle(fileHandle)) {
-      // @ts-ignore
-      const values = fileHandle.values()
-      const children = await Arrays.fromAsync(values)
+      const children = await getChildHandles(fileHandle)
       const childTree = await createUploadTree(path, children)
       uploadTree[path] = childTree
     } else if (isFileHandle(fileHandle)) {
