9cde5fc646e24abb353695e2709890107e5b730c
Le Vivilet <levivlet@pm.me>
2025-04-12 09:01:29 +0200

    feature: add types for collapse

--- packages/explorer-view/src/parts/IsTopLevel/IsTopLevel.ts
+++ packages/explorer-view/src/parts/IsTopLevel/IsTopLevel.ts
@@ -1,3 +1,5 @@
-export const isTopLevel = (dirent: any): boolean => {
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
+
+export const isTopLevel = (dirent: ExplorerItem): boolean => {
   return dirent.depth === 1
 }
--- packages/explorer-view/test/IsTopLevel.test.ts
+++ packages/explorer-view/test/IsTopLevel.test.ts
@@ -1,9 +1,13 @@
 import { expect, test } from '@jest/globals'
+import type { ExplorerItem } from '../src/parts/ExplorerItem/ExplorerItem.ts'
 import * as IsTopLevel from '../src/parts/IsTopLevel/IsTopLevel.ts'
 
 test('isTopLevel - depth 1', () => {
-  const dirent = {
+  const dirent: ExplorerItem = {
     depth: 1,
+    name: '',
+    path: '',
+    type: 0,
   }
   expect(IsTopLevel.isTopLevel(dirent)).toBe(true)
 })
@@ -11,6 +15,9 @@ test('isTopLevel - depth 1', () => {
 test('isTopLevel - depth 2', () => {
   const dirent = {
     depth: 2,
+    name: '',
+    path: '',
+    type: 0,
   }
   expect(IsTopLevel.isTopLevel(dirent)).toBe(false)
 })
