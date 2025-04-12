261f01bdcb154a92492c7ff8efc6a702aa8d8556
Le Vivilet <levivlet@pm.me>
2025-04-12 19:30:40 +0200

    feature: refresh explorer after dropping files

--- packages/explorer-view/src/parts/Refresh/Refresh.ts
+++ packages/explorer-view/src/parts/Refresh/Refresh.ts
@@ -3,19 +3,8 @@ import { getTopLevelDirents } from '../GetTopLevelDirents/GetTopLevelDirents.ts'
 import { mergeDirents } from '../MergeDirents/MergeDirents.ts'
 
 // TODO add lots of tests for this
-export const updateRoot = async (state1: ExplorerState): Promise<ExplorerState> => {
-  // @ts-ignore
-  if (state1.disposed) {
-    return state1
-  }
-  // const file = nativeFiles.files[0]
-  // @ts-ignore
+export const refresh = async (state1: ExplorerState): Promise<ExplorerState> => {
   const topLevelDirents = await getTopLevelDirents(state1.root, state1.pathSeparator, [])
-  // const state2 = Viewlet.getState('Explorer')
-  // // TODO what if root changes while reading directories?
-  // if (state2.disposed || state2.root !== state1.root) {
-  //   return state2
-  // }
   const newDirents = mergeDirents(state1.items, topLevelDirents)
   const state3 = {
     ...state1,
