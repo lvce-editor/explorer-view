88d460235342aa97d1c7bc635790b04f2648973f
Le Vivilet <levivlet@pm.me>
2025-04-12 10:31:15 +0200

    feature: improve file handle upload

--- packages/explorer-view/src/parts/UploadFileSystemHandles/UploadFileSystemHandles.ts
+++ packages/explorer-view/src/parts/UploadFileSystemHandles/UploadFileSystemHandles.ts
@@ -1,3 +1,21 @@
-export const uploadFileSystemHandles = async (root: string, pathSeparator: string, fileSystemHandles: any): Promise<any> => {
-  // TODO send to renderer worker
+export const uploadFileSystemHandles = async (root: string, pathSeparator: string, fileSystemHandles: FileList): Promise<void> => {
+  // console.log({ fileSystemHandles })
+  // @ts-ignore
+  const files: readonly FileSystemHandle[] = [...fileSystemHandles]
+
+  for (const file of files) {
+    // console.log({
+    //   file,
+    //   kind: file.kind,
+    //   name: file.name,
+    //   // text: await file.text(),
+    // })
+  }
+  // console.log({ files })
+
+  // TODO
+  // 1. in electron, use webutils.getPathForFile to see if a path is available
+  // 2. else, walk all files and folders recursively and upload all of them (if there are many, show a progress bar)
+
+  // TODO send file system operations to renderer worker
 }
