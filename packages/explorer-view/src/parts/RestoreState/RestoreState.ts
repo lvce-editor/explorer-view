3efdafe2bcb7bffe73b725e76f87422c5d2c97f1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-12 09:18:19 +0200

    feature: add types for restoreState function (#309)
    * feature: add types for restoreState function
    
    * lint

--- packages/explorer-view/src/parts/RestoreState/RestoreState.ts
+++ packages/explorer-view/src/parts/RestoreState/RestoreState.ts
@@ -1,23 +1,38 @@
-import * as GetSavedRoot from '../GetSavedRoot/GetSavedRoot.ts'
+import type { RestoredState } from '../RestoredState/RestoredState.ts'
 
-export const restoreState = (savedState: any): any => {
+const getSavedMinLineY = (savedState: unknown): number => {
+  if (savedState && typeof savedState === 'object' && 'minLineY' in savedState && typeof savedState.minLineY === 'number') {
+    return savedState.minLineY
+  }
+  return 0
+}
+const getSavedDeltaY = (savedState: unknown): number => {
+  if (savedState && typeof savedState === 'object' && 'deltaY' in savedState && typeof savedState.deltaY === 'number') {
+    return savedState.deltaY
+  }
+  return 0
+}
+
+const getSavedWorkspacePath = (savedState: unknown): string => {
+  if (savedState && typeof savedState === 'object' && 'workspacePath' in savedState && typeof savedState.workspacePath === 'string') {
+    return savedState.workspacePath
+  }
+  return ''
+}
+
+export const restoreState = (savedState: unknown): RestoredState => {
   if (!savedState) {
     return {
+      root: '',
       minLineY: 0,
       deltaY: 0,
     }
   }
-  const root = GetSavedRoot.getSavedRoot(savedState, savedState.workspacePath || '')
-  let minLineY = 0
-  if (savedState && typeof savedState.minLineY === 'number') {
-    minLineY = savedState.minLineY
-  }
-  let deltaY = 0
-  if (savedState && typeof savedState.deltaY === 'number') {
-    deltaY = savedState.deltaY
-  }
+
+  const root = getSavedWorkspacePath(savedState)
+  const minLineY = getSavedMinLineY(savedState)
+  const deltaY = getSavedDeltaY(savedState)
   return {
-    ...savedState,
     root,
     minLineY,
     deltaY,
