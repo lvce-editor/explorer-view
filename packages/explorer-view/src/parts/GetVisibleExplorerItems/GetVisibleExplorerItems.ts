5659a484b3e507e8f9c3033e04cdacf3faaf7536
Le Vivilet <levivlet@pm.me>
2025-04-12 09:52:46 +0200

    lint

--- packages/explorer-view/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.ts
+++ packages/explorer-view/src/parts/GetVisibleExplorerItems/GetVisibleExplorerItems.ts
@@ -3,9 +3,9 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as GetChevronType from '../GetChevronType/GetChevronType.ts'
 import * as GetExpandedType from '../GetExpandedType/GetExpandedType.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as GetTreeItemIndent from '../GetTreeItemIndent/GetTreeItemIndent.ts'
 import * as GetTreeItemIndentWithChevron from '../GetTreeItemIndentWithChevron/GetTreeItemIndentWithChevron.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
 const ariaExpandedValues: (string | undefined)[] = [undefined, 'true', 'false']
 
