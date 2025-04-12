4661789f31b98b9bd4d75fde9ed258c57c100f4e
Le Vivilet <levivlet@pm.me>
2025-04-12 19:20:56 +0200

    fix: file operations

--- packages/explorer-view/src/parts/GetFileOperations/GetFileOperations.ts
+++ packages/explorer-view/src/parts/GetFileOperations/GetFileOperations.ts
@@ -9,11 +9,12 @@ export const getFileOperations = (root: string, uploadTree: any): readonly FileO
 
   const processTree = (tree: any, currentPath: string): void => {
     for (const [path, value] of Object.entries(tree)) {
+      const fullPath = currentPath ? `${currentPath}/${path}` : path
       if (typeof value === 'object') {
-        operations.push({ type: 'createFolder', path: `${root}/${path}`, text: '' })
-        processTree(value, path)
+        operations.push({ type: 'createFolder', path: `${root}/${fullPath}`, text: '' })
+        processTree(value, fullPath)
       } else if (typeof value === 'string') {
-        operations.push({ type: 'createFile', path: `${root}/${path}`, text: value })
+        operations.push({ type: 'createFile', path: `${root}/${fullPath}`, text: value })
       }
     }
   }
