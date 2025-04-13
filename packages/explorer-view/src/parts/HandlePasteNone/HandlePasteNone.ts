93d75d02beacd7594d3280c9c2a71ba629289fe6
Le Vivilet <levivlet@pm.me>
2025-04-13 13:07:35 +0200

    fix: remove console logs

--- packages/explorer-view/src/parts/HandlePasteNone/HandlePasteNone.ts
+++ packages/explorer-view/src/parts/HandlePasteNone/HandlePasteNone.ts
@@ -1,6 +1,6 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
 export const handlePasteNone = async (state: ExplorerState, nativeFiles: any): Promise<ExplorerState> => {
-  console.info('[ViewletExplorer/handlePaste] no paths detected')
+  console.error('[ViewletExplorer/handlePaste] no paths detected')
   return state
 }
