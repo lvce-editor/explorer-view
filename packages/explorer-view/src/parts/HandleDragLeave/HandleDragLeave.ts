ca09d42d9b474bd1617531d8bff3de5ebae58729
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-13 09:47:43 +0200

    fix: ignore drag leave for now due to flickering (#359)

--- packages/explorer-view/src/parts/HandleDragLeave/HandleDragLeave.ts
+++ packages/explorer-view/src/parts/HandleDragLeave/HandleDragLeave.ts
@@ -1,12 +1,13 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
 export const handleDragLeave = (state: ExplorerState): ExplorerState => {
-  const { dropTargets } = state
-  if (dropTargets.length === 0) {
-    return state
-  }
-  return {
-    ...state,
-    dropTargets: [],
-  }
+  return state
+  // const { dropTargets } = state
+  // if (dropTargets.length === 0) {
+  //   return state
+  // }
+  // return {
+  //   ...state,
+  //   dropTargets: [],
+  // }
 }
