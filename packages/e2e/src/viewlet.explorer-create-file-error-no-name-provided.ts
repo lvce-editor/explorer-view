063765a061d1e65f1c04e285b721a9c8671cf6a5
Le Vivilet <levivlet@pm.me>
2025-04-13 12:51:28 +0200

    feature: improve createFile e2e test

--- packages/e2e/src/viewlet.explorer-create-file-error-no-name-provided.ts
+++ packages/e2e/src/viewlet.explorer-create-file-error-no-name-provided.ts
@@ -3,7 +3,6 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-create-file-error-no-name-provided'
-
 export const skip = true
 
 export const test: Test = async ({ FileSystem, Workspace, Explorer, ContextMenu, expect, Locator, KeyBoard }) => {
@@ -12,18 +11,18 @@ export const test: Test = async ({ FileSystem, Workspace, Explorer, ContextMenu,
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
   await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
   await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
-
   await Workspace.setPath(tmpDir)
 
-  await Explorer.focusIndex(-1)
-  await Explorer.openContextMenu(-1)
-  await ContextMenu.selectItem('New File')
+  // act
+  await Explorer.newFile()
 
+  // assert
   const inputBox = Locator('input')
   await expect(inputBox).toBeVisible()
   await expect(inputBox).toBeFocused()
 
-  await KeyBoard.press('Enter')
+  // act
+  await Explorer.acceptEdit()
 
   // TODO there should be an error message below the input box
   const dialog = Locator('#Dialog')
