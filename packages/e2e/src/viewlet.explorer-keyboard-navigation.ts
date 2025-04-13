56925eb48a379dfea7bc53fe62bf5eca1a91a48c
Le Vivilet <levivlet@pm.me>
2025-04-13 09:58:16 +0200

    feature: enable keyboard navigation test

--- packages/e2e/src/viewlet.explorer-keyboard-navigation.ts
+++ packages/e2e/src/viewlet.explorer-keyboard-navigation.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-keyboard-navigation'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Explorer, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
