c0bb18d08021fe4c8bdc17b53202bdea914c7d5c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-13 12:34:17 +0200

    feature: move acceptRename into separate file (#369)

---
+++ packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts
@@ -0,0 +1,34 @@
+import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import * as ComputeExplorerRenamedDirent from '../ComputeExplorerRenamedDirent/ComputeExplorerRenamedDirent.ts'
+import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
+import * as FileSystem from '../FileSystem/FileSystem.ts'
+import * as Path from '../Path/Path.ts'
+
+export const acceptRename = async (state: ExplorerState): Promise<ExplorerState> => {
+  const { editingIndex, editingValue, items, pathSeparator } = state
+  const renamedDirent = items[editingIndex]
+  try {
+    // TODO this does not work with rename of nested file
+    const oldAbsolutePath = renamedDirent.path
+    const oldParentPath = Path.dirname(pathSeparator, oldAbsolutePath)
+    const newAbsolutePath = [oldParentPath, editingValue].join(pathSeparator)
+    await FileSystem.rename(oldAbsolutePath, newAbsolutePath)
+  } catch {
+    // TODO
+    // await ErrorHandling.showErrorDialog(error)
+    return state
+  }
+  const { newDirents, focusedIndex } = ComputeExplorerRenamedDirent.computeExplorerRenamedDirent(items, editingIndex, editingValue)
+  //  TODO move focused index
+  // @ts-ignore
+  state.items = newDirents
+  return {
+    ...state,
+    editingIndex: -1,
+    editingValue: '',
+    editingType: ExplorerEditingType.None,
+    editingIcon: '',
+    focusedIndex,
+    focused: true,
+  }
+}
