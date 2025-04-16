d85e4ca52857fd89ec7a260de9bfc93edff13512
Le Vivilet <levivlet@pm.me>
2025-04-16 23:47:36 +0200

    fix: refresh fileIcons and maxlineY when refreshing explorer

--- packages/explorer-view/src/parts/Refresh/Refresh.ts
+++ packages/explorer-view/src/parts/Refresh/Refresh.ts
@@ -1,14 +1,23 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
+import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 import { getTopLevelDirents } from '../GetTopLevelDirents/GetTopLevelDirents.ts'
 import { mergeDirents } from '../MergeDirents/MergeDirents.ts'
 
 // TODO add lots of tests for this
-export const refresh = async (state1: ExplorerState): Promise<ExplorerState> => {
-  const topLevelDirents = await getTopLevelDirents(state1.root, state1.pathSeparator, [])
-  const newDirents = mergeDirents(state1.items, topLevelDirents)
-  const state3 = {
-    ...state1,
+export const refresh = async (state: ExplorerState): Promise<ExplorerState> => {
+  const { root, pathSeparator, minLineY, height, itemHeight, fileIconCache } = state
+  const topLevelDirents = await getTopLevelDirents(root, pathSeparator, [])
+  const newDirents = mergeDirents(state.items, topLevelDirents)
+  const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
+  const visible = newDirents.slice(minLineY, maxLineY)
+  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, fileIconCache)
+  const state3: ExplorerState = {
+    ...state,
     items: newDirents,
+    fileIconCache: newFileIconCache,
+    icons,
+    maxLineY,
   }
   return state3
 }
