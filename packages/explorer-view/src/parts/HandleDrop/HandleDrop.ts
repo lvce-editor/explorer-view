a41d1cd163f498bf64b019444cec90c1f044b7e2
Le Vivilet <levivlet@pm.me>
2025-04-16 23:21:57 +0200

    feature: move getFileArray into separate file

--- packages/explorer-view/src/parts/HandleDrop/HandleDrop.ts
+++ packages/explorer-view/src/parts/HandleDrop/HandleDrop.ts
@@ -1,5 +1,6 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import { getDropHandler } from '../GetDropHandler/GetDropHandler.ts'
+import * as GetFileArray from '../GetFileArray/GetFileArray.ts'
 import { getFileHandles } from '../GetFileHandles/GetFileHandles.ts'
 import { getFilePaths } from '../GetFilePaths/GetFilePaths.ts'
 import * as GetIndexFromPosition from '../GetIndexFromPosition/GetIndexFromPosition.ts'
@@ -13,8 +14,7 @@ export const handleDrop = async (
   fileList: FileList,
 ): Promise<ExplorerState> => {
   try {
-    // @ts-ignore
-    const files = [...fileList]
+    const files = GetFileArray.getFileArray(fileList)
     const fileHandles = await getFileHandles(fileIds)
     const paths = await getFilePaths(files, state.platform)
     const index = GetIndexFromPosition.getIndexFromPosition(state, x, y)
