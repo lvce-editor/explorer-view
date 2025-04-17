c3e0a45253e70372b7a9bfd00db72c3079cf3217
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-17 21:54:21 +0200

    feature: add rename e2e test (#402)

---
+++ packages/e2e/src/viewlet.explorer-rename-file.ts
@@ -0,0 +1,39 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-rename-file'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Explorer, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
+  await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
+
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await Explorer.focusIndex(1)
+  await Command.execute('Explorer.renameDirent')
+
+  // assert
+  const explorer = Locator('.Explorer')
+  const inputBox = explorer.locator('input')
+  await expect(inputBox).toBeVisible()
+
+  // TODO
+  // await expect(inputBox).toBeFocused()
+
+  // act
+  await Explorer.updateEditingValue('file4.txt')
+  await Explorer.acceptEdit()
+
+  // assert
+  await expect(inputBox).toBeHidden()
+
+  const file2 = Locator('.TreeItem', { hasText: 'file2.txt' })
+  await expect(file2).toBeVisible()
+  await expect(explorer).toBeFocused()
+  await expect(file2).toHaveId('TreeItemActive')
+}
