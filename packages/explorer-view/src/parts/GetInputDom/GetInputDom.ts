929efb49b8adad609373dc9255d92418ef1e3149
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-17 21:46:24 +0200

    feature: move input dom into separate file (#399)

---
+++ packages/explorer-view/src/parts/GetInputDom/GetInputDom.ts
@@ -0,0 +1,26 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputName from '../InputName/InputName.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+const getClassName = (hasEditingError: boolean): string => {
+  if (hasEditingError) {
+    return MergeClassNames.mergeClassNames(ClassNames.InputBox, ClassNames.InputValidationError)
+  }
+  return ClassNames.InputBox
+}
+
+export const getInputDom = (hasEditingError: boolean): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Input,
+      className: getClassName(hasEditingError),
+      id: 'ExplorerInput',
+      onInput: DomEventListenerFunctions.HandleEditingInput,
+      childCount: 0,
+      name: InputName.ExplorerInput,
+    },
+  ]
+}
