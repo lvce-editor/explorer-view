175b180b46ad3a7196d9f0d26a4ed0acfc6e225a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-17 21:54:47 +0200

    feature: allow passing inputSource to updateEditingValue (#403)

--- packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
+++ packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
@@ -1,6 +1,7 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
-export const updateEditingValue = (state: ExplorerState, value: string): ExplorerState => {
+export const updateEditingValue = (state: ExplorerState, value: string, inputSource: number = InputSource.User): ExplorerState => {
   const editingIcon = ''
   return {
     ...state,
