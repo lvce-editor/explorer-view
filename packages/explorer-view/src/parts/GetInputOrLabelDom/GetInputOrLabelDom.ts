c752f453eafcd3eae6d32a30266d23c5375aade2
Le Vivilet <levivlet@pm.me>
2025-04-17 21:41:41 +0200

    feature: move input dom into separate file

--- packages/explorer-view/src/parts/GetInputOrLabelDom/GetInputOrLabelDom.ts
+++ packages/explorer-view/src/parts/GetInputOrLabelDom/GetInputOrLabelDom.ts
@@ -1,8 +1,6 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import * as InputName from '../InputName/InputName.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as GetInputDom from '../GetInputDom/GetInputDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
@@ -12,25 +10,9 @@ const label: VirtualDomNode = {
   childCount: 1,
 }
 
-const getClassName = (hasEditingError: boolean): string => {
-  if (hasEditingError) {
-    return MergeClassNames.mergeClassNames(ClassNames.InputBox, ClassNames.InputValidationError)
-  }
-  return ClassNames.InputBox
-}
-
 export const getInputOrLabelDom = (isEditing: boolean, hasEditingError: boolean, name: string): readonly VirtualDomNode[] => {
   if (isEditing) {
-    return [
-      {
-        type: VirtualDomElements.Input,
-        className: getClassName(hasEditingError),
-        id: 'ExplorerInput',
-        onInput: DomEventListenerFunctions.HandleEditingInput,
-        childCount: 0,
-        name: InputName.ExplorerInput,
-      },
-    ]
+    return GetInputDom.getInputDom(hasEditingError)
   }
   return [label, text(name)]
 }
