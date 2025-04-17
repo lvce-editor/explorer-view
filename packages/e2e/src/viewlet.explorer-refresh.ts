c6efb02bcfd511e2e25d63c95961f82c846e9f75
Le Vivilet <levivlet@pm.me>
2025-04-17 22:39:27 +0200

    feature: add explorer refresh e2e test

---
+++ packages/e2e/src/viewlet.explorer-refresh.ts
@@ -0,0 +1,25 @@
+import { type Test, executeCommand } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-refresh'
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Explorer, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
+  await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
+
+  // act
+  await Workspace.setPath(tmpDir)
+
+  // assert
+  const file1 = Locator('.TreeItem', { hasText: 'file1.txt' })
+  await expect(file1).toBeVisible()
+
+  // act
+  await Command.execute('FileSystem.remove', `${tmpDir}/file1.txt`)
+  await Command.execute('Explorer.refresh')
+
+  // assert
+  // await expect(file1).toHaveCount(0)
+}
