4de468e019645584926b2df65e874ac99182b2cf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-17 22:33:38 +0200

    feature: improve drop test (#408)

--- packages/e2e/src/viewlet.explorer-handle-drop.ts
+++ packages/e2e/src/viewlet.explorer-handle-drop.ts
@@ -12,13 +12,17 @@ export const test: Test = async ({ Command, FileSystem, Workspace, Explorer, Con
   await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
   await Workspace.setPath(tmpDir)
   const directory = await navigator.storage.getDirectory()
-  const fileHandle = await directory.getFileHandle('my first file', {
+  const fileHandle = await directory.getFileHandle('dropped-file.txt', {
     create: true,
   })
   const file = await fileHandle.getFile()
   const fileList = [file]
-  const id = await Command.execute('FileSystemHandle.addFileHandle')
-  // act
+  const id = await Command.execute('FileSystemHandle.addFileHandle', fileHandle)
 
+  // act
   await Command.execute('Explorer.handleDrop', 0, 0, [id], fileList)
+
+  // assert
+  const droppedFile = Locator('.TreeItem', { hasText: 'dropped-file.txt' })
+  await expect(droppedFile).toBeVisible()
 }
