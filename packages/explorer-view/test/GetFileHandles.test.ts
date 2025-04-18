ac23c72c8f95a5c175b23d1c90eaa0aef22b8f8b
Le Vivilet <levivlet@pm.me>
2025-04-18 11:45:18 +0200

    feature: add file handles test

---
+++ packages/explorer-view/test/GetFileHandles.test.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@jest/globals'
+import { getFileHandles } from '../src/parts/GetFileHandles/GetFileHandles.ts'
+import * as RpcId from '../src/parts/RpcId/RpcId.ts'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
+
+class MockFileHandle {
+  constructor(public name: string) {}
+}
+
+test('getFileHandles', async () => {
+  const fileIds = [1, 2, 3]
+  const mockFiles = [new MockFileHandle('file1'), new MockFileHandle('file2'), new MockFileHandle('file3')]
+
+  const mockRpc = {
+    invoke: async () => mockFiles,
+    send: async () => {},
+    invokeAndTransfer: async () => mockFiles,
+    dispose: async () => {},
+  }
+
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+
+  const result = await getFileHandles(fileIds)
+  expect(result).toBe(mockFiles)
+  expect(result[0]).toBeInstanceOf(MockFileHandle)
+})
