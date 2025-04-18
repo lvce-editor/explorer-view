9cb58686bdc56eefc532dd74d122532c27dde3fe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-18 18:13:59 +0200

    feature: simplify diff code (#439)

--- packages/explorer-view/src/parts/DiffModules/DiffModules.ts
+++ packages/explorer-view/src/parts/DiffModules/DiffModules.ts
@@ -2,4 +2,6 @@ import * as DiffEditingIndex from '../DiffEditingIndex/DiffEditingIndex.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 
-export const modules = [DiffItems, DiffEditingIndex, DiffFocus]
+export const modules = [DiffItems.isEqual, DiffEditingIndex.isEqual, DiffFocus.isEqual]
+
+export const numbers = [DiffItems.diffType, DiffEditingIndex.diffType, DiffFocus.diffType]
