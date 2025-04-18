bd7f9428d74958eae766a8934596ed15f04a89ac
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-18 12:41:23 +0200

    feature: simplify types (#426)

--- packages/explorer-view/src/parts/GetFileIcons/GetFileIcons.ts
+++ packages/explorer-view/src/parts/GetFileIcons/GetFileIcons.ts
@@ -2,20 +2,20 @@ import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { FileIconCache } from '../FileIconCache/FileIconCache.ts'
 import * as GetFileIconsCached from '../GetFileIconsCached/GetFileIconsCached.ts'
 import * as GetMissingIconRequests from '../GetMissingIconRequests/GetMissingIconRequests.ts'
+import { getPath } from '../GetPath/GetPath.ts'
 import * as RequestFileIcons from '../RequestFileIcons/RequestFileIcons.ts'
 import * as UpdateIconCache from '../UpdateIconCache/UpdateIconCache.ts'
 
-export const getFileIcons = async (
-  dirents: readonly ExplorerItem[],
-  fileIconCache: FileIconCache,
-): Promise<{
-  icons: readonly string[]
-  newFileIconCache: FileIconCache
-}> => {
+interface FileIconsResult {
+  readonly icons: readonly string[]
+  readonly newFileIconCache: FileIconCache
+}
+
+export const getFileIcons = async (dirents: readonly ExplorerItem[], fileIconCache: FileIconCache): Promise<FileIconsResult> => {
   const missingRequests = GetMissingIconRequests.getMissingIconRequests(dirents, fileIconCache)
   const newIcons = await RequestFileIcons.requestFileIcons(missingRequests)
   const newFileIconCache = UpdateIconCache.updateIconCache(fileIconCache, missingRequests, newIcons)
-  const paths = dirents.map((dirent) => dirent.path)
+  const paths = dirents.map(getPath)
   const icons = GetFileIconsCached.getIconsCached(paths, newFileIconCache)
   return {
     icons,
