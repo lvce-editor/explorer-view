fd26a5d7db7e7282eabbd27d4cc129fe938bf7b1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-18 19:29:07 +0200

    feature: remove unused editingIndex code (#445)

--- packages/explorer-view/src/parts/DiffModules/DiffModules.ts
+++ packages/explorer-view/src/parts/DiffModules/DiffModules.ts
@@ -1,7 +1,6 @@
-import * as DiffEditingIndex from '../DiffEditingIndex/DiffEditingIndex.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 
-export const modules = [DiffItems.isEqual, DiffEditingIndex.isEqual, DiffFocus.isEqual]
+export const modules = [DiffItems.isEqual, DiffFocus.isEqual]
 
-export const numbers = [DiffItems.diffType, DiffEditingIndex.diffType, DiffFocus.diffType]
+export const numbers = [DiffItems.diffType, DiffFocus.diffType]
