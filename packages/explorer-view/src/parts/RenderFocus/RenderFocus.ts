ffbe941bbdfd83df0f6eee8cbc0121778dad5786
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-18 18:25:24 +0200

    feature: focus list items on click (#441)

--- packages/explorer-view/src/parts/RenderFocus/RenderFocus.ts
+++ packages/explorer-view/src/parts/RenderFocus/RenderFocus.ts
@@ -1,10 +1,17 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
 import * as InputName from '../InputName/InputName.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const renderFocus = (oldState: ExplorerState, newState: ExplorerState): readonly any[] => {
+  if (newState.inputSource === InputSource.User) {
+    return []
+  }
   if (newState.focus === FocusId.Input) {
     return ['Viewlet.focusElementByName', InputName.ExplorerInput]
+  }
+  if (newState.focus === FocusId.List) {
+    return ['Viewlet.focusSelector', '.ListItems']
   }
   // TODO
   // 1. when focused, focus the outer list element
