ea73820c2ecf7a7c61e5fb31e4a46c041cd4092c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-19 08:30:20 +0200

    feature: add pointerdown test (#458)

---
+++ packages/explorer-view/test/HandlePointerDown.test.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as DirentType from '../src/parts/DirentType/DirentType.ts'
+import * as FocusId from '../src/parts/FocusId/FocusId.ts'
+import { handlePointerDown } from '../src/parts/HandlePointerDown/HandlePointerDown.ts'
+import * as MouseEventType from '../src/parts/MouseEventType/MouseEventType.ts'
+
+test('left click outside items', () => {
+  const state = createDefaultState()
+  const result = handlePointerDown(state, MouseEventType.LeftClick, 100, 100)
+  expect(result).toEqual({
+    ...state,
+    focused: true,
+    focusedIndex: -1,
+    focus: FocusId.List,
+  })
+})
+
+test('right click outside items', () => {
+  const state = createDefaultState()
+  const result = handlePointerDown(state, 2, 100, 100)
+  expect(result).toEqual(state)
+})
+
+test('left click on item', () => {
+  const state = {
+    ...createDefaultState(),
+    items: [{ name: 'test.txt', type: DirentType.File, path: '/test.txt', depth: 0 }],
+  }
+  const result = handlePointerDown(state, MouseEventType.LeftClick, 0, 0)
+  expect(result).toEqual(state)
+})
