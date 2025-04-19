87f6fa586e62e2aa7f01c6e74cd9d8b0728f1f12
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-19 18:13:05 +0200

    feature: add render test (#501)
    * feature: add render test
    
    * feature: add render test

---
+++ packages/explorer-view/test/Render2.test.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as DiffType from '../src/parts/DiffType/DiffType.ts'
+import * as ExplorerStates from '../src/parts/ExplorerStates/ExplorerStates.ts'
+import * as Render2 from '../src/parts/Render2/Render2.ts'
+
+test('render2 - basic', () => {
+  const uid = 1
+  const diffResult = [DiffType.RenderItems, DiffType.RenderFocus]
+  const oldState = createDefaultState()
+  const newState = { ...oldState }
+  ExplorerStates.set(uid, oldState, newState)
+  const result = Render2.render2(uid, diffResult)
+  expect(result).toEqual([
+    [
+      'Viewlet.setDom2',
+      [
+        {
+          childCount: 1,
+          className: 'Viewlet Explorer',
+          role: 'none',
+          type: 4,
+        },
+        {
+          ariaActiveDescendant: 'TreeItemActive',
+          ariaLabel: 'Files Explorer',
+          childCount: 0,
+          className: 'ListItems',
+          onBlur: 'handleListBlur',
+          onClick: 'handleClick',
+          onContextMenu: 'handleContextMenu',
+          onDragLeave: 'handleDragLeave',
+          onDragOver: 'handleDragOver',
+          onDrop: 'handleDrop',
+          onFocus: 'handleListFocus',
+          onPointerDown: 'handlePointerDown',
+          onWheel: 'handleWheel',
+          role: 'tree',
+          tabIndex: 0,
+          type: 4,
+        },
+      ],
+    ],
+  ])
+})
