0de313e6f11e8f9e4b0a38ebb6c2b28a2740b855
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-19 10:58:28 +0200

    feature: add file paths test (#488)

---
+++ packages/explorer-view/test/GetFilePaths.test.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
+import { getFilePaths } from '../src/parts/GetFilePaths/GetFilePaths.ts'
+import * as PlatformType from '../src/parts/PlatformType/PlatformType.ts'
+import { RendererWorker } from '../src/parts/RpcId/RpcId.ts'
+
+test('getFilePaths - non-electron platform', async () => {
+  const files = [new File([], 'test.txt')]
+  const paths = await getFilePaths(files, PlatformType.Web)
+  expect(paths).toEqual([''])
+})
+
+test('getFilePaths - electron platform', async () => {
+  const mockRpc = await MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'FileSystemHandle.getFilePathElectron') {
+        return Promise.resolve('/path/to/file')
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RpcRegistry.set(RendererWorker, mockRpc)
+
+  const files = [new File([], 'test.txt')]
+  const paths = await getFilePaths(files, PlatformType.Electron)
+  expect(paths).toEqual(['/path/to/file'])
+})
+
+test('getFilePaths - multiple files', async () => {
+  const mockRpc = await MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'FileSystemHandle.getFilePathElectron') {
+        return Promise.resolve('/path/to/file')
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RpcRegistry.set(RendererWorker, mockRpc)
+
+  const files = [new File([], 'test1.txt'), new File([], 'test2.txt')]
+  const paths = await getFilePaths(files, PlatformType.Electron)
+  expect(paths).toEqual(['/path/to/file', '/path/to/file'])
+})
