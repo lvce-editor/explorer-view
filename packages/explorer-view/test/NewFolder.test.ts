543edb8becad73b1dded212303a0dda64e6693b4
Le Vivilet <levivlet@pm.me>
2025-04-19 10:30:34 +0200

    feature: add newFolder test

---
+++ packages/explorer-view/test/NewFolder.test.ts
@@ -0,0 +1,50 @@
+import { expect, jest, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import type { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.js'
+import * as ExplorerEditingType from '../src/parts/ExplorerEditingType/ExplorerEditingType.js'
+import { newFolder } from '../src/parts/NewFolder/NewFolder.js'
+import * as RpcId from '../src/parts/RpcId/RpcId.js'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.js'
+
+test('newFolder', async () => {
+  const invoke = jest.fn((method: string): any => {
+    if (method === 'Workspace.getPath') {
+      return '/new/path'
+    }
+    if (method === 'FileSystem.readDirWithFileTypes') {
+      return []
+    }
+    if (method === 'FileSystem.getPathSeparator') {
+      return '/'
+    }
+    if (method === 'Preferences.get') {
+      return false
+    }
+    if (method === 'Focus.setFocus') {
+      return undefined
+    }
+    throw new Error(`unexpected method ${method}`)
+  })
+
+  const mockRpc = await MockRpc.create({
+    invoke,
+    commandMap: {},
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+  const mockState: ExplorerState = {
+    ...createDefaultState(),
+    focusedIndex: -1,
+    items: [],
+  }
+
+  const result = await newFolder(mockState)
+  expect(mockRpc.invoke).toHaveBeenCalledWith('Focus.setFocus', 14)
+  expect(result).toEqual({
+    ...mockState,
+    editingIndex: -1,
+    editingType: ExplorerEditingType.CreateFolder,
+    editingValue: '',
+    focus: 2,
+  })
+})
