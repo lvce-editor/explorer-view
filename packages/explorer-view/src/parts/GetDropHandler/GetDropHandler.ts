7a299e1720df30b2314c9cb51e957f7a3f292ec8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-19 18:27:33 +0200

    feature: add dropHandler test (#503)

--- packages/explorer-view/src/parts/GetDropHandler/GetDropHandler.ts
+++ packages/explorer-view/src/parts/GetDropHandler/GetDropHandler.ts
@@ -1,16 +1,6 @@
-import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import type { DropHandler } from '../DropHandler/DropHandler.ts'
 import * as HandleDropIndex from '../HandleDropIndex/HandleDropIndex.ts'
 import * as HandleDropRoot from '../HandleDropRoot/HandleDropRoot.ts'
-
-interface DropHandler {
-  (
-    state: ExplorerState,
-    fileHandles: readonly FileSystemHandle[],
-    files: readonly File[],
-    paths: readonly string[],
-    index: number,
-  ): Promise<ExplorerState>
-}
 
 export const getDropHandler = (index: number): DropHandler => {
   switch (index) {
