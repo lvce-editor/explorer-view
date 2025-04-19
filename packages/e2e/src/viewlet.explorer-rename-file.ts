aee72130e798d3d8fa29eb6d48f40067f82f2bd8
Le Vivilet <levivlet@pm.me>
2025-04-19 18:47:30 +0200

    lint

--- packages/e2e/src/viewlet.explorer-rename-file.ts
+++ packages/e2e/src/viewlet.explorer-rename-file.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-rename-file'
 
-export const test: Test = async ({ Command, FileSystem, Workspace, Explorer, Locator, expect }) => {
+export const test: Test = async ({ FileSystem, Workspace, Explorer, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
