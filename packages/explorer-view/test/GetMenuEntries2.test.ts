93eec9dd83738fac8201a2f1da74278ddc2a4185
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-19 10:44:32 +0200

    feature: add menu entries test (#481)
    * feature: add menu entries test
    
    * lint

---
+++ packages/explorer-view/test/GetMenuEntries2.test.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '@jest/globals'
+import type { ExplorerItem } from '../src/parts/ExplorerItem/ExplorerItem.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as DirentType from '../src/parts/DirentType/DirentType.ts'
+import { set } from '../src/parts/ExplorerStates/ExplorerStates.ts'
+import { getMenuEntries2 } from '../src/parts/GetMenuEntries2/GetMenuEntries2.ts'
+
+test('getMenuEntries2 - root', () => {
+  const uid = 1
+  const state = createDefaultState()
+  set(uid, state, state)
+  const menuEntries = getMenuEntries2(uid)
+  expect(menuEntries.length).toBeGreaterThan(0)
+})
+
+test('getMenuEntries2 - directory', () => {
+  const uid = 1
+  const item: ExplorerItem = {
+    type: DirentType.Directory,
+    name: 'test',
+    path: '/test',
+    depth: 0,
+  }
+  const state = {
+    ...createDefaultState(),
+    items: [item],
+    focusedIndex: 0,
+  }
+  set(uid, state, state)
+  const menuEntries = getMenuEntries2(uid)
+  expect(menuEntries.length).toBeGreaterThan(0)
+})
+
+test('getMenuEntries2 - file', () => {
+  const uid = 1
+  const item: ExplorerItem = {
+    type: DirentType.File,
+    name: 'test.txt',
+    path: '/test.txt',
+    depth: 0,
+  }
+  const state = {
+    ...createDefaultState(),
+    items: [item],
+    focusedIndex: 0,
+  }
+  set(uid, state, state)
+  const menuEntries = getMenuEntries2(uid)
+  expect(menuEntries.length).toBeGreaterThan(0)
+})
