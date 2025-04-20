bf8bb03121d431f89d053fd736489b5ee1da145d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-20 17:55:11 +0200

    feature: reset selection when focusIndex is called (#537)

--- packages/explorer-view/src/parts/FocusIndex/FocusIndex.ts
+++ packages/explorer-view/src/parts/FocusIndex/FocusIndex.ts
@@ -1,11 +1,16 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
 export const focusIndex = (state: ExplorerState, index: number): ExplorerState => {
-  const { minLineY, maxLineY } = state
+  const { minLineY, maxLineY, items } = state
+  const newItems = items.map((item, i) => ({
+    ...item,
+    selected: i === index ? true : false,
+  }))
   if (index < minLineY) {
     if (index < 0) {
       return {
         ...state,
+        items: newItems,
         focusedIndex: index,
         focused: true,
       }
@@ -13,6 +18,7 @@ export const focusIndex = (state: ExplorerState, index: number): ExplorerState =
     const diff = maxLineY - minLineY
     return {
       ...state,
+      items: newItems,
       focusedIndex: index,
       focused: true,
       minLineY: index,
@@ -23,6 +29,7 @@ export const focusIndex = (state: ExplorerState, index: number): ExplorerState =
     const diff = maxLineY - minLineY
     return {
       ...state,
+      items: newItems,
       focusedIndex: index,
       focused: true,
       minLineY: index + 1 - diff,
@@ -31,6 +38,7 @@ export const focusIndex = (state: ExplorerState, index: number): ExplorerState =
   }
   return {
     ...state,
+    items: newItems,
     focusedIndex: index,
     focused: true,
   }
