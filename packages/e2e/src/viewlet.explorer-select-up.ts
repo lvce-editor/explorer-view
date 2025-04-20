98c90c1a12b8ecf3ff81eed6efe6fe0fe2b70773
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-20 18:13:20 +0200

    feature: enable selectUp test (#539)

--- packages/e2e/src/viewlet.explorer-select-up.ts
+++ packages/e2e/src/viewlet.explorer-select-up.ts
@@ -2,8 +2,6 @@ import { type Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-select-up'
 
-export const skip = 1
-
 export const test: Test = async ({ Explorer, Command, FileSystem, Workspace, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -17,10 +15,10 @@ export const test: Test = async ({ Explorer, Command, FileSystem, Workspace, Loc
   await Command.execute('Explorer.selectUp')
 
   // assert
-  const file1 = Locator('text=file1.txt')
-  await expect(file1).toHaveClass('Selected')
-  const file2 = Locator('text=file2.txt')
-  await expect(file2).toHaveClass('Selected')
-  const file3 = Locator('text=file3.txt')
-  await expect(file3).not.toHaveClass('Selected')
+  const file1 = Locator('.TreeItem').nth(0)
+  await expect(file1).toHaveClass('TreeItemActive')
+  const file2 = Locator('.TreeItem').nth(1)
+  await expect(file2).toHaveClass('TreeItemActive')
+  const file3 = Locator('.TreeItem').nth(2)
+  await expect(file3).not.toHaveClass('TreeItem')
 }
