98aef86afe9e8fe2e98024b949b63661e7b445bc
Le Vivilet <levivlet@pm.me>
2025-04-20 17:45:36 +0200

    feature: reset selection when focusIndex is called

--- packages/explorer-view/test/FocusIndex.test.ts
+++ packages/explorer-view/test/FocusIndex.test.ts
@@ -1,12 +1,14 @@
 import { expect, test } from '@jest/globals'
+import type { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as DirentType from '../src/parts/DirentType/DirentType.ts'
 import * as ViewletExplorerFocusIndex from '../src/parts/FocusIndex/FocusIndex.ts'
 
 test('focusIndex - scroll up', () => {
-  const state = {
+  const state: ExplorerState = {
+    ...createDefaultState(),
     root: '/home/test-user/test-path',
     focusedIndex: 1,
-    top: 0,
     height: 600,
     deltaY: 0,
     minLineY: 1,
@@ -14,101 +16,154 @@ test('focusIndex - scroll up', () => {
     items: [
       {
         depth: 1,
-        index: 0,
-        languageId: 'unknown',
         name: 'index.css',
         path: '/index.css',
-        setSize: 2,
         type: DirentType.File,
+        selected: false,
       },
       {
         depth: 1,
-        index: 1,
-        languageId: 'unknown',
         name: 'index.html',
         path: '/index.html',
-        setSize: 2,
         type: DirentType.File,
+        selected: true,
       },
     ],
-  } as any
+  }
   expect(ViewletExplorerFocusIndex.focusIndex(state, 0)).toMatchObject({
     focusedIndex: 0,
     minLineY: 0,
     maxLineY: 1,
+    items: [
+      {
+        selected: true,
+      },
+      {
+        selected: false,
+      },
+    ],
   })
 })
 
 test('focusIndex - scroll down', () => {
-  const state = {
+  const state: ExplorerState = {
+    ...createDefaultState(),
     root: '/home/test-user/test-path',
     focusedIndex: 0,
-    top: 0,
-    height: 600,
-    deltaY: 0,
     minLineY: 0,
     maxLineY: 1,
     items: [
       {
         depth: 1,
-        index: 0,
-        languageId: 'unknown',
         name: 'index.css',
         path: '/index.css',
-        setSize: 2,
         type: DirentType.File,
+        selected: true,
       },
       {
         depth: 1,
-        index: 1,
-        languageId: 'unknown',
         name: 'index.html',
         path: '/index.html',
-        setSize: 2,
         type: DirentType.File,
+        selected: false,
       },
     ],
-  } as any
+  }
   expect(ViewletExplorerFocusIndex.focusIndex(state, 1)).toMatchObject({
     focusedIndex: 1,
     minLineY: 1,
     maxLineY: 2,
+    items: [
+      {
+        selected: false,
+      },
+      {
+        selected: true,
+      },
+    ],
   })
 })
 
 test('focusIndex - focus container', () => {
-  const state = {
-    root: '/home/test-user/test-path',
+  const state: ExplorerState = {
+    ...createDefaultState(),
     focusedIndex: 0,
-    top: 0,
-    height: 600,
-    deltaY: 0,
     minLineY: 0,
     maxLineY: 1,
     items: [
       {
         depth: 1,
-        index: 0,
-        languageId: 'unknown',
         name: 'index.css',
         path: '/index.css',
-        setSize: 2,
         type: DirentType.File,
+        selected: true,
       },
       {
         depth: 1,
-        index: 1,
-        languageId: 'unknown',
         name: 'index.html',
         path: '/index.html',
-        setSize: 2,
         type: DirentType.File,
+        selected: false,
       },
     ],
-  } as any
+  }
   expect(ViewletExplorerFocusIndex.focusIndex(state, -1)).toMatchObject({
     focusedIndex: -1,
     minLineY: 0,
     maxLineY: 1,
+    items: [
+      {
+        selected: false,
+      },
+      {
+        selected: false,
+      },
+    ],
+  })
+})
+
+test('focusIndex - unselects all other items', () => {
+  const state: ExplorerState = {
+    ...createDefaultState(),
+    focusedIndex: 0,
+    minLineY: 0,
+    maxLineY: 3,
+    items: [
+      {
+        depth: 1,
+        name: 'index.css',
+        path: '/index.css',
+        type: DirentType.File,
+        selected: true,
+      },
+      {
+        depth: 1,
+        name: 'index.html',
+        path: '/index.html',
+        type: DirentType.File,
+        selected: true,
+      },
+      {
+        depth: 1,
+        name: 'index.js',
+        path: '/index.js',
+        type: DirentType.File,
+        selected: true,
+      },
+    ],
+  }
+  expect(ViewletExplorerFocusIndex.focusIndex(state, 1)).toMatchObject({
+    focusedIndex: 1,
+    items: [
+      {
+        selected: false,
+      },
+      {
+        selected: true,
+      },
+      {
+        selected: false,
+      },
+    ],
   })
 })
