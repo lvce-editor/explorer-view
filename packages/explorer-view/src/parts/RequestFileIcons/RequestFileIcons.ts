14f8a958b072daabc3e6d5b53cde3186702e2a45
Le Vivilet <levivlet@pm.me>
2025-04-21 22:19:31 +0200

    feature: move getFIleIcon into separate file

--- packages/explorer-view/src/parts/RequestFileIcons/RequestFileIcons.ts
+++ packages/explorer-view/src/parts/RequestFileIcons/RequestFileIcons.ts
@@ -1,12 +1,11 @@
 import type { IconRequest } from '../IconRequest/IconRequest.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
-import * as Rpc from '../ParentRpc/ParentRpc.ts'
+import * as GetFileIcon from '../GetFileIcon/GetFileIcon.ts'
+import * as GetFolderIcon from '../GetFolderIcon/GetFolderIcon.ts'
 
 export const requestFileIcons = async (requests: readonly IconRequest[]): Promise<readonly string[]> => {
   const promises = requests.map((request) =>
-    request.type === DirentType.File
-      ? Rpc.invoke('IconTheme.getFileIcon', { name: request.name })
-      : Rpc.invoke('IconTheme.getFolderIcon', { name: request.name }),
+    request.type === DirentType.File ? GetFileIcon.getFileIcon(request.name) : GetFolderIcon.getFolderIcon(request.name),
   )
   return Promise.all(promises)
 }
