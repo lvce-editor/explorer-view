03b8fc26f2befd3736637ff545b4eeaa22c5e105
Le Vivilet <levivlet@pm.me>
2025-04-21 22:30:03 +0200

    test

--- packages/explorer-view/test/UpdateEditingValue.test.ts
+++ packages/explorer-view/test/UpdateEditingValue.test.ts
@@ -3,6 +3,7 @@ import { MockRpc } from '@lvce-editor/rpc'
 import type { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as DirentType from '../src/parts/DirentType/DirentType.ts'
+import * as ExplorerEditingType from '../src/parts/ExplorerEditingType/ExplorerEditingType.ts'
 import * as InputSource from '../src/parts/InputSource/InputSource.ts'
 import * as RpcId from '../src/parts/RpcId/RpcId.ts'
 import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
@@ -44,10 +45,10 @@ test('updateEditingValue - updates state with new value and input source', async
   expect(result.editingIcon).toBe('')
 })
 
-test.skip('updateEditingValue - updates file icon', async () => {
+test('updateEditingValue - updates file icon', async () => {
   const state: ExplorerState = {
     ...createDefaultState(),
-    editingType: DirentType.EditingFile,
+    editingType: ExplorerEditingType.CreateFile,
   }
   const newValue = 'test.txt'
   const result = await updateEditingValue(state, newValue)
@@ -55,10 +56,10 @@ test.skip('updateEditingValue - updates file icon', async () => {
   expect(result.editingIcon).toBe('file-test.txt')
 })
 
-test.skip('updateEditingValue - updates folder icon', async () => {
+test('updateEditingValue - updates folder icon', async () => {
   const state: ExplorerState = {
     ...createDefaultState(),
-    editingType: DirentType.EditingFolder,
+    editingType: ExplorerEditingType.CreateFolder,
   }
   const newValue = 'test'
   const result = await updateEditingValue(state, newValue)
@@ -66,7 +67,7 @@ test.skip('updateEditingValue - updates folder icon', async () => {
   expect(result.editingIcon).toBe('folder-test')
 })
 
-test.skip('updateEditingValue - preserves other state properties', async () => {
+test('updateEditingValue - preserves other state properties', async () => {
   const state: ExplorerState = createDefaultState()
   const result = await updateEditingValue(state, 'new value')
   expect(result.uid).toBe(state.uid)
