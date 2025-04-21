b925e7efc21b453e3d31b860b360c437802d6c44
Le Vivilet <levivlet@pm.me>
2025-04-21 21:39:52 +0200

    feature: reset explorer items when canceling edit

--- packages/explorer-view/src/parts/CancelEdit/CancelEdit.ts
+++ packages/explorer-view/src/parts/CancelEdit/CancelEdit.ts
@@ -1,11 +1,19 @@
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import * as DirentType from '../DirentType/DirentType.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
 
+const isNormalItem = (item: ExplorerItem): boolean => {
+  return item.type !== DirentType.EditingFile && item.type !== DirentType.EditingFolder
+}
+
 export const cancelEdit = (state: ExplorerState): ExplorerState => {
   const { editingIndex } = state
+  const filteredItems = state.items.filter(isNormalItem)
   return {
     ...state,
+    items: filteredItems,
     focusedIndex: editingIndex,
     focused: true,
     editingIndex: -1,
