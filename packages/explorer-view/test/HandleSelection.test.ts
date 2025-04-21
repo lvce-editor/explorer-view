56e3a06b747d96cd4c5f037c945044f72dc3de57
Le Vivilet <levivlet@pm.me>
2025-04-21 10:38:12 +0200

    test

--- packages/explorer-view/test/HandleSelection.test.ts
+++ packages/explorer-view/test/HandleSelection.test.ts
@@ -18,19 +18,3 @@ test('handleSelection toggles selection of item at index', () => {
   expect(newState.items[0].selected).toBe(false)
   expect(newState.items[2].selected).toBe(false)
 })
-
-test('setSelectedIndices sets selection for given indices', () => {
-  const state = {
-    ...createDefaultState(),
-    items: [
-      { name: 'file1', type: 0, path: '/file1', depth: 0, selected: false },
-      { name: 'file2', type: 0, path: '/file2', depth: 0, selected: false },
-      { name: 'file3', type: 0, path: '/file3', depth: 0, selected: false },
-    ] as readonly ExplorerItem[],
-  }
-
-  const newState = setSelectedIndices(state, [0, 2])
-  expect(newState.items[0].selected).toBe(true)
-  expect(newState.items[1].selected).toBe(false)
-  expect(newState.items[2].selected).toBe(true)
-})
