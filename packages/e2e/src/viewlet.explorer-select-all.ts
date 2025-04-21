1949c5d128507a7e169f8b5880bf7ed4fb14b1c3
Le Vivilet <levivlet@pm.me>
2025-04-21 10:44:03 +0200

    enable test

--- packages/e2e/src/viewlet.explorer-select-all.ts
+++ packages/e2e/src/viewlet.explorer-select-all.ts
@@ -2,8 +2,6 @@ import { type Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-select-all'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, Explorer, FileSystem, Workspace, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
