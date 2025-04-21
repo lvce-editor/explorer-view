4aabe0b712dbeb27d69c60442c4c751ff1c01e40
Le Vivilet <levivlet@pm.me>
2025-04-21 07:29:24 +0200

    feature: add mouseactions

---
+++ packages/explorer-view/src/parts/GetMouseActions/GetMouseActions.ts
@@ -0,0 +1,44 @@
+import type { MouseAction } from '../MouseAction/MouseAction.ts'
+import * as MouseEventType from '../MouseEventType/MouseEventType.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+
+export const getMouseActions = (): readonly MouseAction[] => {
+  return [
+    {
+      id: 'explorer.openFile',
+      description: 'Open file on click',
+      button: MouseEventType.LeftClick,
+      modifiers: {},
+      command: 'Explorer.openFile',
+      when: WhenExpression.FocusExplorer,
+    },
+    {
+      id: 'explorer.toggleSelection',
+      description: 'Toggle selection with Ctrl+Click',
+      button: MouseEventType.LeftClick,
+      modifiers: {
+        ctrl: true,
+      },
+      command: 'Explorer.toggleSelection',
+      when: WhenExpression.FocusExplorer,
+    },
+    {
+      id: 'explorer.rangeSelection',
+      description: 'Select range with Shift+Click',
+      button: MouseEventType.LeftClick,
+      modifiers: {
+        shift: true,
+      },
+      command: 'Explorer.rangeSelection',
+      when: WhenExpression.FocusExplorer,
+    },
+    {
+      id: 'explorer.contextMenu',
+      description: 'Show context menu on right click',
+      button: 2, // Right click
+      modifiers: {},
+      command: 'Explorer.showContextMenu',
+      when: WhenExpression.FocusExplorer,
+    },
+  ]
+}
