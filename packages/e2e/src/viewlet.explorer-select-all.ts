43455252be13b6436b19fceebb0b3c000fa142ce
Le Vivilet <levivlet@pm.me>
2025-04-21 10:42:46 +0200

    feature: add selectAll e2e test

---
+++ packages/e2e/src/viewlet.explorer-select-all.ts
@@ -0,0 +1,26 @@
+import { type Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-select-all'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, Explorer, FileSystem, Workspace, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
+  await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
+  await Workspace.setPath(tmpDir)
+  await Explorer.focusIndex(2)
+
+  // act
+  await Command.execute('Explorer.selectAll')
+
+  // assert
+  const file1 = Locator('.TreeItem').nth(0)
+  await expect(file1).toHaveClass('TreeItemActive')
+  const file2 = Locator('.TreeItem').nth(1)
+  await expect(file2).toHaveClass('TreeItemActive')
+  const file3 = Locator('.TreeItem').nth(2)
+  await expect(file3).not.toHaveClass('TreeItemActive')
+}
