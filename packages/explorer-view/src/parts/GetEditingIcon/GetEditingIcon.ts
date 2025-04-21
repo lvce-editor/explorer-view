8631fad49c26f4a2f23bb3ae4f72f75fee0f461e
Le Vivilet <levivlet@pm.me>
2025-04-21 22:24:17 +0200

    fix: editing value update

--- packages/explorer-view/src/parts/GetEditingIcon/GetEditingIcon.ts
+++ packages/explorer-view/src/parts/GetEditingIcon/GetEditingIcon.ts
@@ -1,11 +1,15 @@
-import * as DirentType from '../DirentType/DirentType.ts'
+import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
 
 export const getEditingIcon = async (editingType: number, value: string): Promise<string> => {
-  if (editingType === DirentType.File) {
+  if (editingType === ExplorerEditingType.CreateFile) {
     return Rpc.invoke('IconTheme.getFileIcon', { name: value })
   }
-  if (editingType === DirentType.Directory) {
+  // TODO need renamefile and renamefolder type
+  if (editingType === ExplorerEditingType.Rename) {
+    return Rpc.invoke('IconTheme.getFileIcon', { name: value })
+  }
+  if (editingType === ExplorerEditingType.CreateFolder) {
     return Rpc.invoke('IconTheme.getFolderIcon', { name: value })
   }
   return ''
