818bba95a3906be6b5b2cec859b238e6cd349d55
Le Vivilet <levivlet@pm.me>
2025-04-21 22:10:23 +0200

    feature: move editing icon into separate file

--- packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
+++ packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
@@ -1,17 +1,6 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import * as DirentType from '../DirentType/DirentType.ts'
+import { getEditingIcon } from '../GetEditingIcon/GetEditingIcon.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
-import * as Rpc from '../ParentRpc/ParentRpc.ts'
-
-const getEditingIcon = async (editingType: number, value: string): Promise<string> => {
-  if (editingType === DirentType.File) {
-    return Rpc.invoke('IconTheme.getFileIcon', { name: value })
-  }
-  if (editingType === DirentType.Directory) {
-    return Rpc.invoke('IconTheme.getFolderIcon', { name: value })
-  }
-  return ''
-}
 
 export const updateEditingValue = async (state: ExplorerState, value: string, inputSource: number = InputSource.User): Promise<ExplorerState> => {
   const editingIcon = await getEditingIcon(state.editingType, value)
