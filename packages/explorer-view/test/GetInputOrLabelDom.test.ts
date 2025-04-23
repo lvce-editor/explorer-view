584fa664a48ba21eb0406db341256cf22bd2933b
Le Vivilet <levivlet@pm.me>
2025-04-24 00:03:17 +0200

    feature: add explorerInput classname to input box for more detailed styling

--- packages/explorer-view/test/GetInputOrLabelDom.test.ts
+++ packages/explorer-view/test/GetInputOrLabelDom.test.ts
@@ -7,7 +7,7 @@ test('getInputOrLabelDom - editing state', () => {
   const result = getInputOrLabelDom(true, false, 'test')
   expect(result).toHaveLength(1)
   expect(result[0].type).toBe(Input)
-  expect(result[0].className).toBe(InputBox)
+  expect(result[0].className).toBe(`InputBox ExplorerInputBox`)
 })
 
 test('getInputOrLabelDom - non-editing state', () => {
@@ -23,5 +23,5 @@ test('getInputOrLabelDom - editing state with error', () => {
   const result = getInputOrLabelDom(true, true, 'test')
   expect(result).toHaveLength(1)
   expect(result[0].type).toBe(Input)
-  expect(result[0].className).toBe(`${InputBox} ${InputValidationError}`)
+  expect(result[0].className).toBe(`${InputBox} ExplorerInputBox ${InputValidationError}`)
 })
