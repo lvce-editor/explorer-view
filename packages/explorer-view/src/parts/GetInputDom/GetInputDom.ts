91b103f4a24de8f8407d98b0e4ed61b4410eca55
Le Vivilet <levivlet@pm.me>
2025-04-23 23:53:56 +0200

    feature: move input classname into separate file

--- packages/explorer-view/src/parts/GetInputDom/GetInputDom.ts
+++ packages/explorer-view/src/parts/GetInputDom/GetInputDom.ts
@@ -1,17 +1,9 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as ExplorerStrings from '../ExplorerStrings/ExplorerStrings.ts'
+import { getInputClassName } from '../GetInputClassName/GetInputClassName.ts'
 import * as InputName from '../InputName/InputName.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-
-const getClassName = (hasEditingError: boolean): string => {
-  if (hasEditingError) {
-    return MergeClassNames.mergeClassNames(ClassNames.InputBox, ClassNames.InputValidationError)
-  }
-  return ClassNames.InputBox
-}
 
 export const getInputDom = (hasEditingError: boolean): readonly VirtualDomNode[] => {
   const ariaLabel = ExplorerStrings.typeAFileName()
@@ -22,7 +14,7 @@ export const getInputDom = (hasEditingError: boolean): readonly VirtualDomNode[]
       autocapitalize: 'off',
       autocorrect: 'off',
       childCount: 0,
-      className: getClassName(hasEditingError),
+      className: getInputClassName(hasEditingError),
       id: 'ExplorerInput',
       name: InputName.ExplorerInput,
       onBlur: DomEventListenerFunctions.HandleInputBlur,
