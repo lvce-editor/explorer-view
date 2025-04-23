4f0ba2276245853087f28736e07fdcf91b95c9db
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-24 00:02:58 +0200

    feature: move label dom into separate file (#608)

--- packages/explorer-view/src/parts/GetInputOrLabelDom/GetInputOrLabelDom.ts
+++ packages/explorer-view/src/parts/GetInputOrLabelDom/GetInputOrLabelDom.ts
@@ -1,18 +1,10 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetInputDom from '../GetInputDom/GetInputDom.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-
-const label: VirtualDomNode = {
-  type: VirtualDomElements.Div,
-  className: ClassNames.Label,
-  childCount: 1,
-}
+import { getLabelDom } from '../GetLabelDom/GetLabelDom.ts'
 
 export const getInputOrLabelDom = (isEditing: boolean, hasEditingError: boolean, name: string): readonly VirtualDomNode[] => {
   if (isEditing) {
     return GetInputDom.getInputDom(hasEditingError)
   }
-  return [label, text(name)]
+  return getLabelDom(name)
 }
