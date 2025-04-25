de578cd313e7200d00d6826d7c7f9261dd2a25c5
Le Vivilet <levivlet@pm.me>
2025-04-25 17:55:53 +0200

    name function

--- packages/explorer-view/src/parts/FilterByFocusWord/FilterByFocusWord.ts
+++ packages/explorer-view/src/parts/FilterByFocusWord/FilterByFocusWord.ts
@@ -1,6 +1,9 @@
 import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 
 export const filterByFocusWord = (items: readonly ExplorerItem[], focusWord: string): number => {
-  const matchingIndex = items.findIndex((item) => item.name.toLowerCase().startsWith(focusWord))
+  const matchesWord = (item: ExplorerItem): boolean => {
+    return item.name.toLowerCase().startsWith(focusWord)
+  }
+  const matchingIndex = items.findIndex(matchesWord)
   return matchingIndex
 }
