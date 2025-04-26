0f4f67d40710a698c558f11e0496132900ae92be
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-26 17:16:02 +0200

    Discard changes to packages/explorer-view/src/parts/RequestFileIcons/RequestFileIcons.ts

--- packages/explorer-view/src/parts/RequestFileIcons/RequestFileIcons.ts
+++ packages/explorer-view/src/parts/RequestFileIcons/RequestFileIcons.ts
@@ -1,9 +1,11 @@
 import type { IconRequest } from '../IconRequest/IconRequest.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
-import { toSimpleIconRequest } from '../ToSimpleIconRequest/ToSimpleIconRequest.ts'
+import * as DirentType from '../DirentType/DirentType.ts'
+import * as GetFileIcon from '../GetFileIcon/GetFileIcon.ts'
+import * as GetFolderIcon from '../GetFolderIcon/GetFolderIcon.ts'
 
 export const requestFileIcons = async (requests: readonly IconRequest[]): Promise<readonly string[]> => {
-  const simpleRequests = requests.map(toSimpleIconRequest)
-  const icons = await ParentRpc.invoke('IconTheme.getIcons', simpleRequests)
-  return icons
+  const promises = requests.map((request) =>
+    request.type === DirentType.File ? GetFileIcon.getFileIcon(request.name) : GetFolderIcon.getFolderIcon(request.name),
+  )
+  return Promise.all(promises)
 }
