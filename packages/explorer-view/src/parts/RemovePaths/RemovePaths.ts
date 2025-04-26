2b5149bfc0f85e7cb8cca494c48eeeffdc725ba1
Le Vivilet <levivlet@pm.me>
2025-04-26 14:36:37 +0200

    lint

--- packages/explorer-view/src/parts/RemovePaths/RemovePaths.ts
+++ packages/explorer-view/src/parts/RemovePaths/RemovePaths.ts
@@ -4,6 +4,8 @@ export const removePaths = async (paths: readonly string[]): Promise<void> => {
   for (const item of paths) {
     try {
       await FileSystem.remove(item)
-    } catch {}
+    } catch {
+      // ignore
+    }
   }
 }
