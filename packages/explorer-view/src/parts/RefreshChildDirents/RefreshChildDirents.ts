af23ed99bf20c530e9ab01c945265cdd3b5e286c
Le Vivilet <levivlet@pm.me>
2025-04-26 14:24:20 +0200

    feature: simplify helper function

--- packages/explorer-view/src/parts/RefreshChildDirents/RefreshChildDirents.ts
+++ packages/explorer-view/src/parts/RefreshChildDirents/RefreshChildDirents.ts
@@ -2,6 +2,32 @@ import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 
+export const refreshChildDirent = async (
+  folder: ExplorerItem,
+  dirent: any,
+  pathSeparator: string,
+  expandedFolders: readonly string[],
+): Promise<readonly ExplorerItem[]> => {
+  const path = folder.path.endsWith(pathSeparator) ? `${folder.path}${dirent.name}` : `${folder.path}${pathSeparator}${dirent.name}`
+  const isExpandedFolder = expandedFolders.includes(path)
+  const type = dirent.type === 'directory' ? (isExpandedFolder ? DirentType.DirectoryExpanded : DirentType.Directory) : DirentType.File
+
+  const item: ExplorerItem = {
+    name: dirent.name,
+    type,
+    path,
+    depth: folder.depth + 1,
+    selected: false,
+  }
+
+  if (isExpandedFolder) {
+    const nestedItems = await refreshChildDirents(item, pathSeparator, expandedFolders)
+    return [item, ...nestedItems]
+  }
+
+  return [item]
+}
+
 export const refreshChildDirents = async (
   folder: ExplorerItem,
   pathSeparator: string,
@@ -10,24 +36,7 @@ export const refreshChildDirents = async (
   const childDirents = await FileSystem.readDirWithFileTypes(folder.path)
   const childItems = await Promise.all(
     childDirents.map(async (dirent) => {
-      const path = folder.path.endsWith(pathSeparator) ? `${folder.path}${dirent.name}` : `${folder.path}${pathSeparator}${dirent.name}`
-      const isExpandedFolder = expandedFolders.includes(path)
-      const type = dirent.type === 'directory' ? (isExpandedFolder ? DirentType.DirectoryExpanded : DirentType.Directory) : DirentType.File
-
-      const item: ExplorerItem = {
-        name: dirent.name,
-        type,
-        path,
-        depth: folder.depth + 1,
-        selected: false,
-      }
-
-      if (isExpandedFolder) {
-        const nestedItems = await refreshChildDirents(item, pathSeparator, expandedFolders)
-        return [item, ...nestedItems]
-      }
-
-      return [item]
+      return refreshChildDirent(folder, dirent, pathSeparator, expandedFolders)
     }),
   )
   return childItems.flat()
