874ebb266a42f5866b3ec8a8aa88eec166077107
Le Vivilet <levivlet@pm.me>
2025-04-26 15:52:45 +0200

    lint

--- packages/explorer-view/src/parts/RemoveDirent/RemoveDirent.ts
+++ packages/explorer-view/src/parts/RemoveDirent/RemoveDirent.ts
@@ -1,11 +1,13 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as GetFocusedDirent from '../GetFocusedDirent/GetFocusedDirent.ts'
+import { getPaths } from '../GetPaths/GetPaths.ts'
 import * as Refresh from '../Refresh/Refresh.ts'
 import * as RemovePaths from '../RemovePaths/RemovePaths.ts'
 
 // TODO support multiselection and removing multiple dirents
 export const removeDirent = async (state: ExplorerState): Promise<ExplorerState> => {
-  if (state.focusedIndex < 0) {
+  const { items, focusedIndex } = state
+  if (focusedIndex < 0) {
     return state
   }
   const dirent = GetFocusedDirent.getFocusedDirent(state)
@@ -14,12 +16,12 @@ export const removeDirent = async (state: ExplorerState): Promise<ExplorerState>
   }
 
   // Get all selected items including the focused one
-  const selectedItems = state.items.filter((item) => item.selected || item === dirent)
+  const selectedItems = items.filter((item) => item.selected || item === dirent)
   if (selectedItems.length === 0) {
     return state
   }
 
-  const toRemove = selectedItems.map((item) => item.path)
+  const toRemove = getPaths(selectedItems)
   await RemovePaths.removePaths(toRemove)
   const newState = await Refresh.refresh(state)
   return newState
