973b70a3adcf24783fb111c855c26ad8a3ff7160
Le Vivilet <levivlet@pm.me>
2025-04-26 14:29:54 +0200

    feature: simplify refresh function

--- packages/explorer-view/src/parts/RemoveDirent/RemoveDirent.ts
+++ packages/explorer-view/src/parts/RemoveDirent/RemoveDirent.ts
@@ -1,7 +1,7 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import * as FileSystem from '../FileSystem/FileSystem.ts'
-import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 import * as GetFocusedDirent from '../GetFocusedDirent/GetFocusedDirent.ts'
+import * as Refresh from '../Refresh/Refresh.ts'
+import * as RemovePaths from '../RemovePaths/RemovePaths.ts'
 
 // TODO support multiselection and removing multiple dirents
 export const removeDirent = async (state: ExplorerState): Promise<ExplorerState> => {
@@ -19,45 +19,7 @@ export const removeDirent = async (state: ExplorerState): Promise<ExplorerState>
     return state
   }
 
-  // Remove all selected items from the filesystem
-  for (const item of selectedItems) {
-    try {
-      await FileSystem.remove(item.path)
-    } catch {
-      // TODO handle error
-      return state
-    }
-  }
-
-  // TODO avoid state mutation
-  // @ts-ignore
-  const newVersion = ++state.version
-  // TODO race condition
-  // const newState = await loadContent(state:any)
-  // @ts-ignore
-  if (state.version !== newVersion || state.disposed) {
-    return state
-  }
-
-  // Remove all selected items from the state
-  const newDirents = state.items.filter((item) => !selectedItems.includes(item))
-
-  // Find the new focus index
-  let indexToFocus = -1
-  if (newDirents.length === 0) {
-    indexToFocus = -1
-  } else {
-    const lastSelectedIndex = Math.max(...selectedItems.map((item) => state.items.indexOf(item)))
-    indexToFocus = Math.min(lastSelectedIndex, newDirents.length - 1)
-  }
-
-  const visible = newDirents.slice(state.minLineY, state.maxLineY)
-  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, state.fileIconCache)
-  return {
-    ...state,
-    items: newDirents,
-    icons,
-    fileIconCache: newFileIconCache,
-    focusedIndex: indexToFocus,
-  }
+  const toRemove = selectedItems.map((item) => item.path)
+  await RemovePaths.removePaths(toRemove)
+  return Refresh.refresh(state)
 }
---
+++ packages/explorer-view/src/parts/RemovePaths/RemovePaths.ts
@@ -0,0 +1,9 @@
+import * as FileSystem from '../FileSystem/FileSystem.ts'
+
+export const removePaths = async (paths: readonly string[]): Promise<void> => {
+  for (const item of paths) {
+    try {
+      await FileSystem.remove(item)
+    } catch {}
+  }
+}
