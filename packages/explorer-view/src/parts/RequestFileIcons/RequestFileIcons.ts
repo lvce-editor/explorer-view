2234188581231517fa955245f31aa1e3c7197859
Le Vivilet <levivlet@pm.me>
2025-05-09 22:23:51 +0200

    test

--- packages/explorer-view/src/parts/RequestFileIcons/RequestFileIcons.ts
+++ packages/explorer-view/src/parts/RequestFileIcons/RequestFileIcons.ts
@@ -3,6 +3,9 @@ import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 import { toSimpleIconRequest } from '../ToSimpleIconRequest/ToSimpleIconRequest.ts'
 
 export const requestFileIcons = async (requests: readonly IconRequest[]): Promise<readonly string[]> => {
+  if (requests.length === 0) {
+    return []
+  }
   const simpleRequests = requests.map(toSimpleIconRequest)
   const icons = await ParentRpc.invoke('IconTheme.getIcons', simpleRequests)
   return icons
