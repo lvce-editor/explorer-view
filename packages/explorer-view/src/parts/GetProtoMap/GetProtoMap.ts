3836d138f7b01bfd1e2bde26e94396498c494b93
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 00:21:17 +0200

    feature: improve internal map function (#683)
    * feature: improve internal map function
    
    * lint
    
    * lint

--- packages/explorer-view/src/parts/GetProtoMap/GetProtoMap.ts
+++ packages/explorer-view/src/parts/GetProtoMap/GetProtoMap.ts
@@ -2,6 +2,10 @@ import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { RawDirent } from '../RawDirent/RawDirent.ts'
 import { getProtoMapInternal } from '../GetProtoMapInternal/GetProtoMapInternal.ts'
 
-export const getProtoMap = (root: string, pathToDirents: Record<string, readonly RawDirent[]>): readonly ExplorerItem[] => {
-  return getProtoMapInternal(root, pathToDirents, 1)
+export const getProtoMap = (
+  root: string,
+  pathToDirents: Record<string, readonly RawDirent[]>,
+  expandedPaths: readonly string[],
+): readonly ExplorerItem[] => {
+  return getProtoMapInternal(root, pathToDirents, expandedPaths, 1)
 }
--- packages/explorer-view/src/parts/GetProtoMapInternal/GetProtoMapInternal.ts
+++ packages/explorer-view/src/parts/GetProtoMapInternal/GetProtoMapInternal.ts
@@ -1,7 +1,13 @@
 import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { RawDirent } from '../RawDirent/RawDirent.ts'
+import { restoreDirentType } from '../RestoreDirentType/RestoreDirentType.ts'
 
-export const getProtoMapInternal = (root: string, pathToDirents: Record<string, readonly RawDirent[]>, depth: number): readonly ExplorerItem[] => {
+export const getProtoMapInternal = (
+  root: string,
+  pathToDirents: Record<string, readonly RawDirent[]>,
+  expandedPaths: readonly string[],
+  depth: number,
+): readonly ExplorerItem[] => {
   if (!(root in pathToDirents)) {
     return []
   }
@@ -15,12 +21,12 @@ export const getProtoMapInternal = (root: string, pathToDirents: Record<string, 
       posInSet: i + 1,
       setSize: items.length,
       depth,
-      type: item.type,
+      type: restoreDirentType(item.type, path, expandedPaths),
       path,
       icon: '',
       selected: false,
     }
-    const children = getProtoMapInternal(path, pathToDirents, depth + 1)
+    const children = getProtoMapInternal(path, pathToDirents, expandedPaths, depth + 1)
     protoMap.push(displayDirent, ...children)
   }
   return protoMap
