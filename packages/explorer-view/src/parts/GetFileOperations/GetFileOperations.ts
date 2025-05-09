b444376067ee85581f9c1cbd7222c45c16dcb99e
Le Vivilet <levivlet@pm.me>
2025-05-10 00:34:52 +0200

    feature: add join2 helper function

--- packages/explorer-view/src/parts/GetFileOperations/GetFileOperations.ts
+++ packages/explorer-view/src/parts/GetFileOperations/GetFileOperations.ts
@@ -1,16 +1,17 @@
 import type { FileOperation } from '../FileOperation/FileOperation.ts'
+import { join2 } from '../Path/Path.ts'
 
 export const getFileOperations = (root: string, uploadTree: any): readonly FileOperation[] => {
   const operations: FileOperation[] = []
 
   const processTree = (tree: any, currentPath: string): void => {
     for (const [path, value] of Object.entries(tree)) {
-      const fullPath = currentPath ? `${currentPath}/${path}` : path
+      const fullPath = currentPath ? join2(currentPath, path) : path
       if (typeof value === 'object') {
-        operations.push({ type: 'createFolder', path: `${root}/${fullPath}`, text: '' })
+        operations.push({ type: 'createFolder', path: join2(root, fullPath), text: '' })
         processTree(value, fullPath)
       } else if (typeof value === 'string') {
-        operations.push({ type: 'createFile', path: `${root}/${fullPath}`, text: value })
+        operations.push({ type: 'createFile', path: join2(root, fullPath), text: value })
       }
     }
   }
