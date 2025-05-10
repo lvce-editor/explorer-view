8a0a1f81c529b1c00710e450e5414f28171ad536
Le Vivilet <levivlet@pm.me>
2025-05-10 19:49:04 +0200

    test

--- packages/explorer-view/test/AcceptCreateFile.test.ts
+++ packages/explorer-view/test/AcceptCreateFile.test.ts
@@ -5,22 +5,30 @@ import type { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
 import { acceptCreateFile } from '../src/parts/AcceptCreateFile/AcceptCreateFile.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { RendererWorker } from '../src/parts/RpcId/RpcId.ts'
+import * as DirentType from '../src/parts/DirentType/DirentType.ts'
 
 test('acceptCreateFile', async () => {
   const mockRpc = MockRpc.create({
     commandMap: {},
-    invoke: (method: string) => {
+    invoke: (method: string, ...params: readonly any[]) => {
       if (method === 'FileSystem.createFile') {
-        return Promise.resolve()
+        return
       }
       if (method === 'FileSystem.writeFile') {
-        return Promise.resolve()
+        return
       }
       if (method === 'IconTheme.getFolderIcon') {
-        return Promise.resolve('folder-icon')
+        return 'folder-icon'
       }
       if (method === 'IconTheme.getIcons') {
         return ['folder-icon']
+      }
+      if (method === 'FileSystem.readDirWithFileTypes') {
+        const path = params[0]
+        if (path === '/test') {
+          return [{ name: 'folder1', type: DirentType.Directory }]
+        }
+        return []
       }
       throw new Error(`unexpected method ${method}`)
     },
@@ -29,6 +37,7 @@ test('acceptCreateFile', async () => {
 
   const state: ExplorerState = {
     ...createDefaultState(),
+    root: '/test',
     editingValue: 'test.txt',
     editingIndex: 0,
     items: [
