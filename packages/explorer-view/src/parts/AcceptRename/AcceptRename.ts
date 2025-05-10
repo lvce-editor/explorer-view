6b1344fd04fa119ce155de1187b4581d2cd2cbbf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 14:13:28 +0200

    Discard changes to packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts

--- packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts
+++ packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts
@@ -1,35 +1,34 @@
 import { VError } from '@lvce-editor/verror'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import * as ComputeExplorerRenamedDirent from '../ComputeExplorerRenamedDirent/ComputeExplorerRenamedDirent.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
 import * as Path from '../Path/Path.ts'
-import { updateDirentsAtPath } from '../UpdateDirentsAtPath/UpdateDirentsAtPath.ts'
 
 export const acceptRename = async (state: ExplorerState): Promise<ExplorerState> => {
+  const { editingIndex, editingValue, items, pathSeparator } = state
+  const renamedDirent = items[editingIndex]
   try {
-    const { editingIndex, editingValue, items, pathSeparator, root } = state
-    const renamedDirent = items[editingIndex]
+    // TODO this does not work with rename of nested file
     const oldAbsolutePath = renamedDirent.path
     const oldParentPath = Path.dirname(pathSeparator, oldAbsolutePath)
-    const newAbsolutePath = [oldParentPath, editingValue].join(pathSeparator)
+    const newAbsolutePath = Path.join2(oldParentPath, editingValue)
     await FileSystem.rename(oldAbsolutePath, newAbsolutePath)
-    const newDirents = await FileSystem.readDirWithFileTypes(oldParentPath)
-    const newItems = updateDirentsAtPath(items, oldParentPath, root, newDirents)
-    const focusedIndex = newItems.findIndex((item) => item.path === newAbsolutePath)
-    return {
-      ...state,
-      editingIndex: -1,
-      editingValue: '',
-      editingType: ExplorerEditingType.None,
-      editingIcon: '',
-      focusedIndex,
-      focused: true,
-      focus: FocusId.List,
-      items: newItems,
-    }
   } catch (error) {
     console.error(new VError(error, `Failed to rename file`))
     return state
+  }
+  const { newDirents, focusedIndex } = ComputeExplorerRenamedDirent.computeExplorerRenamedDirent(items, editingIndex, editingValue)
+  return {
+    ...state,
+    editingIndex: -1,
+    editingValue: '',
+    editingType: ExplorerEditingType.None,
+    editingIcon: '',
+    focusedIndex,
+    focused: true,
+    focus: FocusId.List,
+    items: newDirents,
   }
 }
