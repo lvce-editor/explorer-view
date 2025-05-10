f85b69868a9a7d0b6eab14531c1f2dfed56fc404
Le Vivilet <levivlet@pm.me>
2025-05-10 11:17:48 +0200

    simplify code

--- packages/explorer-view/src/parts/CreateRenameMap/CreateRenameMap.ts
+++ packages/explorer-view/src/parts/CreateRenameMap/CreateRenameMap.ts
@@ -1,24 +1,12 @@
-import { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import * as Path from '../Path/Path.ts'
 
-export const createRenameMap = (
-  items: readonly ExplorerItem[],
-  parentPath: string,
-  newItems: readonly ExplorerItem[],
-): Record<string, readonly ExplorerItem[]> => {
-  const itemMap: Record<string, ExplorerItem[]> = Object.create(null)
+export const createTree = (items: readonly ExplorerItem[]) => {
+  const tree: Record<string, ExplorerItem[]> = Object.create(null)
   for (const item of items) {
     const dirname = Path.dirname('/', item.path)
-    if (dirname === parentPath) {
-      continue
-    }
-    itemMap[dirname] ||= []
-    itemMap[dirname].push(item)
+    tree[dirname] ||= []
+    tree[dirname].push(item)
   }
-  for (const item of newItems) {
-    const dirname = Path.dirname('/', item.path)
-    itemMap[dirname] ||= []
-    itemMap[dirname].push(item)
-  }
-  return itemMap
+  return tree
 }
