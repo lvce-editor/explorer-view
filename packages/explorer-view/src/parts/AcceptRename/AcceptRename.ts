7a4e4796ba8e9fe223d498b92a3a79b6b835737b
Le Vivilet <levivlet@pm.me>
2025-05-10 19:53:18 +0200

    fix

--- packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts
+++ packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts
@@ -1,34 +1,7 @@
-import { VError } from '@lvce-editor/verror'
-import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import * as FileSystem from '../FileSystem/FileSystem.ts'
-import * as FocusId from '../FocusId/FocusId.ts'
-import * as Path from '../Path/Path.ts'
-import { updateDirentsAtPath } from '../UpdateDirentsAtPath/UpdateDirentsAtPath.ts'
-
-export const acceptRename = async (state: ExplorerState): Promise<ExplorerState> => {
-  try {
-    const { editingIndex, editingValue, items, pathSeparator, root } = state
-    const renamedDirent = items[editingIndex]
-    const oldAbsolutePath = renamedDirent.path
-    const oldParentPath = Path.dirname(pathSeparator, oldAbsolutePath)
-    const newAbsolutePath = [oldParentPath, editingValue].join(pathSeparator)
-    await FileSystem.rename(oldAbsolutePath, newAbsolutePath)
-    const newDirents = await FileSystem.readDirWithFileTypes(oldParentPath)
-    const newItems = updateDirentsAtPath(items, root, newDirents, oldAbsolutePath, newAbsolutePath)
-    const focusedIndex = newItems.findIndex((item) => item.path === newAbsolutePath)
-    return {
-      ...state,
-      editingIndex: -1,
-      editingValue: '',
-      editingType: ExplorerEditingType.None,
-      editingIcon: '',
-      focusedIndex,
-      focused: true,
-      focus: FocusId.List,
-      items: newItems,
-    }
-  } catch (error) {
+import * as ComputeExplorerRenamedDirent from '../ComputeExplorerRenamedDirent/ComputeExplorerRenamedDirent.ts'
+import { createNewDirentsRename } from '../CreateNewDirentsRename/CreateNewDirentsRename.ts'
+import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
 
 export const acceptRename = async (state: ExplorerState): Promise<ExplorerState> => {
@@ -37,5 +10,17 @@ export const acceptRename = async (state: ExplorerState): Promise<ExplorerState>
   const successful = await createNewDirentsRename(renamedDirent, editingValue, pathSeparator)
   if (!successful) {
     return state
+  }
+  const { newDirents, focusedIndex } = ComputeExplorerRenamedDirent.computeExplorerRenamedDirent(items, editingIndex, editingValue)
+  return {
+    ...state,
+    editingIndex: -1,
+    editingValue: '',
+    editingType: ExplorerEditingType.None,
+    editingIcon: '',
+    focusedIndex,
+    focused: true,
+    focus: FocusId.List,
+    items: newDirents,
   }
 }
