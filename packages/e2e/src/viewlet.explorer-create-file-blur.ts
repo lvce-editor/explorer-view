75cec78193aaab4f227c68eea2a412a015f63386
Le Vivilet <levivlet@pm.me>
2025-05-10 10:01:14 +0200

    feature: accept edit on input blur

---
+++ packages/e2e/src/viewlet.explorer-create-file-blur.ts
@@ -0,0 +1,27 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-create-file-blur'
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Explorer, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await Explorer.newFile()
+
+  // assert
+  const inputBox = Locator('input')
+  await expect(inputBox).toBeVisible()
+  await expect(inputBox).toBeFocused()
+
+  // act
+  await inputBox.type('created.txt')
+  await Explorer.updateEditingValue('created.txt')
+  await Command.execute('Explorer.handleBlur')
+
+  // // assert
+  const newFile = Locator('text=created.txt')
+  await expect(newFile).toBeVisible()
+}
--- packages/explorer-view/test/HandleBlur.test.ts
+++ packages/explorer-view/test/HandleBlur.test.ts
@@ -1,19 +1,68 @@
-import { test, expect } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import type { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as DirentType from '../src/parts/DirentType/DirentType.ts'
 import * as ExplorerEditingType from '../src/parts/ExplorerEditingType/ExplorerEditingType.ts'
 import { handleBlur } from '../src/parts/HandleBlur/HandleBlur.ts'
+import * as RpcId from '../src/parts/RpcId/RpcId.ts'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
 
-test('handleBlur - when not editing, sets focused to false', () => {
+test('handleBlur - when not editing, sets focused to false', async () => {
   const state = createDefaultState()
-  const newState = handleBlur(state)
+  const newState = await handleBlur(state)
   expect(newState.focused).toBe(false)
 })
 
-test('handleBlur - when editing, keeps state unchanged', () => {
-  const state = {
+test('handleBlur - when editing, keeps state unchanged', async () => {
+  const invoke = jest.fn((method: string, ...params: readonly any[]): any => {
+    if (method === 'FileSystem.getPathSeparator') {
+      return '/'
+    }
+    if (method === 'FileSystem.writeFile') {
+      return
+    }
+    if (method === 'IconTheme.getFileIcon') {
+      return ''
+    }
+    if (method === 'IconTheme.getIcons') {
+      return Array(params[0].length).fill('')
+    }
+    throw new Error(`unexpected method ${method}`)
+  })
+
+  const mockRpc = MockRpc.create({
+    invoke,
+    commandMap: {},
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+  const state: ExplorerState = {
     ...createDefaultState(),
-    editingType: ExplorerEditingType.Rename,
+    editingType: ExplorerEditingType.CreateFile,
+    editingIndex: 0,
+    items: [
+      {
+        type: DirentType.File,
+        depth: 0,
+        name: '1',
+        path: '1',
+        selected: false,
+      },
+    ],
+    editingValue: 'created.txt',
   }
-  const newState = handleBlur(state)
-  expect(newState).toBe(state)
+  const newState = await handleBlur(state)
+  expect(newState).toEqual({
+    ...state,
+    editingIndex: -1,
+    editingType: 0,
+    fileIconCache: {
+      '1': '',
+      '1/created.txt': '',
+    },
+    focus: 1,
+    focusedIndex: 1,
+    icons: ['', ''],
+    maxLineY: 2,
+  })
 })
