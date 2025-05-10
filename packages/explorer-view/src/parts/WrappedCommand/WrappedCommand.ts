8d5495c023ca8095fe5a5cc1605913b343932277
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 09:52:16 +0200

    feature: remove unused code (#692)

--- packages/explorer-view/src/parts/WrappedCommand/WrappedCommand.ts
+++
@@ -1,9 +0,0 @@
-import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-
-export interface WrappedFn {
-  (uid: number, ...args: readonly any[]): Promise<void>
-}
-
-export interface Fn {
-  (state: ExplorerState, ...args: readonly any[]): ExplorerState | Promise<ExplorerState>
-}
