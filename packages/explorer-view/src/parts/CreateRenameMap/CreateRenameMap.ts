78f517f217f974f2a3465bb0c80f175ede37de50
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 11:19:24 +0200

    feature: add createRenameMap function (#708)
    * feature: add createRenameMap function
    
    * simplify code
    
    * type

---
+++ packages/explorer-view/src/parts/CreateRenameMap/CreateRenameMap.ts
@@ -0,0 +1,12 @@
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
+import * as Path from '../Path/Path.ts'
+
+export const createTree = (items: readonly ExplorerItem[]): Record<string, readonly ExplorerItem[]> => {
+  const tree: Record<string, ExplorerItem[]> = Object.create(null)
+  for (const item of items) {
+    const dirname = Path.dirname('/', item.path)
+    tree[dirname] ||= []
+    tree[dirname].push(item)
+  }
+  return tree
+}
