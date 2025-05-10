e7383d45a857cf72b035146d94ec837ad5d47a26
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 22:51:22 +0200

    feature: move pathpath children into separate file (#735)
    * feature: move pathpath children into separate file
    
    * tsc
    
    * type

--- packages/explorer-view/src/parts/RevealItemHidden/RevealItemHidden.ts
+++ packages/explorer-view/src/parts/RevealItemHidden/RevealItemHidden.ts
@@ -1,18 +1,11 @@
-import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import type { PathPart } from '../PathPart/PathPart.ts'
-import { getChildDirents } from '../GetChildDirents/GetChildDirents.ts'
 import { getIndex } from '../GetIndex/GetIndex.ts'
 import { getPathParts } from '../GetPathParts/GetPathParts.ts'
+import { getPathPartsChildren } from '../GetPathPartsChildren/GetPathPartsChildren.ts'
 import { getPathPartsToReveal } from '../GetPathPartsToReveal/GetPathPartsToReveal.ts'
 import { mergeVisibleWithHiddenItems } from '../MergeVisibleWithHiddenItems/MergeVisibleWithHiddenItems.ts'
 import { orderDirents } from '../OrderDirents/OrderDirents.ts'
 import { scrollInto } from '../ScrollInto/ScrollInto.ts'
-
-const getPathPartChildren = async (pathPart: PathPart): Promise<readonly ExplorerItem[]> => {
-  const children = await getChildDirents(pathPart.pathSeparator, pathPart)
-  return children
-}
 
 // TODO maybe just insert items into explorer and refresh whole explorer
 export const revealItemHidden = async (state: ExplorerState, uri: string): Promise<ExplorerState> => {
@@ -22,7 +15,7 @@ export const revealItemHidden = async (state: ExplorerState, uri: string): Promi
     return state
   }
   const pathPartsToReveal = getPathPartsToReveal(root, pathParts, items)
-  const pathPartsChildren = await Promise.all(pathPartsToReveal.map(getPathPartChildren))
+  const pathPartsChildren = await getPathPartsChildren(pathPartsToReveal)
   const pathPartsChildrenFlat = pathPartsChildren.flat()
   const orderedPathParts = orderDirents(pathPartsChildrenFlat)
   const mergedDirents = mergeVisibleWithHiddenItems(items, orderedPathParts)
