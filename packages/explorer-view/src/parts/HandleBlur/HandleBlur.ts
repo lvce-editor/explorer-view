ff4f97986ee2a006f68b0c44966d3d273d073c4f
Le Vivilet <levivlet@pm.me>
2025-05-10 10:18:16 +0200

    feature: call cancelEdit function when blurring input

--- packages/explorer-view/src/parts/HandleBlur/HandleBlur.ts
+++ packages/explorer-view/src/parts/HandleBlur/HandleBlur.ts
@@ -1,17 +1,20 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import { acceptEdit } from '../AcceptEdit/AcceptEdit.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
+import { cancelEditInternal } from '../CancelEditInternal/CancelEditInternal.ts'
 
 export const handleBlur = async (state: ExplorerState): Promise<ExplorerState> => {
   // TODO when blur event occurs because of context menu, focused index should stay the same
   // but focus outline should be removed
   const { editingType, editingErrorMessage, editingValue } = state
-
-  if (editingType === ExplorerEditingType.None || editingErrorMessage || !editingValue) {
+  if (editingType === ExplorerEditingType.None) {
     return {
       ...state,
       focused: false,
     }
+  }
+  if (editingErrorMessage || !editingValue) {
+    return cancelEditInternal(state, false)
   }
   return acceptEdit(state)
 }
