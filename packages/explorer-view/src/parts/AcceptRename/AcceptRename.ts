5ec279ebd27842c186d051eb57d56d6d0cec04fb
Le Vivilet <levivlet@pm.me>
2025-05-10 11:23:02 +0200

    simplify code

--- packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts
+++ packages/explorer-view/src/parts/AcceptRename/AcceptRename.ts
@@ -1,13 +1,10 @@
 import { VError } from '@lvce-editor/verror'
-import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
-import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import * as CompareDirent from '../CompareDirent/CompareDirent.ts'
-import { createTree } from '../CreateRenameMap/CreateRenameMap.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
+import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
 import * as Path from '../Path/Path.ts'
-import { treeToArray } from '../TreeToArray/TreeToArray.ts'
+import { updateDirentsAtPath } from '../UpdateDirentsAtPath/UpdateDirentsAtPath.ts'
 
 export const acceptRename = async (state: ExplorerState): Promise<ExplorerState> => {
   try {
@@ -16,39 +13,9 @@ export const acceptRename = async (state: ExplorerState): Promise<ExplorerState>
     const oldAbsolutePath = renamedDirent.path
     const oldParentPath = Path.dirname(pathSeparator, oldAbsolutePath)
     const newAbsolutePath = [oldParentPath, editingValue].join(pathSeparator)
-
-    // 1. Rename the file
     await FileSystem.rename(oldAbsolutePath, newAbsolutePath)
-
-    // 2. Read the parent directory to get updated dirents
-    const newDirents = await FileSystem.readDirWithFileTypes(oldParentPath)
-
-    // 3. Convert raw dirents to explorer items and sort them
-    const sortedDirents = newDirents
-      .map((dirent, index) => ({
-        name: dirent.name,
-        type: dirent.type,
-        path: [oldParentPath, dirent.name].join(pathSeparator),
-        depth: renamedDirent.depth,
-        selected: false,
-        posInSet: index + 1,
-        setSize: newDirents.length,
-        icon: '',
-      }))
-      .sort(CompareDirent.compareDirent)
-
-    // 4. Build a hashmap of items by path
-    const map = {
-      ...createTree(items),
-      [oldParentPath]: sortedDirents,
-    }
-
-    // 5. Build the final items array using DFS
-    const newItems = treeToArray(map, root)
-
-    // Find the index of the renamed item
+    const newItems = await updateDirentsAtPath(items, oldParentPath, root)
     const focusedIndex = newItems.findIndex((item) => item.path === newAbsolutePath)
-
     return {
       ...state,
       editingIndex: -1,
