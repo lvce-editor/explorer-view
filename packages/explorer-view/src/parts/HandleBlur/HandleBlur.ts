0098e432ce2413b7f6faca6cbe44ce05ad4563cb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 10:04:29 +0200

    feature: accept edit on input blur (#694)
    * feature: accept edit on input blur
    
    * feature: don't create file if input field is empty

--- packages/explorer-view/src/parts/HandleBlur/HandleBlur.ts
+++ packages/explorer-view/src/parts/HandleBlur/HandleBlur.ts
@@ -1,15 +1,17 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import { acceptEdit } from '../AcceptEdit/AcceptEdit.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 
-export const handleBlur = (state: ExplorerState): ExplorerState => {
+export const handleBlur = async (state: ExplorerState): Promise<ExplorerState> => {
   // TODO when blur event occurs because of context menu, focused index should stay the same
   // but focus outline should be removed
-  const { editingType } = state
-  if (editingType !== ExplorerEditingType.None) {
-    return state
+  const { editingType, editingErrorMessage, editingValue } = state
+
+  if (editingType === ExplorerEditingType.None || editingErrorMessage || !editingValue) {
+    return {
+      ...state,
+      focused: false,
+    }
   }
-  return {
-    ...state,
-    focused: false,
-  }
+  return acceptEdit(state)
 }
