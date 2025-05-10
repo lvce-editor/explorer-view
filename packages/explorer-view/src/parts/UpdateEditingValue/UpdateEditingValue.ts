72c7e091b525be262101dbe603abb860050fb839
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 10:10:17 +0200

    feature: use object destructuring (#697)

--- packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
+++ packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
@@ -3,7 +3,8 @@ import { getEditingIcon } from '../GetEditingIcon/GetEditingIcon.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 export const updateEditingValue = async (state: ExplorerState, value: string, inputSource: number = InputSource.User): Promise<ExplorerState> => {
-  const editingIcon = await getEditingIcon(state.editingType, value, state.items[state.editingIndex]?.type)
+  const { editingType, items, editingIndex } = state
+  const editingIcon = await getEditingIcon(editingType, value, items[editingIndex]?.type)
   return {
     ...state,
     editingValue: value,
