b47a2c713dd92f32a79042c33e35a94eb6155aed
Le Vivilet <levivlet@pm.me>
2025-05-10 18:53:46 +0200

    feature: move hasSymbolicLinks into separate file

--- packages/explorer-view/src/parts/GetChildDirents/GetChildDirents.ts
+++ packages/explorer-view/src/parts/GetChildDirents/GetChildDirents.ts
@@ -1,13 +1,10 @@
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { RawDirent } from '../RawDirent/RawDirent.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
-import * as IsSymbolicLink from '../IsSymbolicLink/IsSymbolicLink.ts'
+import { hasSymbolicLinks } from '../HasSymbolicLink/HasSymbolicLink.ts'
 import * as ResolveSymbolicLinks from '../ResolveSymbolicLinks/ResolveSymbolicLinks.ts'
 import * as ToDisplayDirents from '../ToDisplayDirents/ToDisplayDirents.ts'
-
-const hasSymbolicLinks = (rawDirents: readonly RawDirent[]): boolean => {
-  return rawDirents.some(IsSymbolicLink.isSymbolicLink)
-}
 
 export const getChildDirentsRaw = async (uri: string): Promise<readonly RawDirent[]> => {
   const rawDirents = await FileSystem.readDirWithFileTypes(uri)
@@ -18,7 +15,11 @@ export const getChildDirentsRaw = async (uri: string): Promise<readonly RawDiren
   return rawDirents
 }
 
-export const getChildDirents = async (pathSeparator: string, parentDirent: any, excluded: readonly string[] = []): Promise<any> => {
+export const getChildDirents = async (
+  pathSeparator: string,
+  parentDirent: any,
+  excluded: readonly string[] = [],
+): Promise<readonly ExplorerItem[]> => {
   Assert.string(pathSeparator)
   Assert.object(parentDirent)
   // TODO use event/actor based code instead, this is impossible to cancel right now
