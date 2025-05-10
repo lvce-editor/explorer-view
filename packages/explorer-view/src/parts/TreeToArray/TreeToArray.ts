4f2a9cdcd54cabd5cd478a118bb240086ce6644b
Le Vivilet <levivlet@pm.me>
2025-05-10 11:15:06 +0200

    test

--- packages/explorer-view/src/parts/TreeToArray/TreeToArray.ts
+++ packages/explorer-view/src/parts/TreeToArray/TreeToArray.ts
@@ -1,8 +1,23 @@
 import { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 
-export const treeToArray = (map: Record<string, readonly ExplorerItem[]>, root: string) => {
+export const treeToArray = (map: Record<string, readonly ExplorerItem[]>, root: string): readonly ExplorerItem[] => {
   const items: ExplorerItem[] = []
-  let children = map[root]
-  const itemMap: Record<string, ExplorerItem[]> = Object.create(null)
-  return itemMap
+  const processChildren = (path: string, depth: number) => {
+    const children = map[path]
+    if (!children) {
+      return
+    }
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i]
+      items.push({
+        ...child,
+        depth,
+        posInSet: i + 1,
+        setSize: children.length,
+      })
+      processChildren(child.path, depth + 1)
+    }
+  }
+  processChildren(root, 0)
+  return items
 }
