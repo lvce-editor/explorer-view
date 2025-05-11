c51fde57c928a96f87a9c5992f854c06356dfaee
Le Vivilet <levivlet@pm.me>
2025-05-11 09:48:49 +0200

    feature: use dynamic error message

--- packages/explorer-view/src/parts/GetErrorMessageDom/GetErrorMessageDom.ts
+++ packages/explorer-view/src/parts/GetErrorMessageDom/GetErrorMessageDom.ts
@@ -1,19 +1,20 @@
 import { type VirtualDomNode, text } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as Px from '../Px/Px.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getErrorMessageDom = (errorMessage: string): readonly VirtualDomNode[] => {
+export const getErrorMessageDom = (errorMessage: string, errorMessageTop: number): readonly VirtualDomNode[] => {
   if (!errorMessage) {
     return []
   }
 
   const heightString = Px.px(20)
-  const translateString = Px.position(0, 20)
+  const translateString = Px.position(0, errorMessageTop)
   return [
     {
       type: VirtualDomElements.Div,
-      className: MergeClassNames.mergeClassNames('ExplorerErrorMessage'),
+      className: MergeClassNames.mergeClassNames(ClassNames.ExplorerErrorMessage),
       childCount: 1,
       height: heightString,
       translate: translateString,
