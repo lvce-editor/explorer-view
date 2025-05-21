88d30e34efff3d994d65425b405e7922b5108af5
Le Vivilet <levivlet@pm.me>
2025-05-21 21:23:09 +0200

    feature: simplify code

--- packages/explorer-view/src/parts/ToDisplayDirent/ToDisplayDirent.ts
+++ packages/explorer-view/src/parts/ToDisplayDirent/ToDisplayDirent.ts
@@ -3,13 +3,13 @@ import type { RawDirent } from '../RawDirent/RawDirent.ts'
 import { join2 } from '../Path/Path.ts'
 
 // TODO figure out whether this uses too much memory (name,path -> redundant, depth could be computed on demand)
-export const toDisplayDirent = (parentDirent: ExplorerItem, rawDirent: RawDirent, index: number, length: number): ExplorerItem => {
-  const path = join2(parentDirent.path, rawDirent.name)
+export const toDisplayDirent = (parentPath: string, parentDepth: number, rawDirent: RawDirent, index: number, length: number): ExplorerItem => {
+  const path = join2(parentPath, rawDirent.name)
   return {
     name: rawDirent.name,
     posInSet: index + 1,
     setSize: length,
-    depth: parentDirent.depth + 1,
+    depth: parentDepth + 1,
     type: rawDirent.type,
     path, // TODO storing absolute path might be too costly, could also store relative path here
     icon: '',
