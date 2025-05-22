3e2d3e2241231f412726e3e4eed58e4bc2e62167
Le Vivilet <levivlet@pm.me>
2025-05-22 23:03:58 +0200

    use uid

--- packages/explorer-view/src/parts/RenderEditingSelection/RenderEditingSelection.ts
+++ packages/explorer-view/src/parts/RenderEditingSelection/RenderEditingSelection.ts
@@ -3,6 +3,6 @@ import type { Renderer } from '../Renderer/Renderer.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const renderEditingSelection: Renderer = (oldState: ExplorerState, newState: ExplorerState): any => {
-  const { editingSelectionStart, editingSelectionEnd } = newState
-  return ['Viewlet.setSelectionByName', InputName.ExplorerInput, editingSelectionStart, editingSelectionEnd]
+  const { editingSelectionStart, editingSelectionEnd, uid } = newState
+  return ['Viewlet.setSelectionByName', uid, InputName.ExplorerInput, editingSelectionStart, editingSelectionEnd]
 }
--- packages/explorer-view/test/RenderEditingSelection.test.ts
+++ packages/explorer-view/test/RenderEditingSelection.test.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@jest/globals'
+import type { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
-import { renderEditingSelection } from '../src/parts/RenderEditingSelection/RenderEditingSelection.ts'
-import { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
 import * as InputName from '../src/parts/InputName/InputName.ts'
+import { renderEditingSelection } from '../src/parts/RenderEditingSelection/RenderEditingSelection.ts'
 
 test('renderEditingSelection', () => {
   const oldState: ExplorerState = createDefaultState()
@@ -12,5 +12,5 @@ test('renderEditingSelection', () => {
     editingSelectionEnd: 6,
   }
   const result = renderEditingSelection(oldState, newState)
-  expect(result).toEqual(['Viewlet.setSelectionByName', InputName.ExplorerInput, 1, 6])
+  expect(result).toEqual(['Viewlet.setSelectionByName', newState.uid, InputName.ExplorerInput, 1, 6])
 })
