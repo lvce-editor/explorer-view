8a00c89561dd28cb750dfa5200620689eec412f0
Le Vivilet <levivlet@pm.me>
2025-05-22 23:17:57 +0200

    feature: validate editing value on type

--- packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
+++ packages/explorer-view/src/parts/UpdateEditingValue/UpdateEditingValue.ts
@@ -1,13 +1,16 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import { getEditingIcon } from '../GetEditingIcon/GetEditingIcon.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
+import * as ValidateFileName2 from '../ValidateFileName2/ValidateFileName2.ts'
 
 export const updateEditingValue = async (state: ExplorerState, value: string, inputSource: number = InputSource.User): Promise<ExplorerState> => {
   const { editingType, items, editingIndex } = state
   const editingIcon = await getEditingIcon(editingType, value, items[editingIndex]?.type)
+  const editingErrorMessage = ValidateFileName2.validateFileName2(value)
   return {
     ...state,
     editingValue: value,
     editingIcon,
+    editingErrorMessage,
   }
 }
