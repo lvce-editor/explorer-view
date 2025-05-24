d0d517f1e33b74e357feb007bc72a0284bd8219d
Le Vivilet <levivlet@pm.me>
2025-05-24 18:01:53 +0200

    feature: add create file when file is focused e2e test

---
+++ packages/e2e/src/viewlet.explorer-create-file-when-file-is-focused.ts
@@ -0,0 +1,32 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-create-file-when-file-is-focused'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Workspace, Explorer, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
+  await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
+  await FileSystem.writeFile(`${tmpDir}/file3.txt`, 'content 3')
+  await Workspace.setPath(tmpDir)
+  await Explorer.focusIndex(0)
+
+  // act
+  await Explorer.newFile()
+
+  // assert
+  const inputBox = Locator('input')
+  await expect(inputBox).toBeVisible()
+  await expect(inputBox).toBeFocused()
+
+  // act
+  await inputBox.type('created.txt')
+  await Explorer.updateEditingValue('created.txt')
+  await Explorer.acceptEdit()
+
+  // assert
+  const newFile = Locator('text=created.txt')
+  await expect(newFile).toBeVisible()
+}
