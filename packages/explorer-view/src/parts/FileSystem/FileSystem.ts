3590fcda101d14ce281da038154551c462341cf0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-24 13:56:00 +0200

    feature: add file system worker module (#791)
    * feature: add file system worker module
    
    * feature: add file system worker module
    
    * memory

--- packages/explorer-view/src/parts/FileSystem/FileSystem.ts
+++ packages/explorer-view/src/parts/FileSystem/FileSystem.ts
@@ -1,41 +1,41 @@
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.ts'
 
 export const remove = async (dirent: string): Promise<void> => {
-  return ParentRpc.invoke('FileSystem.remove', dirent)
+  return FileSystemWorker.invoke('FileSystem.remove', dirent)
 }
 
 export const readDirWithFileTypes = async (uri: string): Promise<readonly any[]> => {
-  return ParentRpc.invoke('FileSystem.readDirWithFileTypes', uri)
+  return FileSystemWorker.invoke('FileSystem.readDirWithFileTypes', uri)
 }
 
 export const getPathSeparator = async (root: string): Promise<string> => {
-  return ParentRpc.invoke('FileSystem.getPathSeparator', root)
+  return FileSystemWorker.invoke('FileSystem.getPathSeparator', root)
 }
 
 export const getRealPath = async (path: string): Promise<string> => {
-  return ParentRpc.invoke('FileSystem.getRealPath', path)
+  return FileSystemWorker.invoke('FileSystem.getRealPath', path)
 }
 
 export const stat = async (dirent: string): Promise<any> => {
-  return ParentRpc.invoke('FileSystem.stat', dirent)
+  return FileSystemWorker.invoke('FileSystem.stat', dirent)
 }
 
 export const createFile = async (uri: string): Promise<void> => {
-  return ParentRpc.invoke('FileSystem.writeFile', uri, '')
+  return FileSystemWorker.invoke('FileSystem.writeFile', uri, '')
 }
 
 export const writeFile = async (uri: string, content: string): Promise<void> => {
-  return ParentRpc.invoke('FileSystem.writeFile', uri, content)
+  return FileSystemWorker.invoke('FileSystem.writeFile', uri, content)
 }
 
 export const mkdir = async (uri: string): Promise<void> => {
-  return ParentRpc.invoke('FileSystem.mkdir', uri)
+  return FileSystemWorker.invoke('FileSystem.mkdir', uri)
 }
 
 export const rename = async (oldUri: string, newUri: string): Promise<void> => {
-  return ParentRpc.invoke('FileSystem.rename', oldUri, newUri)
+  return FileSystemWorker.invoke('FileSystem.rename', oldUri, newUri)
 }
 
 export const copy = async (oldUri: string, newUri: string): Promise<void> => {
-  return ParentRpc.invoke('FileSystem.copy', oldUri, newUri)
+  return FileSystemWorker.invoke('FileSystem.copy', oldUri, newUri)
 }
---
+++ packages/explorer-view/src/parts/FileSystemWorker/FileSystemWorker.ts
@@ -0,0 +1,9 @@
+import type { RendererWorkerApi } from '@lvce-editor/rpc-registry'
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+
+export const invoke = async <T extends keyof RendererWorkerApi>(
+  method: keyof RendererWorkerApi,
+  ...params: Parameters<RendererWorkerApi[T]>
+): Promise<Awaited<ReturnType<RendererWorkerApi[T]>>> => {
+  return ParentRpc.invoke(method, ...params)
+}
