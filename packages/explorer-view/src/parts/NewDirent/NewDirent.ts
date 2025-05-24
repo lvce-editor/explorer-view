e1e24eb8eef2b7b22547c77cb016db1af04868fb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-24 18:14:03 +0200

    fix: navigate to parent folder first when trying to create file and focused item is file (#798)
    * fix: navigate to parent folder first when trying to create file and focused item is file
    
    * test

--- packages/explorer-view/src/parts/NewDirent/NewDirent.ts
+++ packages/explorer-view/src/parts/NewDirent/NewDirent.ts
@@ -1,3 +1,4 @@
+import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
@@ -8,13 +9,28 @@ import * as GetNewDirentsForNewDirent from '../GetNewDirentsForNewDirent/GetNewD
 import * as GetNewDirentType from '../GetNewDirentType/GetNewDirentType.ts'
 import * as SetFocus from '../SetFocus/SetFocus.ts'
 
+const isFolder = (direntType: number): boolean => {
+  return direntType === DirentType.Directory || direntType === DirentType.DirectoryExpanded || direntType === DirentType.SymLinkFolder
+}
+
+const getFittingIndex = (dirents: readonly ExplorerItem[], startIndex: number): number => {
+  for (let i = startIndex; i >= 0; i--) {
+    const dirent = dirents[i]
+    if (isFolder(dirent.type)) {
+      return i
+    }
+  }
+  return -1
+}
+
 export const newDirent = async (state: ExplorerState, editingType: number): Promise<ExplorerState> => {
   // TODO make focus functional instead of side effect
   await SetFocus.setFocus(FocusKey.ExplorerEditBox)
   // TODO do it like vscode, select position between folders and files
   const { focusedIndex, items, minLineY, height, itemHeight, fileIconCache, root } = state
+  const index = getFittingIndex(items, focusedIndex)
   const direntType = GetNewDirentType.getNewDirentType(editingType)
-  const newDirents = await GetNewDirentsForNewDirent.getNewDirentsForNewDirent(items, focusedIndex, direntType, root)
+  const newDirents = await GetNewDirentsForNewDirent.getNewDirentsForNewDirent(items, index, direntType, root)
   const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
   const visible = newDirents.slice(minLineY, maxLineY)
   const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, fileIconCache)
