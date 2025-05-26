a9e93b3d96418c2ac909518ef0825bbec22bc15e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-26 18:54:27 +0200

    feature: add rename permission denied e2e test (#802)

--- packages/e2e/src/viewlet.explorer-rename-file-error-permission-denied.ts
+++ packages/e2e/src/viewlet.explorer-rename-file-error-permission-denied.ts
@@ -1,6 +1,8 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'viewlet.explorer-create-file-error-permission-denied'
+export const name = 'viewlet.explorer-rename-file-error-permission-denied'
+
+export const skip = 1
 
 export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Locator, Extension }) => {
   // arrange
@@ -9,23 +11,18 @@ export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Loca
 
   const prefix = 'extension-host://xyz://'
   await FileSystem.writeFile(`${prefix}/file1.txt`, 'content 1')
-  await FileSystem.writeFile(`${prefix}/file2.txt`, 'content 2')
-  await FileSystem.writeFile(`${prefix}/file3.txt`, 'content 3')
   await Workspace.setPath(`${prefix}/`)
+  await Explorer.focusIndex(0)
 
   // act
-  await Explorer.newFile()
+  await Explorer.rename()
+  await Explorer.updateEditingValue('file4.txt')
+  await Explorer.acceptEdit()
 
   // assert
   const inputBox = Locator('input')
   await expect(inputBox).toBeVisible()
   await expect(inputBox).toBeFocused()
-
-  // act
-  await Explorer.updateEditingValue('file4.txt')
-  await Explorer.acceptEdit()
-
-  // assert
   await expect(inputBox).toHaveClass('InputValidationError')
   const errorMessage = Locator('.ExplorerErrorMessage')
   await expect(errorMessage).toBeVisible()
