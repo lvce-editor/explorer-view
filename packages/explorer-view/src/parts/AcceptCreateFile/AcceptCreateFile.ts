f83fc0cf049a556b2d47b72b92ed1493101237bb
Le Vivilet <levivlet@pm.me>
2025-06-07 17:33:34 +0200

    lint

--- packages/explorer-view/src/parts/AcceptCreateFile/AcceptCreateFile.ts
+++ packages/explorer-view/src/parts/AcceptCreateFile/AcceptCreateFile.ts
@@ -1,6 +1,6 @@
+import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import { acceptCreate } from '../AcceptCreate/AcceptCreate.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
-import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
 export const acceptCreateFile = async (state: ExplorerState): Promise<ExplorerState> => {
   return acceptCreate(state, DirentType.File)
