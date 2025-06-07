c2837d3c1452fe2ff1fccd697674099a52575d85
Le Vivilet <levivlet@pm.me>
2025-06-07 17:37:21 +0200

    feature: move icon request into separate file

--- packages/explorer-view/src/parts/ToSimpleIconRequest/ToSimpleIconRequest.ts
+++ packages/explorer-view/src/parts/ToSimpleIconRequest/ToSimpleIconRequest.ts
@@ -1,22 +1,10 @@
 import type { IconRequest } from '../IconRequest/IconRequest.ts'
 import type { SimpleFileIconRequest } from '../RendererWorkerApi/RendererWorkerApi.ts'
-import * as DirentType from '../DirentType/DirentType.ts'
-
-const getSimpleType = (direntType: number): 1 | 2 => {
-  if (
-    direntType === DirentType.Directory ||
-    direntType === DirentType.DirectoryExpanded ||
-    direntType === DirentType.EditingDirectoryExpanded ||
-    direntType === DirentType.EditingFolder
-  ) {
-    return 2
-  }
-  return 1
-}
+import { getSimpleIconRequestType } from '../GetSimpleIconRequestType/GetSimpleIconRequestType.ts'
 
 export const toSimpleIconRequest = (request: IconRequest): SimpleFileIconRequest => {
   return {
     name: request.name,
-    type: getSimpleType(request.type),
+    type: getSimpleIconRequestType(request.type),
   }
 }
