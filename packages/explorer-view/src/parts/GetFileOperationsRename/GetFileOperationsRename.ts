d050402adfaf1dcaf9bc6aa6a7d1e4cd55582705
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-07 09:48:26 +0200

    feature: add function to create rename file operations (#819)

---
+++ packages/explorer-view/src/parts/GetFileOperationsRename/GetFileOperationsRename.ts
@@ -0,0 +1,17 @@
+import type { FileOperation } from '../FileOperation/FileOperation.ts'
+import * as FileOperationType from '../FileOperationType/FileOperationType.ts'
+import * as Path from '../Path/Path.ts'
+
+export const getFileOperationsRename = (oldAbsolutePath: string, newFileName: string): readonly FileOperation[] => {
+  const operations: FileOperation[] = []
+  const oldParentPath = Path.dirname2(oldAbsolutePath)
+  const newAbsolutePath = Path.join2(oldParentPath, newFileName)
+  operations.push({
+    type: FileOperationType.Rename,
+    path: newAbsolutePath,
+    from: oldAbsolutePath,
+    text: '',
+  })
+
+  return operations
+}
