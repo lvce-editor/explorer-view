170e0d9969bae84be800a5a9def9562db25c6544
Le Vivilet <levivlet@pm.me>
2025-06-07 17:35:01 +0200

    enum

--- packages/explorer-view/test/GetFileOperations.test.ts
+++ packages/explorer-view/test/GetFileOperations.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@jest/globals'
 import { getFileOperations } from '../src/parts/GetFileOperations/GetFileOperations.ts'
+import * as FileOperationType from '../src/parts/FileOperationType/FileOperationType.ts'
 
 test('getFileOperations - empty tree', () => {
   const root = '/test'
@@ -20,7 +21,7 @@ test('getFileOperations - single folder', () => {
   const uploadTree = {
     folder: {},
   }
-  expect(getFileOperations(root, uploadTree)).toEqual([{ type: 'createFolder', path: '/test/folder' }])
+  expect(getFileOperations(root, uploadTree)).toEqual([{ type: FileOperationType.CreateFolder, path: '/test/folder' }])
 })
 
 test('getFileOperations - nested structure', () => {
@@ -35,10 +36,10 @@ test('getFileOperations - nested structure', () => {
     'file3.txt': 'content3',
   }
   expect(getFileOperations(root, uploadTree)).toEqual([
-    { type: 'createFolder', path: '/test/folder1' },
-    { type: 'createFile', path: '/test/folder1/file1.txt', text: 'content1' },
-    { type: 'createFolder', path: '/test/folder1/subfolder' },
-    { type: 'createFile', path: '/test/folder1/subfolder/file2.txt', text: 'content2' },
-    { type: 'createFile', path: '/test/file3.txt', text: 'content3' },
+    { type: FileOperationType.CreateFolder, path: '/test/folder1' },
+    { type: FileOperationType.CreateFile, path: '/test/folder1/file1.txt', text: 'content1' },
+    { type: FileOperationType.CreateFolder, path: '/test/folder1/subfolder' },
+    { type: FileOperationType.CreateFile, path: '/test/folder1/subfolder/file2.txt', text: 'content2' },
+    { type: FileOperationType.CreateFile, path: '/test/file3.txt', text: 'content3' },
   ])
 })
