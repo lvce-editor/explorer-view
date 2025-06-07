3a1c7d7d5ca5bad4cdda61e34bb9074f796d8989
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-07 17:35:13 +0200

    feature: remove unused code (#824)
    * feature: remove unused code
    
    * remove unused code
    
    * lint

--- packages/explorer-view/src/parts/AcceptCreateFolder/AcceptCreateFolder.ts
+++ packages/explorer-view/src/parts/AcceptCreateFolder/AcceptCreateFolder.ts
@@ -1,8 +1,7 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import { acceptCreate } from '../AcceptCreate/AcceptCreate.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
-import * as FileSystem from '../FileSystem/FileSystem.ts'
 
 export const acceptCreateFolder = async (state: ExplorerState): Promise<ExplorerState> => {
-  return acceptCreate(state, DirentType.Directory, FileSystem.mkdir)
+  return acceptCreate(state, DirentType.Directory)
 }
