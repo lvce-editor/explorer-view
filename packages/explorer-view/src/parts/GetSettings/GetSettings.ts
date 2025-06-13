f13a0be4d015a6d70235f3f93925b1102f1db97c
Le Vivilet <levivlet@pm.me>
2025-06-13 18:29:01 +0200

    feature: rename module

--- packages/explorer-view/src/parts/GetSettings/GetSettings.ts
+++ packages/explorer-view/src/parts/GetSettings/GetSettings.ts
@@ -1,5 +1,5 @@
 import type { Settings } from '../Settings/Settings.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as ParentRpc from '../RendererWorker/RendererWorker.ts'
 
 export const getSettings = async (): Promise<Settings> => {
   const useChevronsRaw = await ParentRpc.invoke('Preferences.get', 'explorer.useChevrons')
