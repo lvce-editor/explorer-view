cca440200d0ee3c17284033ee79e271ba7acfe90
Le Vivilet <levivlet@pm.me>
2025-06-24 17:39:54 +0200

    test

--- packages/explorer-view/test/GetFileOperationsRename.test.ts
+++ packages/explorer-view/test/GetFileOperationsRename.test.ts
@@ -6,8 +6,11 @@ test('should return rename operation', () => {
   const ops = getFileOperationsRename('/folder/old.txt', 'new.txt')
   expect(ops).toHaveLength(1)
   expect(ops[0].type).toBe(FileOperationType.Rename)
-  if ('from' in ops[0]) {
-    expect((ops[0] as any).from).toBe('/folder/old.txt')
-  }
-  expect(ops[0].path).toContain('new.txt')
+  expect(ops).toEqual([
+    {
+      from: '/folder/old.txt',
+      path: '/folder/new.txt',
+      type: 4,
+    },
+  ])
 })
