0d07bd6e587730457d525e914da586c4dc365a2d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-25 12:52:16 +0200

    feature: use i18n string (#909)
    * feature: use i18n string
    
    * sort

--- packages/explorer-view/src/parts/ConfirmPaste/ConfirmPaste.ts
+++ packages/explorer-view/src/parts/ConfirmPaste/ConfirmPaste.ts
@@ -1,7 +1,8 @@
+import * as ExplorerStrings from '../ExplorerStrings/ExplorerStrings.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const confirmPaste = async (): Promise<boolean> => {
   // @ts-ignore
-  const result = await RendererWorker.invoke('Confirmprompt.prompt', 'Are you sure you want to paste these files?')
+  const result = await RendererWorker.invoke('Confirmprompt.prompt', ExplorerStrings.pasteConfirmation())
   return result === true
 }
