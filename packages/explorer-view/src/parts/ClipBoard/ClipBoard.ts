1a7bb6c583a031a57a5419d7336b14a7066947e5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-25 17:14:25 +0200

    feature: add nativeFilesResult type (#912)

--- packages/explorer-view/src/parts/ClipBoard/ClipBoard.ts
+++ packages/explorer-view/src/parts/ClipBoard/ClipBoard.ts
@@ -1,10 +1,12 @@
+import type { NativeFilesResult } from '../NativeFilesResult/NativeFilesResult.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const writeText = async (text: string): Promise<void> => {
   await RendererWorker.invoke('ClipBoard.writeText', /* text */ text)
 }
 
-export const readNativeFiles = async (): Promise<any> => {
+export const readNativeFiles = async (): Promise<NativeFilesResult> => {
+  // @ts-ignore
   return RendererWorker.invoke('ClipBoard.readNativeFiles')
 }
 
--- packages/explorer-view/src/parts/HandlePasteCopy/HandlePasteCopy.ts
+++ packages/explorer-view/src/parts/HandlePasteCopy/HandlePasteCopy.ts
@@ -1,15 +1,18 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import type { NativeFilesResult } from '../NativeFilesResult/NativeFilesResult.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as Path from '../Path/Path.ts'
 import { getBaseName } from '../Path/Path.ts'
 import { updateRoot } from '../UpdateRoot/UpdateRoot.ts'
 
-export const handlePasteCopy = async (state: ExplorerState, nativeFiles: any): Promise<ExplorerState> => {
+export const handlePasteCopy = async (state: ExplorerState, nativeFiles: NativeFilesResult): Promise<ExplorerState> => {
   // TODO handle pasting files into nested folder
   // TODO handle pasting files into symlink
   // TODO handle pasting files into broken symlink
   // TODO handle pasting files into hardlink
   // TODO what if folder is big and it takes a long time
+
+  // TODO use file operations and bulk edit
   for (const source of nativeFiles.files) {
     // @ts-ignore
     const target = Path.join(state.pathSeperator, state.root, getBaseName(state.pathSeparator, source))
