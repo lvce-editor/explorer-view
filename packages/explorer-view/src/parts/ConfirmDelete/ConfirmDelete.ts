e0d7fd70aff122a400abbabdd9db52d566da34e0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-25 12:21:09 +0200

    feature: add confirmDelete function (#898)
    * feature: add confirmDelete function
    
    * feature: add confirmDelete function
    
    * Discard changes to packages/explorer-view/test/RemoveDirent.test.ts
    
    * test

---
+++ packages/explorer-view/src/parts/ConfirmDelete/ConfirmDelete.ts
@@ -0,0 +1,9 @@
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+
+export const confirmDelete = async (paths: readonly string[]): Promise<boolean> => {
+  // TODO use i18n string
+  const message = paths.length === 1 ? `Are you sure you want to delete "${paths[0]}"?` : `Are you sure you want to delete ${paths.length} items?`
+  // @ts-ignore
+  const result = await RendererWorker.invoke('Confirmprompt.prompt', message)
+  return result === true
+}
