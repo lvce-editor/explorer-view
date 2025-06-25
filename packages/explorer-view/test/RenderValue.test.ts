8a8100d28fdc3eea1982b5fbcdab43b80f976f53
Le Vivilet <levivlet@pm.me>
2025-06-25 12:17:23 +0200

    test

--- packages/explorer-view/test/RenderValue.test.ts
+++ packages/explorer-view/test/RenderValue.test.ts
@@ -4,52 +4,11 @@ import * as FocusId from '../src/parts/FocusId/FocusId.ts'
 import * as InputName from '../src/parts/InputName/InputName.ts'
 import * as InputSource from '../src/parts/InputSource/InputSource.ts'
 import { renderValue } from '../src/parts/RenderValue/RenderValue.ts'
-
-const createMockState = (overrides: Partial<ExplorerState> = {}): ExplorerState => ({
-  uid: 1,
-  parentUid: 0,
-  root: '/test',
-  items: [],
-  focusedIndex: 0,
-  focused: false,
-  hoverIndex: -1,
-  x: 0,
-  y: 0,
-  width: 100,
-  height: 100,
-  deltaY: 0,
-  minLineY: 0,
-  maxLineY: 100,
-  pathSeparator: '/',
-  version: 1,
-  editingIndex: -1,
-  itemHeight: 20,
-  dropTargets: [],
-  excluded: [],
-  editingValue: '',
-  editingType: 0,
-  editingIcon: '',
-  editingErrorMessage: '',
-  editingSelectionStart: 0,
-  editingSelectionEnd: 0,
-  icons: [],
-  useChevrons: false,
-  fileIconCache: {} as any,
-  platform: 1,
-  focus: FocusId.None,
-  inputSource: InputSource.Script,
-  focusWord: '',
-  focusWordTimeout: 0,
-  finalDeltaY: 0,
-  scrollBarHeight: 0,
-  handleOffset: 0,
-  scrollBarActive: false,
-  ...overrides,
-})
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 
 test('should return empty array when inputSource is User', () => {
-  const oldState = createMockState()
-  const newState = createMockState({ inputSource: InputSource.User })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, inputSource: InputSource.User }
 
   const result = renderValue(oldState, newState)
 
@@ -57,11 +16,8 @@ test('should return empty array when inputSource is User', () => {
 })
 
 test('should return setValue command when focus is Input', () => {
-  const oldState = createMockState()
-  const newState = createMockState({
-    focus: FocusId.Input,
-    editingValue: 'test-value',
-  })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, focus: FocusId.Input, editingValue: 'test-value' }
 
   const result = renderValue(oldState, newState)
 
@@ -69,8 +25,8 @@ test('should return setValue command when focus is Input', () => {
 })
 
 test('should return empty array when focus is not Input', () => {
-  const oldState = createMockState()
-  const newState = createMockState({ focus: FocusId.List })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, focus: FocusId.List }
 
   const result = renderValue(oldState, newState)
 
@@ -78,8 +34,8 @@ test('should return empty array when focus is not Input', () => {
 })
 
 test('should return empty array when focus is None', () => {
-  const oldState = createMockState()
-  const newState = createMockState({ focus: FocusId.None })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, focus: FocusId.None }
 
   const result = renderValue(oldState, newState)
 
@@ -87,11 +43,8 @@ test('should return empty array when focus is None', () => {
 })
 
 test('should return empty array when inputSource is Script and focus is not Input', () => {
-  const oldState = createMockState()
-  const newState = createMockState({
-    inputSource: InputSource.Script,
-    focus: FocusId.List,
-  })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, inputSource: InputSource.Script, focus: FocusId.List }
 
   const result = renderValue(oldState, newState)
 
