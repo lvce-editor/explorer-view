d3ae60bdcf84d9ea404a9514a1068aed50d6c28a
Le Vivilet <levivlet@pm.me>
2025-06-25 17:36:35 +0200

    test

--- packages/explorer-view/src/parts/GetFileOperationsCopy/GetFileOperationsCopy.ts
+++ packages/explorer-view/src/parts/GetFileOperationsCopy/GetFileOperationsCopy.ts
@@ -2,11 +2,9 @@ import type { FileOperation } from '../FileOperation/FileOperation.ts'
 import * as FileOperationType from '../FileOperationType/FileOperationType.ts'
 import { generateUniqueName } from '../GenerateUniqueName/GenerateUniqueName.ts'
 import * as Path from '../Path/Path.ts'
-import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 
-export const getFileOperationsCopy = (root: string, dirents: readonly ExplorerItem[], files: readonly string[]): readonly FileOperation[] => {
+export const getFileOperationsCopy = (root: string, existingUris: readonly string[], files: readonly string[]): readonly FileOperation[] => {
   const operations: FileOperation[] = []
-  const existingUris = dirents.map((dirent) => dirent.path)
 
   for (const file of files) {
     const baseName = Path.getBaseName('/', file)
