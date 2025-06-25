d172d8d3105cdec29382cfdfae7fbf0df65ad66f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-25 12:23:41 +0200

    feature: add confirmdelete setting (#899)
    * feature: add confirmdelete setting
    
    * test
    
    * test
    
    * lint
    
    * lint

--- packages/explorer-view/test/RenderFocusContext.test.ts
+++ packages/explorer-view/test/RenderFocusContext.test.ts
@@ -1,54 +1,12 @@
-import { test, expect } from '@jest/globals'
-import type { ExplorerState } from '../src/parts/ExplorerState/ExplorerState.ts'
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as FocusId from '../src/parts/FocusId/FocusId.ts'
 import { renderFocus } from '../src/parts/RenderFocusContext/RenderFocusContext.ts'
 import * as WhenExpression from '../src/parts/WhenExpression/WhenExpression.ts'
 
-const createMockState = (overrides: Partial<ExplorerState> = {}): ExplorerState => ({
-  uid: 1,
-  parentUid: 0,
-  root: '/test',
-  items: [],
-  focusedIndex: 0,
-  focused: false,
-  hoverIndex: -1,
-  x: 0,
-  y: 0,
-  width: 100,
-  height: 100,
-  deltaY: 0,
-  minLineY: 0,
-  maxLineY: 100,
-  pathSeparator: '/',
-  version: 1,
-  editingIndex: -1,
-  itemHeight: 20,
-  dropTargets: [],
-  excluded: [],
-  editingValue: '',
-  editingType: 0,
-  editingIcon: '',
-  editingErrorMessage: '',
-  editingSelectionStart: 0,
-  editingSelectionEnd: 0,
-  icons: [],
-  useChevrons: false,
-  fileIconCache: {} as any,
-  platform: 1,
-  focus: FocusId.None,
-  inputSource: 1,
-  focusWord: '',
-  focusWordTimeout: 0,
-  finalDeltaY: 0,
-  scrollBarHeight: 0,
-  handleOffset: 0,
-  scrollBarActive: false,
-  ...overrides,
-})
-
 test('should return FocusExplorerEditBox when focus is Input', () => {
-  const oldState = createMockState()
-  const newState = createMockState({ focus: FocusId.Input })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, focus: FocusId.Input }
 
   const result = renderFocus(oldState, newState)
 
@@ -56,8 +14,8 @@ test('should return FocusExplorerEditBox when focus is Input', () => {
 })
 
 test('should return FocusExplorer when focus is List', () => {
-  const oldState = createMockState()
-  const newState = createMockState({ focus: FocusId.List })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, focus: FocusId.List }
 
   const result = renderFocus(oldState, newState)
 
@@ -65,8 +23,8 @@ test('should return FocusExplorer when focus is List', () => {
 })
 
 test('should return empty array when focus is None', () => {
-  const oldState = createMockState()
-  const newState = createMockState({ focus: FocusId.None })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, focus: FocusId.None }
 
   const result = renderFocus(oldState, newState)
 
@@ -74,8 +32,8 @@ test('should return empty array when focus is None', () => {
 })
 
 test('should return empty array for unknown focus value', () => {
-  const oldState = createMockState()
-  const newState = createMockState({ focus: 999 })
+  const oldState = createDefaultState()
+  const newState = { ...oldState, focus: 999 }
 
   const result = renderFocus(oldState, newState)
 
