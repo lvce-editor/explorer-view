0a63c783d7a742e6ef7a925c29c94c19dd174b97
Le Vivilet <levivlet@pm.me>
2025-06-27 23:04:24 +0200

    typo

--- packages/explorer-view/src/parts/PathPart/PathPart.ts
+++ packages/explorer-view/src/parts/PathPart/PathPart.ts
@@ -1,6 +1,6 @@
 export interface PathPart {
   readonly depth: number
-  readonly expande?: boolean
+  readonly expanded?: boolean
   readonly path: string
   readonly pathSeparator: string
   readonly root: string
