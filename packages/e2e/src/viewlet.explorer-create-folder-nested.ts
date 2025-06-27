243989d8142a185bd7d76f178ecfa0b67ab2203e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-27 23:15:39 +0200

    feature: mark folder as expanded when creating nested folder (#930)

--- packages/e2e/src/viewlet.explorer-create-folder-nested.ts
+++ packages/e2e/src/viewlet.explorer-create-folder-nested.ts
@@ -25,10 +25,13 @@ export const test: Test = async ({ FileSystem, Workspace, Explorer, Locator, exp
   // assert
   const file1 = Locator('.TreeItem').nth(0)
   await expect(file1).toHaveText('a')
+  await expect(file1).toHaveAttribute('aria-expanded', 'true')
   const file2 = Locator('.TreeItem').nth(1)
   await expect(file2).toHaveText('b')
+  await expect(file2).toHaveAttribute('aria-expanded', 'true')
   const file3 = Locator('.TreeItem').nth(2)
   await expect(file3).toHaveText('c')
+  await expect(file3).toHaveAttribute('aria-expanded', 'false')
   const file4 = Locator('.TreeItem').nth(3)
   await expect(file4).toHaveText('file1.txt')
 }
