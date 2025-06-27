0de6f15a337c1869e1ced88e96d359aeeee5fa5d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-27 22:54:22 +0200

    feature: add acceptCreate test (#925)

--- packages/explorer-view/test/AcceptCreate.test.ts
+++ packages/explorer-view/test/AcceptCreate.test.ts
@@ -23,7 +23,7 @@ test('acceptCreate - empty file name', async () => {
   })
 })
 
-test.skip('acceptCreate - successful file creation', async () => {
+test('acceptCreate - successful file creation', async () => {
   const invoke = jest.fn((method: string, ...params: readonly any[]): any => {
     if (method === 'FileSystem.getPathSeparator') {
       return '/'
@@ -35,7 +35,20 @@ test.skip('acceptCreate - successful file creation', async () => {
       return Array(params[0].length).fill('')
     }
     if (method === 'FileSystem.readDirWithFileTypes') {
-      return [{ name: 'test.txt', type: DirentType.File }]
+      const path = params[0]
+      if (path === 'memfs:///workspace') {
+        return [{ name: 'test', type: DirentType.Directory }]
+      }
+      if (path === 'memfs:///workspace/test') {
+        return [{ name: 'test.txt', type: DirentType.File }]
+      }
+      throw new Error(`unexpected file read ${path}`)
+    }
+    if (method === 'FileSystem.mkdir') {
+      return
+    }
+    if (method === 'FileSystem.writeFile') {
+      return
     }
     throw new Error(`unexpected method ${method}`)
   })
@@ -48,8 +61,29 @@ test.skip('acceptCreate - successful file creation', async () => {
 
   const state: ExplorerState = {
     ...createDefaultState(),
+    root: 'memfs:///workspace',
     editingValue: 'test.txt',
-    items: [{ name: 'test.txt', type: DirentType.File, path: '/test.txt', depth: 0, selected: false }],
+    focusedIndex: 0,
+    editingIndex: 1,
+    items: [
+      {
+        name: 'test',
+        type: DirentType.Directory,
+        path: 'memfs:///workspace/test',
+        depth: 0,
+        selected: false,
+      },
+      {
+        name: 'test.txt',
+        type: DirentType.EditingFile,
+        path: 'memfs:///workspace/test/test.txt',
+        depth: 1,
+        posInSet: 1,
+        setSize: 1,
+        icon: '',
+        selected: false,
+      },
+    ],
     minLineY: 0,
     height: 100,
     itemHeight: 20,
@@ -60,15 +94,36 @@ test.skip('acceptCreate - successful file creation', async () => {
   expect(result).toEqual({
     ...state,
     items: [
-      { name: 'test.txt', type: DirentType.File, path: '/test.txt', depth: 0, selected: false },
-      { name: 'test.txt', type: DirentType.File, path: '/test.txt/test.txt', depth: 1, posInSet: 1, setSize: 1, icon: '', selected: false },
+      {
+        name: 'test',
+        type: DirentType.Directory,
+        depth: 1,
+        selected: false,
+        icon: '',
+        path: 'memfs:///workspace/test',
+        posInSet: 1,
+        setSize: 1,
+      },
+      {
+        name: 'test.txt',
+        type: DirentType.File,
+        path: 'memfs:///workspace/test/test.txt',
+        depth: 2,
+        posInSet: 1,
+        setSize: 1,
+        icon: '',
+        selected: false,
+      },
     ],
     editingIndex: -1,
-    focusedIndex: -1,
+    focusedIndex: 1,
     editingType: ExplorerEditingType.None,
     maxLineY: 2,
     focus: FocusId.List,
-    icons: [],
-    fileIconCache: {},
+    icons: ['', ''],
+    fileIconCache: {
+      'memfs:///workspace/test': '',
+      'memfs:///workspace/test/test.txt': '',
+    },
   })
 })
