3828dd50a86b40c6d471ad96df798928a1fbb679
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-28 14:53:39 +0200

    feature: support cutting multiple files (#958)
    * feature: support cutting multiple files
    
    * lint

--- packages/explorer-view/src/parts/HandleCut/HandleCut.ts
+++ packages/explorer-view/src/parts/HandleCut/HandleCut.ts
@@ -1,18 +1,16 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as ClipBoard from '../ClipBoard/ClipBoard.ts'
-import * as GetFocusedDirent from '../GetFocusedDirent/GetFocusedDirent.ts'
+import { getSelectedItems } from '../GetSelectedItems/GetSelectedItems.ts'
 
 export const handleCut = async (state: ExplorerState): Promise<ExplorerState> => {
   // TODO handle multiple files
   // TODO if not file is selected, what happens?
-  const dirent = GetFocusedDirent.getFocusedDirent(state)
-  if (!dirent) {
-    console.error('[ViewletExplorer/handleCut] no dirent selected')
+  const { items, focusedIndex } = state
+  const dirents = getSelectedItems(items, focusedIndex)
+  if (dirents.length === 0) {
     return state
   }
-  const absolutePath = dirent.path
-  // TODO handle cut error gracefully
-  const files = [absolutePath]
+  const files = dirents.map((dirent) => dirent.path)
   await ClipBoard.writeNativeFiles('cut', files)
   return {
     ...state,
