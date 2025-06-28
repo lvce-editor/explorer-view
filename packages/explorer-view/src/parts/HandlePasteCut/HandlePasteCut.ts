acd17e17688a75f10c19c6a7e8a04e061ae03f69
Le Vivilet <levivlet@pm.me>
2025-06-28 11:31:00 +0200

    feature: paste into focused folder instead of root

--- packages/explorer-view/src/parts/HandlePasteCut/HandlePasteCut.ts
+++ packages/explorer-view/src/parts/HandlePasteCut/HandlePasteCut.ts
@@ -1,17 +1,42 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import type { NativeFilesResult } from '../NativeFilesResult/NativeFilesResult.ts'
 import * as AdjustScrollAfterPaste from '../AdjustScrollAfterPaste/AdjustScrollAfterPaste.ts'
+import * as FileOperationType from '../FileOperationType/FileOperationType.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
+import * as ApplyFileOperations from '../ApplyFileOperations/ApplyFileOperations.ts'
 import { getIndex } from '../GetIndex/GetIndex.ts'
-import { getBaseName } from '../Path/Path.ts'
+import { getBaseName, join2 } from '../Path/Path.ts'
 import { refresh } from '../Refresh/Refresh.ts'
+import { FileOperation } from '../FileOperation/FileOperation.ts'
+import { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
+
+const getOperations = (toUri: string, files: readonly string[]): readonly FileOperation[] => {
+  const operations: FileOperation[] = []
+  for (const file of files) {
+    const baseName = getBaseName('/', file)
+    const newUri = join2(toUri, baseName)
+    operations.push({
+      type: FileOperationType.Rename,
+      from: file,
+      path: newUri,
+    })
+  }
+  return operations
+}
+
+const getTargetUri = (root: string, items: readonly ExplorerItem[], index: number): string => {
+  if (index === -1) {
+    return root
+  }
+  return items[index].path
+}
 
 export const handlePasteCut = async (state: ExplorerState, nativeFiles: NativeFilesResult): Promise<ExplorerState> => {
-  const { root, pathSeparator } = state
-  for (const source of nativeFiles.files) {
-    const target = `${root}${pathSeparator}${getBaseName(pathSeparator, source)}`
-    await FileSystem.rename(source, target)
-  }
+  const { root, pathSeparator, items, focusedIndex } = state
+  // TODO root is not necessrily target uri
+  const targetUri = getTargetUri(root, items, focusedIndex)
+  const operations = getOperations(targetUri, nativeFiles.files)
+  await ApplyFileOperations.applyFileOperations(operations)
 
   // Refresh the state after cut operations
   const latestState = await refresh(state)
