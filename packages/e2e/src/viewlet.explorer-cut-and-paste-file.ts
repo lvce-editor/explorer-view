06ed13deb863e1807cee4b3e71eb2f589de9e901
Le Vivilet <levivlet@pm.me>
2025-06-28 11:29:07 +0200

    feature: improve cut test

--- packages/e2e/src/viewlet.explorer-cut-and-paste-file.ts
+++ packages/e2e/src/viewlet.explorer-cut-and-paste-file.ts
@@ -2,10 +2,9 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-cut-and-paste-file'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Locator, Command }) => {
   // arrange
+  await Command.execute('ClipBoard.enableMemoryClipBoard')
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.mkdir(`${tmpDir}/a`)
   await FileSystem.writeFile(`${tmpDir}/a/file.txt`, 'content')
