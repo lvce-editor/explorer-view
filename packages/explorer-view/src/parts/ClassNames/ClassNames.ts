eae9eee7eedea08b12af0a443fa5c99fedfabc1a
Le Vivilet <levivlet@pm.me>
2025-06-28 12:57:57 +0200

    feature: add cut label class

--- packages/explorer-view/src/parts/ClassNames/ClassNames.ts
+++ packages/explorer-view/src/parts/ClassNames/ClassNames.ts
@@ -1,3 +1,14 @@
+// 0 = 'Button'
+// 1 = 'IconButton'
+// 2 = 'Button IconButton'
+// it could make dom diffing faster, since for classname,
+// once at start, send all classnames to renderer process
+// only numbers are compared. it could also make rendering faster,
+// representing the concatenated strings for example
+// since less data is transferred to renderer process
+// then, components uses numeric classname
+// TODO add option to make classnames numeric
+// when a component uses multiple classnames, it is a new number
 export const Actions = 'Actions'
 export const Button = 'Button'
 export const ButtonNarrow = 'ButtonNarrow'
@@ -5,10 +16,10 @@ export const ButtonPrimary = 'ButtonPrimary'
 export const ButtonWide = 'ButtonWide'
 export const Chevron = 'Chevron'
 export const ContainContent = 'ContainContent'
-export const ExplorerErrorMessage = 'ExplorerErrorMessage'
 export const Empty = ''
 export const Explorer = 'Explorer'
 export const ExplorerDropTarget = 'DropTarget'
+export const ExplorerErrorMessage = 'ExplorerErrorMessage'
 export const ExplorerInputBox = 'ExplorerInputBox'
 export const FileIcon = 'FileIcon'
 export const FocusOutline = 'FocusOutline'
@@ -16,6 +27,7 @@ export const IconButton = 'IconButton'
 export const InputBox = 'InputBox'
 export const InputValidationError = 'InputValidationError'
 export const Label = 'Label'
+export const LabelCut = 'LabelCut'
 export const List = 'List'
 export const ListItems = 'ListItems'
 export const MaskIcon = 'MaskIcon'
@@ -33,26 +45,14 @@ export const QuickPickMaskIcon = 'QuickPickMaskIcon'
 export const QuickPickScrollbar = 'QuickPickScrollbar'
 export const QuickPickScrollbarSlider = 'QuickPickScrollbarSlider'
 export const QuickPickStatus = 'QuickPickStatus'
+export const Scrollbar = 'Scrollbar'
 export const ScrollBar = 'ScrollBar'
 export const ScrollBarSmall = 'ScrollBarSmall'
-export const ScrollBarThumb = 'ScrollBarThumb'
-export const Scrollbar = 'Scrollbar'
 export const ScrollbarThumb = 'ScrollbarThumb'
+export const ScrollBarThumb = 'ScrollBarThumb'
 export const ScrollbarTrack = 'ScrollbarTrack'
 export const TreeItem = 'TreeItem'
 export const TreeItemActive = 'TreeItemActive'
 export const Viewlet = 'Viewlet'
 export const Welcome = 'Welcome'
 export const WelcomeMessage = 'WelcomeMessage'
-
-// TODO add option to make classnames numeric
-// once at start, send all classnames to renderer process
-// then, components uses numeric classname
-// when a component uses multiple classnames, it is a new number
-// representing the concatenated strings for example
-// 0 = 'Button'
-// 1 = 'IconButton'
-// 2 = 'Button IconButton'
-// it could make dom diffing faster, since for classname,
-// only numbers are compared. it could also make rendering faster,
-// since less data is transferred to renderer process
