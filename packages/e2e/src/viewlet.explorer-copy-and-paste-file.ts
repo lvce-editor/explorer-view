c2ed962e82bb031a702bac97d38c2f61d749af4e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-30 22:50:51 +0200

    feature: enable copy e2e test (#961)

--- packages/e2e/src/viewlet.explorer-copy-and-paste-file.ts
+++ packages/e2e/src/viewlet.explorer-copy-and-paste-file.ts
@@ -2,10 +2,9 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-copy-and-paste-file'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Locator, Command }) => {
   // arrange
+  await Command.execute('ClipBoard.enableMemoryClipBoard')
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.mkdir(`${tmpDir}/a`)
   await FileSystem.writeFile(`${tmpDir}/a/file.txt`, 'content')
@@ -16,7 +15,7 @@ export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Loca
   await Explorer.focusIndex(1)
 
   // act
-  await Command.execute('Explorer.handleCut')
+  await Command.execute('Explorer.handleCopy')
   await Explorer.focusIndex(2)
   await Command.execute('Explorer.handlePaste')
 
