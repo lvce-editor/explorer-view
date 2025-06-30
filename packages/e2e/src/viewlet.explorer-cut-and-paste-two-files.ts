82e8894ee8f853d8bda83263a8bfbd6000fbe439
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-30 18:23:08 +0200

    feature: enable copy e2e test (#960)
    * feature: enable copy e2e test
    
    * Discard changes to packages/e2e/src/viewlet.explorer-copy-and-paste-file.ts

--- packages/e2e/src/viewlet.explorer-cut-and-paste-two-files.ts
+++ packages/e2e/src/viewlet.explorer-cut-and-paste-two-files.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-cut-and-paste-two-files'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Locator, Command }) => {
   // arrange
   await Command.execute('ClipBoard.enableMemoryClipBoard')
