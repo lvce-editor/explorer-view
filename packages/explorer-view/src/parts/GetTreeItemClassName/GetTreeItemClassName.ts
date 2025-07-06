bfeaf9144ce5e4a2be7e96d94c2d4ed733a3635d
Le Vivilet <levivlet@pm.me>
2025-07-06 13:05:35 +0200

    feature: add classname for dropping items

--- packages/explorer-view/src/parts/GetTreeItemClassName/GetTreeItemClassName.ts
+++ packages/explorer-view/src/parts/GetTreeItemClassName/GetTreeItemClassName.ts
@@ -1,15 +1,13 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
-const focused = MergeClassNames.mergeClassNames(ClassNames.TreeItem, ClassNames.TreeItemActive)
-const selected = MergeClassNames.mergeClassNames(ClassNames.TreeItem, ClassNames.TreeItemActive)
-
-export const getTreeItemClassName = (isSelected: boolean, isFocused: boolean): string => {
-  if (isFocused) {
-    return focused
+export const getTreeItemClassName = (isSelected: boolean, isFocused: boolean, isDropping: boolean): string => {
+  let className = ClassNames.TreeItem
+  if (isSelected || isFocused) {
+    className = MergeClassNames.mergeClassNames(className, ClassNames.TreeItemActive)
   }
-  if (isSelected) {
-    return selected
+  if (isDropping) {
+    className = MergeClassNames.mergeClassNames(className, 'DropTarget')
   }
-  return ClassNames.TreeItem
+  return className
 }
