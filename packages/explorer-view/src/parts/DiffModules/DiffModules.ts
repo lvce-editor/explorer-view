75d86c429f1190e20f34a2d7c4b7d8ff46dd6c94
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-06 12:32:03 +0200

    Discard changes to packages/explorer-view/src/parts/DiffModules/DiffModules.ts

--- packages/explorer-view/src/parts/DiffModules/DiffModules.ts
+++ packages/explorer-view/src/parts/DiffModules/DiffModules.ts
@@ -1,17 +1,9 @@
-import * as DiffDragData from '../DiffDragData/DiffDragData.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffSelection from '../DiffSelection/DiffSelection.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffItems.isEqual, DiffFocus.isEqual, DiffFocus.isEqual, DiffValue.isEqual, DiffSelection.isEqual, DiffDragData.isEqual]
+export const modules = [DiffItems.isEqual, DiffFocus.isEqual, DiffFocus.isEqual, DiffValue.isEqual, DiffSelection.isEqual]
 
-export const numbers = [
-  DiffItems.diffType,
-  DiffFocus.diffType,
-  DiffType.RenderFocusContext,
-  DiffValue.diffType,
-  DiffSelection.diffType,
-  DiffDragData.diffType,
-]
+export const numbers = [DiffItems.diffType, DiffFocus.diffType, DiffType.RenderFocusContext, DiffValue.diffType, DiffSelection.diffType]
