4dbc000e1ba5ef9506ec102a507a8da222cc16f3
Le Vivilet <levivlet@pm.me>
2025-07-06 16:08:08 +0200

    label

--- packages/explorer-view/src/parts/GetDragData/GetDragData.ts
+++ packages/explorer-view/src/parts/GetDragData/GetDragData.ts
@@ -1,11 +1,5 @@
 import type { DragDataItem } from '../DragDataItem/DragDataItem.ts'
-
-const getDragLabel = (urls: readonly string[]): string => {
-  if (urls.length === 1) {
-    return urls[0]
-  }
-  return `${urls.length}`
-}
+import { getDragLabel } from '../GetDragLabel/GetDragLabel.ts'
 
 const toUri = (path: string): string => {
   if (path.startsWith('file://')) {
