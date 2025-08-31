73b0b094b1bc254ef555f277e4632646c1a430e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-31 22:34:09 +0200

    feature: add paste folder e2e test (#1049)

---
+++ packages/e2e/src/viewlet.explorer-copy-and-paste-folder.ts
@@ -0,0 +1,33 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-copy-and-paste-folder'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Workspace, Explorer, ClipBoard }) => {
+  // arrange
+  await ClipBoard.enableMemoryClipBoard()
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.mkdir(`${tmpDir}/a`)
+  await FileSystem.mkdir(`${tmpDir}/b`)
+  await Workspace.setPath(tmpDir)
+  await Explorer.focusIndex(0)
+  await Explorer.expandRecursively()
+
+  // act
+  await Explorer.handleCopy()
+  await Explorer.focusIndex(2)
+  await Explorer.handlePaste()
+
+  // assert
+  // const file1 = Locator('.TreeItem').nth(0)
+  // await expect(file1).toHaveText('a')
+  // await expect(file1).toHaveAttribute('aria-expanded', 'true')
+  // const file2 = Locator('.TreeItem').nth(1)
+  // await expect(file2).toHaveText('file.txt')
+  // const file3 = Locator('.TreeItem').nth(2)
+  // await expect(file3).toHaveText('b')
+  // await expect(file3).toHaveAttribute('aria-expanded', 'false') // TODO should be true
+  // const file4 = Locator('.TreeItem').nth(3)
+  // await expect(file4).toHaveText('file.txt')
+}
