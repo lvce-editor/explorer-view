61a4f72adc7abc182cc36077801bc79ab658b8b0
Le Vivilet <levivlet@pm.me>
2025-08-31 08:49:25 +0200

    typo

--- packages/explorer-view/test/ConfirmPaste.test.ts
+++ packages/explorer-view/test/ConfirmPaste.test.ts
@@ -8,7 +8,7 @@ test('confirmPaste returns true when user confirms', async () => {
   const mockRpc = MockRpc.create({
     commandMap: {},
     invoke: (method: string) => {
-      if (method === 'confirmPrompt.prompt') {
+      if (method === 'ConfirmPrompt.prompt') {
         return true
       }
       throw new Error(`unexpected method ${method}`)
@@ -24,7 +24,7 @@ test('confirmPaste returns false when user cancels', async () => {
   const mockRpc = MockRpc.create({
     commandMap: {},
     invoke: (method: string) => {
-      if (method === 'confirmPrompt.prompt') {
+      if (method === 'ConfirmPrompt.prompt') {
         return false
       }
       throw new Error(`unexpected method ${method}`)
