02d37bfe90f235dd65ed9dfc8bd1037048097208
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-01 13:47:50 +0200

    feature: improve cut cancel function (#1056)

--- packages/e2e/src/viewlet.explorer-cut-cancel.ts
+++ packages/e2e/src/viewlet.explorer-cut-cancel.ts
@@ -4,7 +4,7 @@ export const name = 'viewlet.explorer-cut-cancel'
 
 export const skip = 1
 
-export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Locator, ClipBoard }) => {
+export const test: Test = async ({ Command, FileSystem, Workspace, Explorer, expect, Locator, ClipBoard }) => {
   // arrange
   await ClipBoard.enableMemoryClipBoard()
   const tmpDir = await FileSystem.getTmpDir()
@@ -22,21 +22,6 @@ export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Loca
   const treeItemLabel = treeItem.locator('.Label')
   await expect(treeItemLabel).toHaveClass('LabelCut')
 
-  // await Explorer.focusIndex(2)
-  // await Explorer.handlePaste()
-
-  // // TODO folder should expanded automatically
-  // await Explorer.focusIndex(1)
-  // await Explorer.expandRecursively()
-
-  // // assert
-  // const file1 = Locator('.TreeItem').nth(0)
-  // await expect(file1).toHaveText('a')
-  // await expect(file1).toHaveAttribute('aria-expanded', 'true')
-  // // TODO should be hidden
-  // const file2 = Locator('.TreeItem').nth(1)
-  // await expect(file2).toHaveText('b')
-  // await expect(file2).toHaveAttribute('aria-expanded', 'true')
-  // const file3 = Locator('.TreeItem').nth(2)
-  // await expect(file3).toHaveText('file.txt')
+  await Command.execute('Explorer.handleEscape')
+  await expect(treeItemLabel).toHaveJSProperty('className', 'Label')
 }
--- packages/explorer-view/src/parts/HandleEscape/HandleEscape.ts
+++ packages/explorer-view/src/parts/HandleEscape/HandleEscape.ts
@@ -1,8 +1,8 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
 export const handleEscape = async (state: ExplorerState): Promise<ExplorerState> => {
-  // TODO cancel cut highlights
   return {
     ...state,
+    cutItems: [],
   }
 }
