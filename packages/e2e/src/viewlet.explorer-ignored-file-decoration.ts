c2588d5b40243cfcaf2bb2f96e9928e11800d4ac
Le Vivilet <levivlet@pm.me>
2025-09-01 15:12:23 +0200

    remove unused

--- packages/e2e/src/viewlet.explorer-ignored-file-decoration.ts
+++ packages/e2e/src/viewlet.explorer-ignored-file-decoration.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-ignored-file-decoration'
 
-export const test: Test = async ({ FileSystem, Workspace, ClipBoard }) => {
+export const test: Test = async ({ FileSystem, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.mkdir(`${tmpDir}/a`)
