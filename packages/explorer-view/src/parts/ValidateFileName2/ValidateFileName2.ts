5d1fcb08039bccc7940561da76249800489ea09a
Le Vivilet <levivlet@pm.me>
2025-09-01 14:03:30 +0200

    validate

--- packages/explorer-view/src/parts/ValidateFileName2/ValidateFileName2.ts
+++ packages/explorer-view/src/parts/ValidateFileName2/ValidateFileName2.ts
@@ -1,12 +1,16 @@
 import * as ExplorerStrings from '../ExplorerStrings/ExplorerStrings.ts'
+import * as Character from '../Character/Character.ts'
 
 export const validateFileName2 = (name: string): string => {
   if (!name) {
     const editingErrorMessage = ExplorerStrings.fileOrFolderNameMustBeProvided()
     return editingErrorMessage
   }
-  if (name.startsWith('/')) {
+  if (name.startsWith(Character.Slash)) {
     return ExplorerStrings.fileCannotStartWithSlash()
+  }
+  if (name.startsWith(Character.BackSlash)) {
+    return ExplorerStrings.fileCannotStartWithBackSlash()
   }
   return ''
 }
