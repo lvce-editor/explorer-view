3f84e8707f809e2b58e6a56bdd014e634556f8ba
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-01 17:34:17 +0200

    feature: simplify more tests (#1069)
    * feature: simplify more tests
    
    * lint

--- packages/explorer-view/test/ConfirmPaste.test.ts
+++ packages/explorer-view/test/ConfirmPaste.test.ts
@@ -1,37 +1,23 @@
-import { test, expect } from '@jest/globals'
-import { MockRpc } from '@lvce-editor/rpc'
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { confirmPaste } from '../src/parts/ConfirmPaste/ConfirmPaste.ts'
-import * as RpcId from '../src/parts/RpcId/RpcId.ts'
-import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
 
 test('confirmPaste returns true when user confirms', async () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: (method: string) => {
-      if (method === 'ConfirmPrompt.prompt') {
-        return true
-      }
-      throw new Error(`unexpected method ${method}`)
+  RendererWorker.registerMockRpc({
+    'ConfirmPrompt.prompt'() {
+      return true
     },
   })
-  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
-
   const result = await confirmPaste()
   expect(result).toBe(true)
 })
 
 test('confirmPaste returns false when user cancels', async () => {
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: (method: string) => {
-      if (method === 'ConfirmPrompt.prompt') {
-        return false
-      }
-      throw new Error(`unexpected method ${method}`)
+  RendererWorker.registerMockRpc({
+    'ConfirmPrompt.prompt'() {
+      return false
     },
   })
-  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
-
   const result = await confirmPaste()
   expect(result).toBe(false)
 })
