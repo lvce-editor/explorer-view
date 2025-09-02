0dba6b68b2e0ec97fd71b5b04cab65d0f22991e3
Le Vivilet <levivlet@pm.me>
2025-09-02 15:01:54 +0200

    feature: improve delete file e2e test

--- packages/e2e/src/viewlet.explorer-delete-file-error.ts
+++ packages/e2e/src/viewlet.explorer-delete-file-error.ts
@@ -7,8 +7,10 @@ export const skip = 1
 // @ts-ignore
 export const test: Test = async ({ Dialog, Extension, FileSystem, Workspace, Explorer, Locator, expect }) => {
   // arrange
+  let _message: string = ''
   // @ts-ignore
-  await Dialog.mockConfirm(() => {
+  await Dialog.mockConfirm((message: string) => {
+    _message = message
     return true
   })
   const uri = new URL('../fixtures/sample.file-system-provider-delete-file-error', import.meta.url).toString()
@@ -18,12 +20,14 @@ export const test: Test = async ({ Dialog, Extension, FileSystem, Workspace, Exp
   await FileSystem.writeFile(`${prefix}/file2.txt`, 'content 2')
   await FileSystem.writeFile(`${prefix}/file3.txt`, 'content 3')
   await Workspace.setPath(prefix)
+  await Explorer.focusFirst()
 
   // act
-  await Explorer.focusFirst()
   await Explorer.removeDirent()
 
   // assert
-  const file1 = Locator('text=file1.txt')
-  await expect(file1).toBeHidden()
+  const expectedMessage = 'Error: Failed to execute file system provider: oops'
+  if (_message !== expectedMessage) {
+    throw new Error(`expected confirm message to be `)
+  }
 }
