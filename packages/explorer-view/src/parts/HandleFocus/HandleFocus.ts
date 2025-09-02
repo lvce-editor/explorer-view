3aebe494b3c85c3b740a6b04d54a2cdddac72313
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-02 11:21:56 +0200

    feature: update whenExpression imports (#1076)

--- packages/explorer-view/src/parts/HandleFocus/HandleFocus.ts
+++ packages/explorer-view/src/parts/HandleFocus/HandleFocus.ts
@@ -1,6 +1,6 @@
+import { WhenExpression } from '@lvce-editor/constants'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as SetFocus from '../SetFocus/SetFocus.ts'
-import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const handleFocus = async (state: ExplorerState): Promise<ExplorerState> => {
   await SetFocus.setFocus(WhenExpression.FocusExplorer)
