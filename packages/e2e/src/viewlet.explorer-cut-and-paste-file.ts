6a2a014ed5525610705ea6966af224c149acbe33
Le Vivilet <levivilet@pm.me>
2025-09-07 15:07:25 +0200

    feature: enable cut e2e test (#1097)

--- packages/e2e/src/viewlet.explorer-cut-and-paste-file.ts
+++ packages/e2e/src/viewlet.explorer-cut-and-paste-file.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-cut-and-paste-file'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Explorer, expect, Locator, ClipBoard }) => {
   // arrange
   await ClipBoard.enableMemoryClipBoard()
