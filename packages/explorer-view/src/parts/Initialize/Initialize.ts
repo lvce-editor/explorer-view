a1794661dc90e95a7921686c7e6292c89320ebed
Le Vivilet <levivilet@pm.me>
2025-09-10 23:50:04 +0200

    feature: add direct connection to icon theme worker

--- packages/explorer-view/src/parts/Initialize/Initialize.ts
+++ packages/explorer-view/src/parts/Initialize/Initialize.ts
@@ -1,5 +1,7 @@
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
+import { initializeIconThemeWorker } from '../InitializeIconThemeWorker/InitializeIconThemeWorker.ts'
 
 export const initialize = async (): Promise<void> => {
   await initializeFileSystemWorker()
+  await initializeIconThemeWorker()
 }
