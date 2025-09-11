3879a97ee7027c3657df0db69622403d4d63d475
Le Vivilet <levivilet@pm.me>
2025-09-11 11:04:09 +0200

    feature: enable explorer welcome e2e test (#1116)

--- packages/e2e/src/viewlet.explorer-empty-workspace.ts
+++ packages/e2e/src/viewlet.explorer-empty-workspace.ts
@@ -2,13 +2,17 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-empty-workspace'
 
-export const skip = 1
-
-export const test: Test = async ({ Workspace, Explorer }) => {
-  // arrange
-  await Workspace.setPath('')
-  await Explorer.selectIndices([0, 1])
-
+export const test: Test = async ({ Workspace, Locator, expect }) => {
   // act
-  await Explorer.handleBlur()
+  await Workspace.setPath('')
+
+  // assert
+  const welcome = Locator('.Explorer .Welcome')
+  await expect(welcome).toBeVisible()
+  const welcomeMessage = Locator('.Explorer .WelcomeMessage')
+  await expect(welcomeMessage).toBeVisible()
+  await expect(welcomeMessage).toHaveText('You have not yet opened a folder')
+  const openFolderButton = Locator('.Explorer .Button')
+  await expect(openFolderButton).toBeVisible()
+  await expect(openFolderButton).toHaveText('Open folder')
 }
