2eeb8cb79742eeb44792d4781024181ed7db406c
Le Vivilet <levivilet@pm.me>
2025-09-16 18:14:35 +0200

    simplify

--- packages/explorer-view/src/parts/HandleClickDirectory/HandleClickDirectory.ts
+++ packages/explorer-view/src/parts/HandleClickDirectory/HandleClickDirectory.ts
@@ -3,24 +3,9 @@ import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as GetChildDirents from '../GetChildDirents/GetChildDirents.ts'
-import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
-import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
-import * as GetVisibleExplorerItems from '../GetVisibleExplorerItems/GetVisibleExplorerItems.ts'
 import * as SetFocus from '../SetFocus/SetFocus.ts'
 
 export const handleClickDirectory = async (state: ExplorerState, dirent: ExplorerItem, index: number, keepFocus: boolean): Promise<ExplorerState> => {
-  const {
-    cutItems,
-    sourceControlIgnoredUris,
-    dropTargets,
-    editingErrorMessage,
-    editingIcon,
-    editingIndex,
-    editingType,
-    editingValue,
-    focusedIndex,
-    useChevrons,
-  } = state
   // @ts-ignore
   dirent.type = DirentType.DirectoryExpanding
   // TODO handle error
@@ -41,40 +26,14 @@ export const handleClickDirectory = async (state: ExplorerState, dirent: Explore
   dirent.type = DirentType.DirectoryExpanded
   // @ts-ignore
   dirent.icon = ''
-  const { height, itemHeight, minLineY } = state2
   // TODO when focused index has changed while expanding, don't update it
-  const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
-
-  const parts = newDirents.slice(minLineY, maxLineY)
-  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(parts, state.fileIconCache)
-
-  const visibleExplorerItems = GetVisibleExplorerItems.getVisibleExplorerItems(
-    newDirents,
-    minLineY,
-    maxLineY,
-    focusedIndex,
-    editingIndex,
-    editingType,
-    editingValue,
-    editingErrorMessage,
-    icons,
-    useChevrons,
-    dropTargets,
-    editingIcon,
-    cutItems,
-    sourceControlIgnoredUris,
-  )
 
   // TODO use functional focus rendering
   await SetFocus.setFocus(WhenExpression.FocusExplorer)
   return {
     ...state,
-    fileIconCache: newFileIconCache,
     focused: keepFocus,
     focusedIndex: newIndex,
-    icons,
     items: newDirents,
-    maxLineY,
-    visibleExplorerItems,
   }
 }
