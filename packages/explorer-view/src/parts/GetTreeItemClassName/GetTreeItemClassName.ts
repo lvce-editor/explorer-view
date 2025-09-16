9cd20ed3b16ad3d4d7c1d1471d8f80b63bf4612e
Le Vivilet <levivilet@pm.me>
2025-09-16 16:28:54 +0200

    feature: add indent class to tree items

--- packages/explorer-view/src/parts/GetTreeItemClassName/GetTreeItemClassName.ts
+++ packages/explorer-view/src/parts/GetTreeItemClassName/GetTreeItemClassName.ts
@@ -1,8 +1,9 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
-export const getTreeItemClassName = (isSelected: boolean, isFocused: boolean, isDropping: boolean): string => {
+export const getTreeItemClassName = (isSelected: boolean, isFocused: boolean, isDropping: boolean, useChevrons: boolean, depth: number): string => {
   let className = ClassNames.TreeItem
+  className = MergeClassNames.mergeClassNames(className, `Indent-${depth}`)
   if (isSelected || isFocused) {
     className = MergeClassNames.mergeClassNames(className, ClassNames.TreeItemActive)
   }
