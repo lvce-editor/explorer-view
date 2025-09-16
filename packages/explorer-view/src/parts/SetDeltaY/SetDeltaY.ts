0f31904a1d767809eeba23a36cb17870bc49b637
Le Vivilet <levivilet@pm.me>
2025-09-16 16:51:37 +0200

    feature: set visible explorer items in setDeltaY (#1152)

--- packages/explorer-view/src/parts/SetDeltaY/SetDeltaY.ts
+++ packages/explorer-view/src/parts/SetDeltaY/SetDeltaY.ts
@@ -1,8 +1,23 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
+import * as GetVisibleExplorerItems from '../GetVisibleExplorerItems/GetVisibleExplorerItems.ts'
 
 export const setDeltaY = async (state: ExplorerState, deltaY: number): Promise<ExplorerState> => {
-  const { itemHeight, height, items } = state
+  const {
+    itemHeight,
+    useChevrons,
+    height,
+    cutItems,
+    sourceControlIgnoredUris,
+    dropTargets,
+    editingErrorMessage,
+    editingIcon,
+    editingIndex,
+    editingType,
+    editingValue,
+    focusedIndex,
+    items,
+  } = state
   if (deltaY < 0) {
     deltaY = 0
   } else if (deltaY > items.length * itemHeight - height) {
@@ -15,12 +30,29 @@ export const setDeltaY = async (state: ExplorerState, deltaY: number): Promise<E
   const maxLineY = minLineY + Math.round(height / itemHeight)
   const visible = items.slice(minLineY, maxLineY)
   const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, state.fileIconCache)
+  const visibleExplorerItems = GetVisibleExplorerItems.getVisibleExplorerItems(
+    items,
+    minLineY,
+    maxLineY,
+    focusedIndex,
+    editingIndex,
+    editingType,
+    editingValue,
+    editingErrorMessage,
+    icons,
+    useChevrons,
+    dropTargets,
+    editingIcon,
+    cutItems,
+    sourceControlIgnoredUris,
+  )
   return {
     ...state,
     deltaY,
-    minLineY,
-    maxLineY,
-    icons,
     fileIconCache: newFileIconCache,
+    icons,
+    maxLineY,
+    minLineY,
+    visibleExplorerItems,
   }
 }
