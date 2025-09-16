ee380862a6f102fa55def7935898ecfc1ab8310d
Le Vivilet <levivilet@pm.me>
2025-09-16 16:17:38 +0200

    feature: render css indent

--- packages/explorer-view/src/parts/RenderCss/RenderCss.ts
+++ packages/explorer-view/src/parts/RenderCss/RenderCss.ts
@@ -1,10 +1,20 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import { getTreeItemIndent } from '../GetTreeItemIndent/GetTreeItemIndent.ts'
 
 export const renderCss = (oldState: ExplorerState, newState: ExplorerState): readonly any[] => {
-  const { scrollBarHeight, uid } = newState
-  const css = `.Explorer {
-  --ScrollBarThumbheight: ${scrollBarHeight}px;
-}`
+  const { scrollBarHeight, uid, maxIndent } = newState
+  const rules = [
+    `.Explorer {
+  --ScrollBarThumbHeight: ${scrollBarHeight}px;
+`,
+  ]
+  for (let i = 0; i < maxIndent; i++) {
+    const indent = getTreeItemIndent(i)
+    rules.push(`.Indent-${i} {
+  padding-left: ${indent}px;
+}`)
+  }
+  const css = rules.join('\n')
   return [ViewletCommand.SetCss, uid, css]
 }
