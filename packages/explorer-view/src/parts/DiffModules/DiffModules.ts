3714b150c014971219bf6590a0d96439a27d057f
Le Vivilet <levivilet@pm.me>
2025-09-16 16:26:11 +0200

    feature: add css renderer (#1143)

--- packages/explorer-view/src/parts/DiffModules/DiffModules.ts
+++ packages/explorer-view/src/parts/DiffModules/DiffModules.ts
@@ -1,3 +1,4 @@
+import * as DiffCss from '../DiffCss/DiffCss.ts'
 import * as DiffDragData from '../DiffDragData/DiffDragData.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
@@ -5,7 +6,15 @@ import * as DiffSelection from '../DiffSelection/DiffSelection.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffItems.isEqual, DiffFocus.isEqual, DiffFocus.isEqual, DiffValue.isEqual, DiffSelection.isEqual, DiffDragData.isEqual]
+export const modules = [
+  DiffItems.isEqual,
+  DiffFocus.isEqual,
+  DiffFocus.isEqual,
+  DiffValue.isEqual,
+  DiffSelection.isEqual,
+  DiffDragData.isEqual,
+  DiffCss.isEqual,
+]
 
 export const numbers = [
   DiffType.RenderItems,
@@ -14,4 +23,5 @@ export const numbers = [
   DiffType.RenderValue,
   DiffType.RenderSelection,
   DiffType.RenderDragData,
+  DiffType.RenderCss,
 ]
