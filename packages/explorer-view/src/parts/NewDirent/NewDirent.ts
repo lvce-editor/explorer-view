f448a13caefb12bc2f44dbf547021e96617ec649
Le Vivilet <levivilet@pm.me>
2025-09-16 18:15:27 +0200

    simplify

--- packages/explorer-view/src/parts/NewDirent/NewDirent.ts
+++ packages/explorer-view/src/parts/NewDirent/NewDirent.ts
@@ -1,13 +1,11 @@
+import * as DirentType from '../DirentType/DirentType.ts'
 import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import * as DirentType from '../DirentType/DirentType.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
 import * as FocusKey from '../FocusKey/FocusKey.ts'
 import * as GetExplorerMaxLineY from '../GetExplorerMaxLineY/GetExplorerMaxLineY.ts'
-import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 import * as GetNewDirentsForNewDirent from '../GetNewDirentsForNewDirent/GetNewDirentsForNewDirent.ts'
 import * as GetNewDirentType from '../GetNewDirentType/GetNewDirentType.ts'
-import * as GetVisibleExplorerItems from '../GetVisibleExplorerItems/GetVisibleExplorerItems.ts'
 import * as SetFocus from '../SetFocus/SetFocus.ts'
 
 const isFolder = (direntType: number): boolean => {
@@ -28,56 +26,20 @@ export const newDirent = async (state: ExplorerState, editingType: number): Prom
   // TODO make focus functional instead of side effect
   await SetFocus.setFocus(FocusKey.ExplorerEditBox)
   // TODO do it like vscode, select position between folders and files
-  const {
-    minLineY,
-    height,
-    itemHeight,
-    root,
-    fileIconCache,
-    cutItems,
-    sourceControlIgnoredUris,
-    dropTargets,
-    editingErrorMessage,
-    editingIcon,
-    editingValue,
-    focusedIndex,
-    items,
-    useChevrons,
-  } = state
+  const { minLineY, height, itemHeight, root, focusedIndex, items } = state
   const index = getFittingIndex(items, focusedIndex)
   const direntType = GetNewDirentType.getNewDirentType(editingType)
   const newDirents = await GetNewDirentsForNewDirent.getNewDirentsForNewDirent(items, index, direntType, root)
   const maxLineY = GetExplorerMaxLineY.getExplorerMaxLineY(minLineY, height, itemHeight, newDirents.length)
-  const visible = newDirents.slice(minLineY, maxLineY)
-  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, fileIconCache)
   const editingIndex = newDirents.findIndex((item) => item.type === DirentType.EditingFile || item.type === DirentType.EditingFolder)
-  const visibleExplorerItems = GetVisibleExplorerItems.getVisibleExplorerItems(
-    items,
-    minLineY,
-    maxLineY,
-    focusedIndex,
-    editingIndex,
-    editingType,
-    editingValue,
-    editingErrorMessage,
-    icons,
-    useChevrons,
-    dropTargets,
-    editingIcon,
-    cutItems,
-    sourceControlIgnoredUris,
-  )
   return {
     ...state,
     editingIndex,
     editingType,
     editingValue: '',
-    fileIconCache: newFileIconCache,
     focus: FocusId.Input,
     focusedIndex: editingIndex,
-    icons,
     items: newDirents,
     maxLineY,
-    visibleExplorerItems,
   }
 }
