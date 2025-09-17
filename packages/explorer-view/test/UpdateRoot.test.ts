56be55bab9eed7134e82f7d82130aca9dbd7561b
Le Vivilet <levivilet@pm.me>
2025-09-17 02:15:26 +0200

    simplify

--- packages/explorer-view/test/UpdateRoot.test.ts
+++ packages/explorer-view/test/UpdateRoot.test.ts
@@ -11,21 +11,16 @@ test('updateRoot should return same disposed state', async () => {
   expect(result).toBe(state)
 })
 
-const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
-  if (method === 'FileSystem.readDirWithFileTypes') {
-    return [
-      { name: 'file1', type: 'file' },
-      { name: 'dir1', type: 'directory' },
-    ]
-  }
-  throw new Error(`Unexpected method: ${method}`)
-}
-
 test('updateRoot should merge dirents correctly', async () => {
   const state = createDefaultState()
 
   const mockRpc = RendererWorker.registerMockRpc({
-    'FileSystem.readDirWithFileTypes': invoke.bind(undefined, 'FileSystem.readDirWithFileTypes'),
+    'FileSystem.readDirWithFileTypes'() {
+      return [
+        { name: 'file1', type: 'file' },
+        { name: 'dir1', type: 'directory' },
+      ]
+    },
   })
 
   const result = await updateRoot(state)
