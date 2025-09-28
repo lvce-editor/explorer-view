e5d8727899a2659adbbdafd928724f7d18e54006
Le Vivilet <levivilet@pm.me>
2025-09-28 23:21:41 +0200

    feature: add todo (#1241)

--- packages/explorer-view/src/parts/DiffCss/DiffCss.ts
+++ packages/explorer-view/src/parts/DiffCss/DiffCss.ts
@@ -1,9 +1,13 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
 export const isEqual = (oldState: ExplorerState, newState: ExplorerState): boolean => {
+  // TODO compute css more optimized
+  // maybe only when items change, and even then not
+  // always, but only when it affects the css
   return (
     oldState.scrollBarHeight === newState.scrollBarHeight &&
     oldState.scrollBarActive === newState.scrollBarActive &&
-    oldState.maxIndent === newState.maxIndent
+    oldState.maxIndent === newState.maxIndent &&
+    oldState.visibleExplorerItems === newState.visibleExplorerItems
   )
 }
