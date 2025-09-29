65368076face2da5ca4e66c119f3ff8884cbc896
Le Vivilet <levivilet@pm.me>
2025-09-29 15:22:00 +0200

    fix: lint (#1255)

--- packages/explorer-view/src/parts/GetParentEndIndex/GetParentEndIndex.ts
+++ packages/explorer-view/src/parts/GetParentEndIndex/GetParentEndIndex.ts
@@ -2,7 +2,7 @@ import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 
 export const getParentEndIndex = (dirents: readonly ExplorerItem[], index: number): number => {
   const dirent = dirents[index]
-  const depth = dirent.depth
+  const { depth } = dirent
   let endIndex = index + 1
   while (endIndex < dirents.length && dirents[endIndex].depth > depth) {
     endIndex++
