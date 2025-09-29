1b7200462c4c127dc85e522d21a2c4a8c4c664e3
Le Vivilet <levivilet@pm.me>
2025-09-29 17:42:05 +0200

    feature: simplify icon theme imports (#1257)

--- packages/explorer-view/src/parts/InitializeIconThemeWorker/InitializeIconThemeWorker.ts
+++ packages/explorer-view/src/parts/InitializeIconThemeWorker/InitializeIconThemeWorker.ts
@@ -1,5 +1,5 @@
+import { IconThemeWorker } from '@lvce-editor/rpc-registry'
 import { createIconThemeWorkerRpc } from '../CreateIconThemeWorkerRpc/CreateIconThemeWorkerRpc.ts'
-import * as IconThemeWorker from '../IconThemeWorker/IconThemeWorker.ts'
 
 export const initializeIconThemeWorker = async (): Promise<void> => {
   const rpc = await createIconThemeWorkerRpc()
