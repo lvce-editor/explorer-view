5ba262a028348be92f065b8ee68b79f6bf255145
Le Vivilet <levivilet@pm.me>
2025-10-18 08:58:05 +0200

    feature: add icon theme e2e test (#1265)

---
+++ packages/e2e/src/viewlet.explorer-handle-icon-theme-change.ts
@@ -0,0 +1,22 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-icon-theme.change'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Workspace, Extension, IconTheme, Locator, expect, BaseUrl }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.xyz`, 'test')
+  await FileSystem.mkdir(`${tmpDir}/test-folder`)
+  await Workspace.setPath(tmpDir)
+  await Extension.addWebExtension(new URL('../fixtures/sample.icon-theme', import.meta.url).toString())
+
+  // act
+  await IconTheme.setIconTheme('test-icon-theme')
+
+  // assert
+  const iconFile = Locator('.TreeItem[aria-label="test.xyz"] .FileIcon')
+  const baseUrl = BaseUrl.getBaseUrl()
+  await expect(iconFile).toHaveAttribute('src', `${baseUrl}packages/extension-host-worker-tests/fixtures/sample.icon-theme/icons/default_file.svg`)
+}
