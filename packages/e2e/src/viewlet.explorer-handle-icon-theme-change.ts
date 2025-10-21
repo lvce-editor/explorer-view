7629e966a5496f5e0e8f86528c5af692c0039e36
Le Vivilet <levivilet@pm.me>
2025-10-21 17:47:23 +0200

    feature: enable icon theme test

--- packages/e2e/src/viewlet.explorer-handle-icon-theme-change.ts
+++ packages/e2e/src/viewlet.explorer-handle-icon-theme-change.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-icon-theme.change'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Extension, IconTheme, Locator, expect, BaseUrl }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
