51be007e4b32583282bbc9221e69c60e051adc93
Le Vivilet <levivilet@pm.me>
2025-11-16 22:57:48 +0100

    feature: add hasProperty helper function

--- packages/explorer-view/src/parts/RestoreState/RestoreState.ts
+++ packages/explorer-view/src/parts/RestoreState/RestoreState.ts
@@ -1,20 +1,21 @@
+import { hasProperty } from '../HasProperty/HasProperty.ts'
 import type { RestoredState } from '../RestoredState/RestoredState.ts'
 
 const getSavedMinLineY = (savedState: unknown): number => {
-  if (savedState && typeof savedState === 'object' && 'minLineY' in savedState && typeof savedState.minLineY === 'number') {
+  if (hasProperty(savedState, 'minLineY') && typeof savedState.minLineY === 'number') {
     return savedState.minLineY
   }
   return 0
 }
 const getSavedDeltaY = (savedState: unknown): number => {
-  if (savedState && typeof savedState === 'object' && 'deltaY' in savedState && typeof savedState.deltaY === 'number') {
+  if (hasProperty(savedState, 'deltaY') && typeof savedState.deltaY === 'number') {
     return savedState.deltaY
   }
   return 0
 }
 
 const getSavedWorkspacePath = (savedState: unknown): string => {
-  if (savedState && typeof savedState === 'object' && 'workspacePath' in savedState && typeof savedState.workspacePath === 'string') {
+  if (hasProperty(savedState, 'workspacePath') && typeof savedState.workspacePath === 'string') {
     return savedState.workspacePath
   }
   return ''
