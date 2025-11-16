777815f6766b42303eb2f43d67a327ea60818aa5
Le Vivilet <levivilet@pm.me>
2025-11-16 23:02:29 +0100

    feature: simplify create (#1322)

--- packages/explorer-view/src/parts/Create2/Create2.ts
+++ packages/explorer-view/src/parts/Create2/Create2.ts
@@ -1,8 +1,4 @@
-import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
-import * as ExplorerStates from '../ExplorerStates/ExplorerStates.ts'
-import * as Height from '../Height/Height.ts'
-import * as PathSeparatorType from '../PathSeparatorType/PathSeparatorType.ts'
+import { create } from '../Create/Create.ts'
 
 // TODO parentUid might ot be needed
 export const create2 = (
@@ -16,56 +12,5 @@ export const create2 = (
   parentUid: any,
   platform: number = 0,
 ): void => {
-  const state: ExplorerState = {
-    uid,
-    parentUid,
-    root: '',
-    items: [],
-    focusedIndex: -1,
-    focused: false,
-    hoverIndex: -1,
-    x,
-    y,
-    width,
-    height,
-    deltaY: 0,
-    minLineY: 0,
-    maxLineY: 0,
-    pathSeparator: PathSeparatorType.Slash,
-    version: 0,
-    editingIndex: -1,
-    itemHeight: Height.ListItem,
-    dropTargets: [],
-    excluded: [],
-    editingValue: '',
-    editingType: ExplorerEditingType.None,
-    editingIcon: '',
-    fileIconCache: Object.create(null),
-    useChevrons: false,
-    confirmDelete: false,
-    icons: [],
-    platform,
-    focus: 0,
-    editingErrorMessage: '',
-    inputSource: 0,
-    editingSelectionEnd: 0,
-    editingSelectionStart: 0,
-    focusWord: '',
-    focusWordTimeout: 800,
-    finalDeltaY: 0,
-    handleOffset: 0,
-    scrollBarActive: false,
-    scrollBarHeight: 0,
-    confirmPaste: false,
-    pasteShouldMove: false,
-    cutItems: [],
-    isPointerDown: false,
-    pointerDownIndex: -1,
-    sourceControlIgnoredUris: [],
-    maxIndent: 0,
-    errorMessageLeft: 0,
-    errorMessageTop: 0,
-    visibleExplorerItems: [],
-  }
-  ExplorerStates.set(uid, state, state)
+  return create(uid, uri, x, y, width, height, args, parentUid, platform)
 }
