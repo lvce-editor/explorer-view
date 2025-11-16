f6551543d0d8f843250e45f79851973268f764f1
Le Vivilet <levivilet@pm.me>
2025-11-16 22:53:20 +0100

    feature: simplify css

--- packages/explorer-view/src/parts/GetCss/GetCss.ts
+++ packages/explorer-view/src/parts/GetCss/GetCss.ts
@@ -1,17 +1,18 @@
+const getIndentRule = (indent: number): string => {
+  return `.Indent-${indent} {
+  padding-left: ${indent}px;
+}`
+}
+
 export const getCss = (scrollBarHeight: number, uniqueIndents: readonly number[], errorMessageLeft: number, errorMessageTop: number): string => {
-  // TODO each visible item should have an indent property
   const rules = [
     `.Explorer {
   --ScrollBarThumbHeight: ${scrollBarHeight}px;
   --ErrorMessageTop: ${errorMessageTop}px;
   --ErrorMessageLeft: ${errorMessageLeft}px;
 }`,
+    ...uniqueIndents.map(getIndentRule),
   ]
-  for (const item of uniqueIndents) {
-    rules.push(`.Indent-${item} {
-  padding-left: ${item}px;
-}`)
-  }
   const css = rules.join('\n')
   return css
 }
