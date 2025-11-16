6a4ffae7fb77a4127092545d9b9b30fae7e6c5b1
Le Vivilet <levivilet@pm.me>
2025-11-16 23:15:19 +0100

    lint

--- packages/explorer-view/src/parts/GetMenuEntries2/GetMenuEntries2.ts
+++ packages/explorer-view/src/parts/GetMenuEntries2/GetMenuEntries2.ts
@@ -1,6 +1,6 @@
-import { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import { getMenuEntries } from '../GetMenuEntries/GetMenuEntries.ts'
+import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
+import { getMenuEntries } from '../GetMenuEntries/GetMenuEntries.ts'
 
 export const getMenuEntries2 = (state: ExplorerState): readonly MenuEntry[] => {
   return getMenuEntries(state)
