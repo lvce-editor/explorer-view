0bf170ddc225a6713f29e59accadfd2db0ba6d2d
Le Vivilet <levivilet@pm.me>
2025-11-20 13:15:26 +0100

    feature: render error message position via adoptedStylesheet instead of inline style (#1335)

--- packages/explorer-view/src/parts/GetErrorMessageDom/GetErrorMessageDom.ts
+++ packages/explorer-view/src/parts/GetErrorMessageDom/GetErrorMessageDom.ts
@@ -1,21 +1,18 @@
 import { type VirtualDomNode, text } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
-import * as Px from '../Px/Px.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getErrorMessageDom = (errorMessage: string, errorMessageLeft: number, errorMessageTop: number): readonly VirtualDomNode[] => {
+export const getErrorMessageDom = (errorMessage: string): readonly VirtualDomNode[] => {
   if (!errorMessage) {
     return []
   }
 
-  const translateString = Px.position(errorMessageLeft, errorMessageTop)
   return [
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.ExplorerErrorMessage),
       childCount: 1,
-      translate: translateString,
     },
     text(errorMessage),
   ]
