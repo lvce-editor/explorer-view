f07ad0d6fa612ef884196ba34fb54ac55987b4ed
Le Vivilet <levivilet@pm.me>
2025-11-22 17:41:06 +0100

    feature: pass scheme to getFileDecorations (#1349)

--- packages/explorer-view/src/parts/GetFileDecorations/GetFileDecorations.ts
+++ packages/explorer-view/src/parts/GetFileDecorations/GetFileDecorations.ts
@@ -12,13 +12,18 @@ const ensureUris = (maybeUris: readonly string[]): readonly string[] => {
   return uris
 }
 
-export const getFileDecorations = async (maybeUris: readonly string[]): Promise<readonly any[]> => {
+export const getFileDecorations = async (scheme: string, root: string, maybeUris: readonly string[]): Promise<readonly any[]> => {
   try {
+    const providerIds = await SourceControlWorker.invoke('SourceControl.getEnabledProviderIds', scheme, root)
+    if (providerIds.length === 0) {
+      return []
+    }
     // @ts-ignore
     const uris = ensureUris(maybeUris)
-    const decorations = await SourceControlWorker.invoke('SourceControl.getDecorations', uris)
+    const decorations = await SourceControlWorker.invoke('SourceControl.getFileDecorations', uris)
     return decorations
-  } catch {
+  } catch (error) {
+    console.error(error)
     return []
   }
 }
