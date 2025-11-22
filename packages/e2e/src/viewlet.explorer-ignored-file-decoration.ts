3060f408de114138c4d96dacf59e0a0b2b1413d3
Le Vivilet <levivilet@pm.me>
2025-11-22 23:50:52 +0100

    feature: update source control decoration e2e test

--- packages/e2e/src/viewlet.explorer-ignored-file-decoration.ts
+++ packages/e2e/src/viewlet.explorer-ignored-file-decoration.ts
@@ -4,11 +4,16 @@ export const name = 'viewlet.explorer-ignored-file-decoration'
 
 export const skip = 1
 
-export const test: Test = async ({ FileSystem, Workspace }) => {
+export const test: Test = async ({ FileSystem, Workspace, Extension, Settings }) => {
   // arrange
-  const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.mkdir(`${tmpDir}/a`)
-  await FileSystem.mkdir(`${tmpDir}/b`)
+  await Settings.update({
+    'explorer.sourceControlDecorations': true,
+  })
+  const uri = import.meta.resolve('../fixtures/sample.source-control-decoration')
+  await Extension.addWebExtension(uri)
+  const tmpDir = 'extension-host://xyz://'
+  await FileSystem.writeFile(`${tmpDir}/a`, '')
+  await FileSystem.writeFile(`${tmpDir}/b`, '')
   await FileSystem.writeFile(`${tmpDir}/.gitignore`, 'a')
 
   // act
