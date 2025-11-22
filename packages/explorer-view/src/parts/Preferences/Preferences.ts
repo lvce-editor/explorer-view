ba34b87ba6e64bbba5f2015b186acd515247366a
Le Vivilet <levivilet@pm.me>
2025-11-22 23:35:42 +0100

    feature: remove unused code

--- packages/explorer-view/src/parts/Preferences/Preferences.ts
+++
@@ -1,9 +0,0 @@
-export interface Prefrences {
-  readonly sourceControlDecorations: boolean
-}
-
-export const getPreferences = (): Prefrences => {
-  return {
-    sourceControlDecorations: false,
-  }
-}
