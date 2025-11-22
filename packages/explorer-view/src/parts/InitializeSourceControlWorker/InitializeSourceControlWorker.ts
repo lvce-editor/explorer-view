52eeae78a26bbed75bea70a26d4ca0ac1508e3dc
Le Vivilet <levivilet@pm.me>
2025-11-22 18:18:54 +0100

    feature: add option whether or not to enable file decorations

--- packages/explorer-view/src/parts/InitializeSourceControlWorker/InitializeSourceControlWorker.ts
+++ packages/explorer-view/src/parts/InitializeSourceControlWorker/InitializeSourceControlWorker.ts
@@ -1,8 +1,12 @@
 import { SourceControlWorker } from '@lvce-editor/rpc-registry'
+import { DecorationsEnabled } from '../Config/Config.ts'
 import { createSourceControlWorkerRpc } from '../CreateSourceControlWorkerRpc/CreateSourceControlWorkerWorkerRpc.ts'
 
 export const initializeSourceControlWorker = async (): Promise<void> => {
   try {
+    if (!DecorationsEnabled) {
+      return
+    }
     const rpc = await createSourceControlWorkerRpc()
     // TODO
     SourceControlWorker.set(rpc)
