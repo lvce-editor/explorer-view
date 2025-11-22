be4fca1b78fda165e5343439dc73936b139135db
Le Vivilet <levivilet@pm.me>
2025-11-22 18:21:17 +0100

    feature: add option whether or not to enable file decorations (#1351)

--- packages/explorer-view/src/parts/GetFileDecorations/GetFileDecorations.ts
+++ packages/explorer-view/src/parts/GetFileDecorations/GetFileDecorations.ts
@@ -1,4 +1,5 @@
 import { SourceControlWorker } from '@lvce-editor/rpc-registry'
+import { DecorationsEnabled } from '../Config/Config.ts'
 
 const ensureUris = (maybeUris: readonly string[]): readonly string[] => {
   const uris: string[] = []
@@ -14,13 +15,17 @@ const ensureUris = (maybeUris: readonly string[]): readonly string[] => {
 
 export const getFileDecorations = async (scheme: string, root: string, maybeUris: readonly string[]): Promise<readonly any[]> => {
   try {
+    if (!DecorationsEnabled) {
+      return []
+    }
     const providerIds = await SourceControlWorker.invoke('SourceControl.getEnabledProviderIds', scheme, root)
     if (providerIds.length === 0) {
       return []
     }
+    const providerId = providerIds[0]
     // @ts-ignore
     const uris = ensureUris(maybeUris)
-    const decorations = await SourceControlWorker.invoke('SourceControl.getFileDecorations', uris)
+    const decorations = await SourceControlWorker.invoke('SourceControl.getFileDecorations', providerId, uris)
     return decorations
   } catch (error) {
     console.error(error)
