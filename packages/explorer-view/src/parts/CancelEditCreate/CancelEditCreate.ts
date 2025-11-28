e8048eef5ade7984a775745f60faeaf93ac4a954
Le Vivilet <levivilet@pm.me>
2025-11-28 11:41:08 +0100

    fix: reset focused index when canceling edit

--- packages/explorer-view/src/parts/CancelEditCreate/CancelEditCreate.ts
+++ packages/explorer-view/src/parts/CancelEditCreate/CancelEditCreate.ts
@@ -1,15 +1,17 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
+import { getFocusedIndexCancel } from '../GetFocusedIndexCancel/GetFocusedIndexCancel.ts'
 import { isNormalItem } from '../IsNormalItem/IsNormalItem.ts'
 
 export const cancelEditCreate = async (state: ExplorerState, keepFocus: boolean): Promise<ExplorerState> => {
   const { editingIndex, items } = state
   const filteredItems = items.filter(isNormalItem)
+  const newFocusedIndex = getFocusedIndexCancel(items, editingIndex)
   return {
     ...state,
     items: filteredItems,
-    focusedIndex: editingIndex,
+    focusedIndex: newFocusedIndex,
     focused: keepFocus,
     editingIndex: -1,
     editingValue: '',
--- packages/explorer-view/src/parts/CancelEditRename/CancelEditRename.ts
+++ packages/explorer-view/src/parts/CancelEditRename/CancelEditRename.ts
@@ -1,15 +1,17 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as ExplorerEditingType from '../ExplorerEditingType/ExplorerEditingType.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
+import { getFocusedIndexCancel } from '../GetFocusedIndexCancel/GetFocusedIndexCancel.ts'
 import { getNewDirentsForCancelRename } from '../GetNewDirentsForCancelRename/GetNewDirentsForCancelRename.ts'
 
 export const cancelEditRename = (state: ExplorerState, keepFocus: boolean): ExplorerState => {
   const { editingIndex, items } = state
   const newItems = getNewDirentsForCancelRename(items, editingIndex)
+  const newFocusedIndex = getFocusedIndexCancel(items, editingIndex)
   return {
     ...state,
     items: newItems,
-    focusedIndex: editingIndex,
+    focusedIndex: newFocusedIndex,
     focused: keepFocus,
     editingIndex: -1,
     editingValue: '',
