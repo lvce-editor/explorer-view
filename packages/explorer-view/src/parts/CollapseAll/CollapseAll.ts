a51babf59f14ae5152873462f32b4fba0e0b621b
Le Vivilet <levivilet@pm.me>
2025-12-06 12:38:17 +0100

    feature: ensure focusedIndex doesn't become out of bounds when collapsing items

--- packages/explorer-view/src/parts/CollapseAll/CollapseAll.ts
+++ packages/explorer-view/src/parts/CollapseAll/CollapseAll.ts
@@ -1,12 +1,15 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import { getFocusedIndexCancel } from '../GetFocusedIndexCancel/GetFocusedIndexCancel.ts'
 import * as IsTopLevel from '../IsTopLevel/IsTopLevel.ts'
 import * as ToCollapsedDirent from '../ToCollapsedDirent/ToCollapsedDirent.ts'
 
 export const collapseAll = async (state: ExplorerState): Promise<ExplorerState> => {
-  const { items } = state
+  const { focusedIndex, items } = state
   const newDirents = items.filter(IsTopLevel.isTopLevel).map(ToCollapsedDirent.toCollapsedDirent)
+  const newFocusedIndex = getFocusedIndexCancel(newDirents, focusedIndex)
   return {
     ...state,
+    focusedIndex: newFocusedIndex,
     items: newDirents,
   }
 }
