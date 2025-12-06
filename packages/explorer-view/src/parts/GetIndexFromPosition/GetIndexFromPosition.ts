ce5576c7f6561a79e942cdfd169bcc89b7e9f277
Le Vivilet <levivilet@pm.me>
2025-12-06 14:40:18 +0100

    fix: adjust index by scroll position

--- packages/explorer-view/src/parts/GetIndexFromPosition/GetIndexFromPosition.ts
+++ packages/explorer-view/src/parts/GetIndexFromPosition/GetIndexFromPosition.ts
@@ -1,7 +1,7 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
 export const getIndexFromPosition = (state: ExplorerState, eventX: number, eventY: number): number => {
-  const { itemHeight, items, y } = state
+  const { itemHeight, items, minLineY, y } = state
   const index = Math.floor((eventY - y) / itemHeight)
   if (index < 0) {
     return 0
@@ -9,5 +9,5 @@ export const getIndexFromPosition = (state: ExplorerState, eventX: number, event
   if (index >= items.length) {
     return -1
   }
-  return index
+  return index + minLineY
 }
