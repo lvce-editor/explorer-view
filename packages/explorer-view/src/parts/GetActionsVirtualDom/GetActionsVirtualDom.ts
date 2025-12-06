f18a219b9a7c751ee8c9c0ad2d74829c553cc12c
Le Vivilet <levivilet@pm.me>
2025-12-06 14:32:07 +0100

    click

--- packages/explorer-view/src/parts/GetActionsVirtualDom/GetActionsVirtualDom.ts
+++ packages/explorer-view/src/parts/GetActionsVirtualDom/GetActionsVirtualDom.ts
@@ -2,6 +2,7 @@ import type { ViewletAction } from '../ViewletAction/ViewletAction.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetActionVirtualDom from '../GetActionVirtualDom/GetActionVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
@@ -10,6 +11,7 @@ export const getActionsVirtualDom = (actions: readonly ViewletAction[]): readonl
     {
       childCount: actions.length,
       className: ClassNames.Actions,
+      onClick: DomEventListenerFunctions.HandleButtonClick,
       role: AriaRoles.ToolBar,
       type: VirtualDomElements.Div,
     },
