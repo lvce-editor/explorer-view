7b88fc763197a014e2a26abf21004ebbef31e6a3
Le Vivilet <levivilet@pm.me>
2026-01-14 00:40:02 +0100

    feature: add more e2e tests (#1443)

--- packages/e2e/src/viewlet.explorer-context-menu-delete-file.ts
+++ packages/e2e/src/viewlet.explorer-context-menu-delete-file.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-context-menu-delete-file'
 
-export const skip = 1
-
 export const test: Test = async ({ ContextMenu, expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
