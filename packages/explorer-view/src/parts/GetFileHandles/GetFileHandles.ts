33009efc5d5ae8f3512243ffcbb961d9d4f5bdc4
Le Vivilet <levivilet@pm.me>
2026-01-14 21:58:15 +0100

    feature: use page object

--- packages/explorer-view/src/parts/GetFileHandles/GetFileHandles.ts
+++ packages/explorer-view/src/parts/GetFileHandles/GetFileHandles.ts
@@ -1,6 +1,6 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const getFileHandles = async (fileIds: readonly number[]): Promise<readonly FileSystemHandle[]> => {
-  const files = await RendererWorker.invoke('FileSystemHandle.getFileHandles', fileIds)
+  const files = await RendererWorker.getFileHandles(fileIds)
   return files
 }
