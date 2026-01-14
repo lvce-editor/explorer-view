8a75d1f3f86fc9add15699c4caba68235e3d3022
Le Vivilet <levivilet@pm.me>
2026-01-14 21:49:50 +0100

    feature: adjust whitespace (#1458)

--- packages/e2e/src/viewlet.explorer-drag-drop-error-scenarios.ts
+++ packages/e2e/src/viewlet.explorer-drag-drop-error-scenarios.ts
@@ -1,6 +1,7 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-drag-drop-error-scenarios'
+
 export const skip = 1
 
 export const test: Test = async ({ Command, expect, Explorer, FileSystem, Locator, Workspace }) => {
