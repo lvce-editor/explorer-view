8c606c6d26c1c198c996a6e7c955b3f9e429f52a
Le Vivilet <levivilet@pm.me>
2026-01-14 21:42:35 +0100

    feature: add mixed sorting e2e test

---
+++ packages/e2e/src/viewlet.explorer-sorting-mixed-alphanumeric-special.ts
@@ -0,0 +1,37 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-sorting-mixed-alphanumeric-special'
+
+export const skip = 1
+
+export const test: Test = async ({ expect, FileSystem, Locator, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/1file.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/a-file.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/b_file.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/file2.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/file@10.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/file#2.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/file$3.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/z-file.txt`, '')
+  await FileSystem.writeFile(`${tmpDir}/10file.txt`, '')
+
+  // act
+  await Workspace.setPath(tmpDir)
+
+  // assert
+  const treeItems = Locator('.TreeItem')
+  await expect(treeItems).toHaveCount(10)
+  await expect(treeItems.nth(0)).toHaveText('1file.txt')
+  await expect(treeItems.nth(1)).toHaveText('10file.txt')
+  await expect(treeItems.nth(2)).toHaveText('a-file.txt')
+  await expect(treeItems.nth(3)).toHaveText('b_file.txt')
+  await expect(treeItems.nth(4)).toHaveText('file1.txt')
+  await expect(treeItems.nth(5)).toHaveText('file2.txt')
+  await expect(treeItems.nth(6)).toHaveText('file#2.txt')
+  await expect(treeItems.nth(7)).toHaveText('file$3.txt')
+  await expect(treeItems.nth(8)).toHaveText('file@10.txt')
+  await expect(treeItems.nth(9)).toHaveText('z-file.txt')
+}
