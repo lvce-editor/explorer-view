f4b6755424b2b5d7e66a10f1e8a964bb5a12e5b8
Le Vivilet <levivilet@pm.me>
2026-01-14 21:45:09 +0100

    feature: add sorting emoji e2e test

--- packages/e2e/src/viewlet.explorer-sorting-emojis.ts
+++ packages/e2e/src/viewlet.explorer-sorting-emojis.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-sorting-emojis'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
