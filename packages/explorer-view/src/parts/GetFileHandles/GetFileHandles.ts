c4f32ccc4e817ba75788677fba04c51f7bf76b1e
Le Vivilet <levivilet@pm.me>
2026-01-15 11:13:50 +0100

    feature: don't call getFileHandles if array is empty (#1473)

--- packages/explorer-view/src/parts/GetFileHandles/GetFileHandles.ts
+++ packages/explorer-view/src/parts/GetFileHandles/GetFileHandles.ts
@@ -1,6 +1,9 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const getFileHandles = async (fileIds: readonly number[]): Promise<readonly FileSystemHandle[]> => {
+  if (fileIds.length === 0) {
+    return []
+  }
   const files = await RendererWorker.getFileHandles(fileIds)
   return files
 }
