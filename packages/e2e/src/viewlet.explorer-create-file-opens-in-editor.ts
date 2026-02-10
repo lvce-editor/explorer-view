b61588047b7d059b3246ba532f41a1a065a4c2bd
Le Vivilet <levivilet@pm.me>
2026-02-10 10:57:20 +0100

    skip

--- packages/e2e/src/viewlet.explorer-create-file-opens-in-editor.ts
+++ packages/e2e/src/viewlet.explorer-create-file-opens-in-editor.ts
@@ -2,12 +2,13 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-create-file-opens-in-editor'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, 'content 1')
   await FileSystem.writeFile(`${tmpDir}/file2.txt`, 'content 2')
-
   await Workspace.setPath(tmpDir)
 
   // act
