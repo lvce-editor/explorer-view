b7727579287dcae66a24b4f1764c8585d0bb91c9
Le Vivilet <levivilet@pm.me>
2026-02-10 14:32:29 +0100

    feature: refactor openUri function to use RendererWorker.openUri (#1520)

--- packages/explorer-view/src/parts/OpenUri/OpenUri.ts
+++ packages/explorer-view/src/parts/OpenUri/OpenUri.ts
@@ -1,5 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const openUri = async (uri: string, focus: boolean): Promise<void> => {
-  await RendererWorker.invoke(/* Main.openAbsolutePath */ 'Main.openUri', /* absolutePath */ uri, /* focus */ focus)
+  await RendererWorker.openUri(uri, /* focus */ focus)
 }
