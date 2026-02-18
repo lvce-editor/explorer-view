9a671cf3fb3e88fd1d6ce32c63a940113608c8e0
Le Vivilet <levivilet@pm.me>
2026-02-18 14:58:31 +0100

    refactor: avoid mutation in getNewDirentsForRename by using slice for item replacement (#1529)

--- packages/explorer-view/src/parts/GetNewDirentsForRename/GetNewDirentsForRename.ts
+++ packages/explorer-view/src/parts/GetNewDirentsForRename/GetNewDirentsForRename.ts
@@ -3,12 +3,13 @@ import * as GetEditingType from '../GetEditingType/GetEditingType.ts'
 
 export const getNewDirentsForRename = (items: readonly ExplorerItem[], focusedIndex: number): readonly ExplorerItem[] => {
   const item = items[focusedIndex]
-  const newItems = [...items]
   const editingType = GetEditingType.getEditingType(item.type)
-  // TODO avoid mutation
-  newItems[focusedIndex] = {
-    ...item,
-    type: editingType,
-  }
-  return newItems
+  return [
+    ...items.slice(0, focusedIndex),
+    {
+      ...item,
+      type: editingType,
+    },
+    ...items.slice(focusedIndex + 1),
+  ]
 }
