7dd91db93dcfb604826055f84dc634c98a2a9367
Le Vivilet <levivilet@pm.me>
2026-02-18 19:34:31 +0100

    feature: remove skip flag from explorer create file test

--- packages/e2e/src/viewlet.explorer-create-file-opens-in-editor.ts
+++ packages/e2e/src/viewlet.explorer-create-file-opens-in-editor.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-create-file-opens-in-editor'
 
-export const skip = 1
-
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
