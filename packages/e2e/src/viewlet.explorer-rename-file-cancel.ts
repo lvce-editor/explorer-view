69ac4c149443d8da9d6ea7b8fa23ec1c33217179
Le Vivilet <levivilet@pm.me>
2026-03-29 22:50:58 +0200

    e2e

--- packages/e2e/src/viewlet.explorer-rename-file-cancel.ts
+++ packages/e2e/src/viewlet.explorer-rename-file-cancel.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-rename-file-cancel'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
