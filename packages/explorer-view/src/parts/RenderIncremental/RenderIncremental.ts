8a5282519173d9d34edbc769b42de49905a7c162
Le Vivilet <levivilet@pm.me>
2026-03-29 22:19:36 +0200

    feature: update initial state handling in Create, LoadContent, and RenderIncremental

--- packages/explorer-view/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/explorer-view/src/parts/RenderIncremental/RenderIncremental.ts
@@ -6,6 +6,11 @@ import { renderItems } from '../RenderItems/RenderItems.ts'
 export const renderIncremental = (oldState: ExplorerState, newState: ExplorerState): any => {
   const oldDom = renderItems(oldState, oldState)[1]
   const newDom = renderItems(newState, newState)[1]
+  if (oldState.initial) {
+    return [ViewletCommand.SetDom2, newState.uid, newDom]
+  }
   const patches = diffTree(oldDom, newDom)
+
+  console.log({ patches })
   return [ViewletCommand.SetPatches, newState.uid, patches]
 }
