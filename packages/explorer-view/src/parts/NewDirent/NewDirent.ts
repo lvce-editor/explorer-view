8f426e0e3774973f55465d29b551da3e85a96a99
Le Vivilet <levivilet@pm.me>
2026-04-14 08:17:39 +0200

    feature: fix index clamping logic in getFocusedIndexCancel and add tests (#1593)

--- packages/explorer-view/src/parts/NewDirent/NewDirent.ts
+++ packages/explorer-view/src/parts/NewDirent/NewDirent.ts
@@ -1,23 +1,9 @@
-import type { ExplorerItem } from '../ExplorerItem/ExplorerItem.ts'
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
+import * as GetFittingIndex from '../GetFittingIndex/GetFittingIndex.ts'
 import * as GetNewDirentsForNewDirent from '../GetNewDirentsForNewDirent/GetNewDirentsForNewDirent.ts'
 import * as GetNewDirentType from '../GetNewDirentType/GetNewDirentType.ts'
-
-const isFolder = (direntType: number): boolean => {
-  return direntType === DirentType.Directory || direntType === DirentType.DirectoryExpanded || direntType === DirentType.SymLinkFolder
-}
-
-const getFittingIndex = (dirents: readonly ExplorerItem[], startIndex: number): number => {
-  for (let i = startIndex; i >= 0; i--) {
-    const dirent = dirents[i]
-    if (isFolder(dirent.type)) {
-      return i
-    }
-  }
-  return -1
-}
 
 export const newDirent = async (state: ExplorerState, editingType: number): Promise<ExplorerState> => {
   // TODO do it like vscode, select position between folders and files
@@ -25,7 +11,7 @@ export const newDirent = async (state: ExplorerState, editingType: number): Prom
   if (editingIndex !== -1) {
     return state
   }
-  const index = getFittingIndex(items, focusedIndex)
+  const index = GetFittingIndex.getFittingIndex(items, focusedIndex)
   const direntType = GetNewDirentType.getNewDirentType(editingType)
   const newDirents = await GetNewDirentsForNewDirent.getNewDirentsForNewDirent(items, index, direntType, root)
   const newEditingIndex = newDirents.findIndex((item) => item.type === DirentType.EditingFile || item.type === DirentType.EditingFolder)
