bd6c76d9401f2b17089821181601f0deba2998d1
Le Vivilet <levivilet@pm.me>
2026-04-30 15:37:45 +0200

    mv

--- packages/explorer-view/src/parts/RevealItem/RevealItem.ts
+++ packages/explorer-view/src/parts/RevealItem/RevealItem.ts
@@ -1,22 +1,15 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as GetIndex from '../GetIndex/GetIndex.ts'
+import * as IsUriWithinRoot from '../IsUriWithinRoot/IsUriWithinRoot.ts'
 import * as RevealItemHidden from '../RevealItemHidden/RevealItemHidden.ts'
 import * as RevealItemVisible from '../RevealItemVisible/RevealItemVisible.ts'
-
-const isUriWithinRoot = (root: string, uri: string, pathSeparator: string): boolean => {
-  if (uri === root) {
-    return true
-  }
-  const rootWithSeparator = root.endsWith(pathSeparator) ? root : `${root}${pathSeparator}`
-  return uri.startsWith(rootWithSeparator)
-}
 
 export const revealItem = async (state: ExplorerState, uri: string): Promise<ExplorerState> => {
   Assert.object(state)
   Assert.string(uri)
   const { items, pathSeparator, root } = state
-  if (!isUriWithinRoot(root, uri, pathSeparator)) {
+  if (!IsUriWithinRoot.isUriWithinRoot(root, uri, pathSeparator)) {
     return state
   }
   const index = GetIndex.getIndex(items, uri)
