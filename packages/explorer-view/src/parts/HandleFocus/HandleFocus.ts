f224a2ec282b26fc21125103be9895bcfcb143d1
Le Vivilet <levivilet@pm.me>
2026-05-03 15:44:39 +0000

    test: add tests for handleFocus function to verify focus behavior during editing

--- packages/explorer-view/src/parts/HandleFocus/HandleFocus.ts
+++ packages/explorer-view/src/parts/HandleFocus/HandleFocus.ts
@@ -2,6 +2,9 @@ import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
 
 export const handleFocus = async (state: ExplorerState): Promise<ExplorerState> => {
+  if (state.editingIndex !== -1) {
+    return state
+  }
   return {
     ...state,
     focus: FocusId.List,
