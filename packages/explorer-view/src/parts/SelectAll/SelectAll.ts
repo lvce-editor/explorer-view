12303c19b9e6fec07c577ffd2f60c49403823971
Le Vivilet <levivilet@pm.me>
2026-05-27 00:22:26 +0200

    refactor: update TODO comments for selectAll function clarity (#1649)

--- packages/explorer-view/src/parts/SelectAll/SelectAll.ts
+++ packages/explorer-view/src/parts/SelectAll/SelectAll.ts
@@ -1,5 +1,7 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
+// TODO select all should only select all items in the current folder
+// and when calling it next item, expand selection to its parent folder
 export const selectAll = (state: ExplorerState): ExplorerState => {
   const { items } = state
   const newItems = items.map((item) => ({
