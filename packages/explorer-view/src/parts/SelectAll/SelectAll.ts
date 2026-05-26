9a0dccfb269cc221cf4ff2c745938dbd6fd7915b
Le Vivilet <levivilet@pm.me>
2026-05-27 00:18:36 +0200

    fix

--- packages/explorer-view/src/parts/SelectAll/SelectAll.ts
+++ packages/explorer-view/src/parts/SelectAll/SelectAll.ts
@@ -1,6 +1,5 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 
-
 // TODO select all should only select all items in the current folder
 // and when calling it next item, expand selection to its parent folder
 export const selectAll = (state: ExplorerState): ExplorerState => {
