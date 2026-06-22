f78ed7cc60ce845f973985d4cb0d71efd2627367
Le Vivilet <levivilet@pm.me>
2026-06-23 00:30:33 +0200

    fix: explorer race condition (#1697)

--- packages/explorer-view/src/parts/Render2/Render2.ts
+++ packages/explorer-view/src/parts/Render2/Render2.ts
@@ -2,8 +2,8 @@ import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as ExplorerStates from '../ExplorerStates/ExplorerStates.ts'
 
 export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] => {
-  const { newState, oldState } = ExplorerStates.get(uid)
-  ExplorerStates.set(uid, newState, newState)
-  const commands = ApplyRender.applyRender(oldState, newState, diffResult)
+  const { oldState, scheduledState } = ExplorerStates.get(uid)
+  ExplorerStates.set(uid, scheduledState, scheduledState)
+  const commands = ApplyRender.applyRender(oldState, scheduledState, diffResult)
   return commands
 }
