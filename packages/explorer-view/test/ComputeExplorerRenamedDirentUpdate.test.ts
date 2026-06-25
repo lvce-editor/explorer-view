cd2f1eb2b8d65da0e692297db2ace7c24bdf2d9d
Le Vivilet <levivilet@pm.me>
2026-06-26 00:43:07 +0200

    Fix rename order regression (#1713)

--- packages/explorer-view/test/ComputeExplorerRenamedDirentUpdate.test.ts
+++ packages/explorer-view/test/ComputeExplorerRenamedDirentUpdate.test.ts
@@ -157,3 +157,96 @@ test('computeExplorerRenamedDirentUpdate - preserves expanded renamed folder', (
     'parent/new': tree['parent/old'],
   })
 })
+
+test('computeExplorerRenamedDirentUpdate - sorts renamed parent children and updates positions', () => {
+  const root = '/'
+  const parentPath = '/'
+  const oldUri = '/README.md'
+  const newUri = '/readme2.md'
+  const children: ExplorerItem[] = [
+    {
+      depth: 1,
+      icon: '',
+      name: 'readme2.md',
+      path: '/readme2.md',
+      posInSet: 1,
+      selected: false,
+      setSize: 1,
+      type: DirentType.File,
+    },
+    {
+      depth: 1,
+      icon: '',
+      name: '.nvmrc',
+      path: '/.nvmrc',
+      posInSet: 2,
+      selected: false,
+      setSize: 1,
+      type: DirentType.File,
+    },
+    {
+      depth: 1,
+      icon: '',
+      name: 'package.json',
+      path: '/package.json',
+      posInSet: 3,
+      selected: false,
+      setSize: 1,
+      type: DirentType.File,
+    },
+    {
+      depth: 1,
+      icon: '',
+      name: 'LICENSE',
+      path: '/LICENSE',
+      posInSet: 4,
+      selected: false,
+      setSize: 1,
+      type: DirentType.File,
+    },
+    {
+      depth: 1,
+      icon: '',
+      name: 'eslint.config.js',
+      path: '/eslint.config.js',
+      posInSet: 5,
+      selected: false,
+      setSize: 1,
+      type: DirentType.File,
+    },
+  ]
+  const tree: Tree = {}
+
+  const result = computeExplorerRenamedDirentUpdate(root, parentPath, oldUri, children, tree, newUri)
+
+  expect(result).toEqual({
+    '': [
+      {
+        ...children[1],
+        posInSet: 1,
+        setSize: 5,
+      },
+      {
+        ...children[3],
+        posInSet: 2,
+        setSize: 5,
+      },
+      {
+        ...children[4],
+        posInSet: 3,
+        setSize: 5,
+      },
+      {
+        ...children[2],
+        posInSet: 4,
+        setSize: 5,
+      },
+      {
+        ...children[0],
+        posInSet: 5,
+        setSize: 5,
+      },
+    ],
+    'readme2.md': [],
+  })
+})
