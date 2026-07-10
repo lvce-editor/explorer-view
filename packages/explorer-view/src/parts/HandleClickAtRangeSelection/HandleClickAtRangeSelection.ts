823cc185010fbe78fbba609edf29f44619535a2f
Le Vivilet <levivilet@pm.me>
2026-07-10 21:40:44 +0000

    fix: simplify range selection anchor

--- packages/explorer-view/src/parts/HandleClickAtRangeSelection/HandleClickAtRangeSelection.ts
+++ packages/explorer-view/src/parts/HandleClickAtRangeSelection/HandleClickAtRangeSelection.ts
@@ -4,7 +4,10 @@ import * as HandleRangeSelection from '../HandleRangeSelection/HandleRangeSelect
 export const handleClickAtRangeSelection = async (state: ExplorerState, index: number): Promise<ExplorerState> => {
   const { focusedIndex, items } = state
   const firstSelectedIndex = items.findIndex((item) => item.selected)
-  const anchorIndex = firstSelectedIndex === -1 ? (focusedIndex === -1 ? index : focusedIndex) : firstSelectedIndex
+  let anchorIndex = firstSelectedIndex
+  if (anchorIndex === -1) {
+    anchorIndex = focusedIndex === -1 ? index : focusedIndex
+  }
   const min = Math.min(anchorIndex, index)
   const max = Math.max(anchorIndex, index)
   const newState = HandleRangeSelection.handleRangeSelection(state, min, max)
