a9409df8e0d5aa120417b68ff3e2426d98909f81
Le Vivilet <levivilet@pm.me>
2026-07-13 09:33:15 +0000

    feature: restore explorer state per workspace

--- packages/explorer-view/src/parts/HandleWorkspaceChange/HandleWorkspaceChange.ts
+++ packages/explorer-view/src/parts/HandleWorkspaceChange/HandleWorkspaceChange.ts
@@ -2,9 +2,9 @@ import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import * as GetWorkspacePath from '../GetWorkspacePath/GetWorkspacePath.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 
-export const handleWorkspaceChange = async (state: ExplorerState): Promise<ExplorerState> => {
+export const handleWorkspaceChange = async (state: ExplorerState, _workspacePath?: string, savedState?: unknown): Promise<ExplorerState> => {
   const newRoot = await GetWorkspacePath.getWorkspacePath()
   const state1 = { ...state, root: newRoot }
-  const newState = await LoadContent.loadContent(state1, undefined)
+  const newState = await LoadContent.loadContent(state1, savedState)
   return newState
 }
