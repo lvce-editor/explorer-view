0f9a697e5c4ef8e178c53fd78df58b280f584270
Le Vivilet <levivilet@pm.me>
2026-07-16 08:42:22 +0200

    feature: add explorer accessibility e2e tests (#1740)

--- packages/e2e/src/viewlet.explorer-accessibility-create-updates-aria.ts
+++ packages/e2e/src/viewlet.explorer-accessibility-create-updates-aria.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-accessibility-create-updates-aria'
 
-export const skip = 1
-
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -22,9 +20,13 @@ export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Worksp
   const folder = Locator('.TreeItem[aria-label="folder"]')
   const a = Locator('.TreeItem[aria-label="a.txt"]')
   const b = Locator('.TreeItem[aria-label="b.txt"]')
+  await expect(folder).toHaveAttribute('aria-expanded', 'true')
+  await expect(folder).toHaveAttribute('aria-posinset', '1')
   await expect(folder).toHaveAttribute('aria-setsize', '1')
+  await expect(a).toHaveAttribute('aria-level', '2')
   await expect(a).toHaveAttribute('aria-posinset', '1')
   await expect(a).toHaveAttribute('aria-setsize', '2')
+  await expect(b).toHaveAttribute('aria-level', '2')
   await expect(b).toHaveAttribute('aria-posinset', '2')
   await expect(b).toHaveAttribute('aria-setsize', '2')
 }
--- packages/e2e/src/viewlet.explorer-accessibility-refresh-updates-aria.ts
+++ packages/e2e/src/viewlet.explorer-accessibility-refresh-updates-aria.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-accessibility-refresh-updates-aria'
 
-export const skip = 1
-
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -17,10 +15,14 @@ export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Worksp
   await Explorer.refresh()
 
   // assert
+  const folder = Locator('.TreeItem[aria-label="folder"]')
   const a = Locator('.TreeItem[aria-label="a.txt"]')
   const b = Locator('.TreeItem[aria-label="b.txt"]')
+  await expect(folder).toHaveAttribute('aria-expanded', 'true')
+  await expect(a).toHaveAttribute('aria-level', '2')
   await expect(a).toHaveAttribute('aria-posinset', '1')
   await expect(a).toHaveAttribute('aria-setsize', '2')
+  await expect(b).toHaveAttribute('aria-level', '2')
   await expect(b).toHaveAttribute('aria-posinset', '2')
   await expect(b).toHaveAttribute('aria-setsize', '2')
 }
