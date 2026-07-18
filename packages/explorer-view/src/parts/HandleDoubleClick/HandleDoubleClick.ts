ca3274986e623669729ea0ec24e3853bcac21878
Le Vivilet <levivilet@pm.me>
2026-07-18 13:37:57 +0200

    fix: open explorer files permanently on double click (#1756)

--- packages/explorer-view/src/parts/HandleDoubleClick/HandleDoubleClick.ts
+++ packages/explorer-view/src/parts/HandleDoubleClick/HandleDoubleClick.ts
@@ -1,11 +1,19 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import * as DirentType from '../DirentType/DirentType.ts'
 import { getIndexFromPosition } from '../GetIndexFromPosition/GetIndexFromPosition.ts'
 import { newFile } from '../NewFile/NewFile.ts'
+import { normalizeDirentType } from '../NormalizeDirentType/NormalizeDirentType.ts'
+import * as OpenUri from '../OpenUri/OpenUri.ts'
 
 export const handleDoubleClick = async (state: ExplorerState, eventX: number, eventY: number): Promise<ExplorerState> => {
   const index = getIndexFromPosition(state, eventX, eventY)
-  if (index !== -1) {
-    return state
+  if (index === -1) {
+    return newFile(state)
   }
-  return newFile(state)
+  const item = state.items[index]
+  const type = normalizeDirentType(item.type)
+  if (type === DirentType.File || type === DirentType.SymLinkFile) {
+    await OpenUri.openUri(item.path, true)
+  }
+  return state
 }
