c5d8f10cc5e39e1a9b6d7110295970fd2e574cc3
Le Vivilet <levivilet@pm.me>
2026-07-19 14:47:52 +0000

    refactor: destructure explorer input source

--- packages/explorer-view/src/parts/CancelEdit/CancelEdit.ts
+++ packages/explorer-view/src/parts/CancelEdit/CancelEdit.ts
@@ -3,9 +3,10 @@ import { cancelEditInternal } from '../CancelEditInternal/CancelEditInternal.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 export const cancelEdit = async (state: ExplorerState): Promise<ExplorerState> => {
+  const { inputSource } = state
   const newState = await cancelEditInternal(state, true)
   return {
     ...newState,
-    inputSource: state.inputSource === InputSource.User ? InputSource.Script : state.inputSource,
+    inputSource: inputSource === InputSource.User ? InputSource.Script : inputSource,
   }
 }
