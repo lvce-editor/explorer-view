d71ffc753e0295dd7799be4b0aa20b00baf029aa
Le Vivilet <levivilet@pm.me>
2026-07-19 14:43:13 +0000

    fix: restore explorer focus after edit commands

--- packages/explorer-view/src/parts/CancelEdit/CancelEdit.ts
+++ packages/explorer-view/src/parts/CancelEdit/CancelEdit.ts
@@ -1,6 +1,11 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
 import { cancelEditInternal } from '../CancelEditInternal/CancelEditInternal.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const cancelEdit = async (state: ExplorerState): Promise<ExplorerState> => {
-  return cancelEditInternal(state, true)
+  const newState = await cancelEditInternal(state, true)
+  return {
+    ...newState,
+    inputSource: state.inputSource === InputSource.User ? InputSource.Script : state.inputSource,
+  }
 }
