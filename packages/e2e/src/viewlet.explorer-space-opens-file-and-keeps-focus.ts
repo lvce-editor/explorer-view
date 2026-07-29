ecea3a59cd418ce6e49739fd9a94d208499085f5
Le Vivilet <levivilet@pm.me>
2026-07-29 12:24:53 +0000

    fix: focus explorer in space e2e test

--- packages/e2e/src/viewlet.explorer-space-opens-file-and-keeps-focus.ts
+++ packages/e2e/src/viewlet.explorer-space-opens-file-and-keeps-focus.ts
@@ -2,12 +2,15 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-space-opens-file-and-keeps-focus'
 
-export const test: Test = async ({ expect, Explorer, FileSystem, KeyBoard, Locator, Workspace }) => {
+export const test: Test = async ({ Command, expect, Explorer, FileSystem, KeyBoard, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file.txt`, 'content')
   await Workspace.setPath(tmpDir)
+  await Command.execute('Explorer.focus')
   await Explorer.focusIndex(0)
+  const explorerItems = Locator('.Explorer .ListItems')
+  await expect(explorerItems).toBeFocused()
 
   // act
   await KeyBoard.press('Space')
@@ -16,6 +19,5 @@ export const test: Test = async ({ expect, Explorer, FileSystem, KeyBoard, Locat
   const editorTab = Locator('.MainTab[title$="file.txt"]')
   await expect(editorTab).toBeVisible()
   await expect(editorTab).toHaveClass('MainTabPreview')
-  const explorerItems = Locator('.Explorer .ListItems')
   await expect(explorerItems).toBeFocused()
 }
