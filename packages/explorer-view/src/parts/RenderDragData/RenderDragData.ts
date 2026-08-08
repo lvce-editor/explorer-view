649ca7d770cdef8214a66de326efaae5fd1f1062
Le Vivilet <levivilet@pm.me>
2026-08-08 05:55:50 +0000

    fix: keep explorer drag data compact

--- packages/explorer-view/src/parts/RenderDragData/RenderDragData.ts
+++ packages/explorer-view/src/parts/RenderDragData/RenderDragData.ts
@@ -1,11 +1,21 @@
 import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
-import { getCurrentDragData } from '../GetCurrentDragData/GetCurrentDragData.ts'
+import { getDragData } from '../GetDragData/GetDragData.ts'
+import { getSelectedItems } from '../GetSelectedItems/GetSelectedItems.ts'
 
 export const renderDragData = (oldState: ExplorerState, newState: ExplorerState): readonly any[] => {
-  const { isPointerDown, uid } = newState
+  const { focusedIndex, isPointerDown, items, pointerDownIndex, uid } = newState
   if (!isPointerDown) {
     return []
   }
-  const dragData = getCurrentDragData(newState)
+  const pointerDownItem = items[pointerDownIndex]
+  let draggedItems
+  if (pointerDownIndex === focusedIndex || pointerDownItem?.selected) {
+    draggedItems = getSelectedItems(items, focusedIndex)
+  } else if (pointerDownItem) {
+    draggedItems = [pointerDownItem]
+  } else {
+    draggedItems = []
+  }
+  const dragData = getDragData(draggedItems.map((item) => item.path))
   return ['Viewlet.setDragData', uid, dragData]
 }
