78ce679f96fd5b3f4b7840826f4ce77b1f60852d
Le Vivilet <levivilet@pm.me>
2026-08-18 04:54:45 -0700

    fix: schedule explorer drag data rendering (#1818)

--- packages/explorer-view/src/parts/ExplorerStates/ExplorerStates.ts
+++ packages/explorer-view/src/parts/ExplorerStates/ExplorerStates.ts
@@ -77,6 +77,10 @@ const hasSameVisibleExplorerItemInputs = (oldState: ExplorerState, newState: Exp
   )
 }
 
+const hasSameDragDataInputs = (oldState: ExplorerState, newState: ExplorerState): boolean => {
+  return oldState.isPointerDown === newState.isPointerDown && oldState.pointerDownIndex === newState.pointerDownIndex
+}
+
 export const updateGitIgnoredUris = (state: ExplorerState, generation: number, sourceControlIgnoredUris: readonly string[]): ExplorerState => {
   const { gitIgnoreGeneration } = state
   if (gitIgnoreGeneration !== generation) {
@@ -174,7 +178,7 @@ const wrapListItemCommandInternal = <T extends any[]>(fn: Fn<T>, queued: boolean
     const intermediate = get(id)
     set(id, intermediate.oldState, updatedState, intermediate.scheduledState)
     if (hasSameVisibleExplorerItemInputs(intermediate.newState, updatedState)) {
-      if (updatedState.inputSource === InputSource.User) {
+      if (updatedState.inputSource === InputSource.User || !hasSameDragDataInputs(intermediate.newState, updatedState)) {
         set(id, intermediate.oldState, updatedState)
       }
       return {
