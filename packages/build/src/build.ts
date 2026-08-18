503ce720614e3cb312df2536f26028ca12f92d73
Le Vivilet <levivilet@pm.me>
2026-08-18 09:26:48 +0000

    feature: publish explorer settings contribution

--- packages/build/src/build.ts
+++ packages/build/src/build.ts
@@ -72,3 +72,4 @@ await writeJson(join(dist, 'package.json'), packageJson)
 
 await cp(join(root, 'README.md'), join(dist, 'README.md'))
 await cp(join(root, 'LICENSE'), join(dist, 'LICENSE'))
+await cp(join(root, 'packages', 'explorer-view', 'settings.json'), join(dist, 'dist', 'settings.json'))
