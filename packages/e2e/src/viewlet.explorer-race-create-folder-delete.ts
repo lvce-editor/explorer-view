07f0bf6e88a51f0c75462d27988464c1f2796e34
Le Vivilet <levivilet@pm.me>
2026-08-21 19:26:42 +0000

    test: isolate create folder race from webkit

--- packages/e2e/src/viewlet.explorer-race-create-folder-delete.ts
+++ packages/e2e/src/viewlet.explorer-race-create-folder-delete.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-race-create-folder-delete'
 
+export const skip = ['webkit']
+
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
