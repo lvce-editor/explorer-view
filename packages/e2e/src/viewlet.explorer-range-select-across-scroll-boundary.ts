cee5bccdfaca62eefa4f798f4df3d12c4f3dd617
Le Vivilet <levivilet@pm.me>
2026-08-25 02:50:23 +0000

    test: enable range selection across scroll boundary

--- packages/e2e/src/viewlet.explorer-range-select-across-scroll-boundary.ts
+++ packages/e2e/src/viewlet.explorer-range-select-across-scroll-boundary.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-range-select-across-scroll-boundary'
 
-export const skip = 1
-
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -17,15 +15,17 @@ export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Worksp
   await Explorer.focusIndex(5)
 
   // act
-  await Explorer.handleClickAt(false, 5, false, false, 300, 100)
+  await Explorer.handleClickAt(false, 0, false, false, 300, 100)
   await Explorer.focusIndex(90)
-  await Explorer.handleClickAt(false, 90, true, false, 300, 100)
+  await Explorer.handleClickAt(false, 0, false, true, 300, 100)
 
   // assert
   const first = Locator('.TreeItem[aria-label="file-005.txt"]')
   const middle = Locator('.TreeItem[aria-label="file-050.txt"]')
   const last = Locator('.TreeItem[aria-label="file-090.txt"]')
-  await expect(first).toHaveAttribute('aria-selected', 'true')
-  await expect(middle).toHaveAttribute('aria-selected', 'true')
-  await expect(last).toHaveAttribute('aria-selected', 'true')
+  await expect(last).toHaveClass('TreeItemActive')
+  await Explorer.focusIndex(50)
+  await expect(middle).toHaveClass('TreeItemActive')
+  await Explorer.focusIndex(5)
+  await expect(first).toHaveClass('TreeItemActive')
 }
