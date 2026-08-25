ea239c4afcb2b9585ed487f522f338aeac791321
Le Vivilet <levivilet@pm.me>
2026-08-25 03:41:59 +0000

    test: enable drop on file row e2e test

--- packages/e2e/src/viewlet.explorer-drop-on-file-row.ts
+++ packages/e2e/src/viewlet.explorer-drop-on-file-row.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.explorer-drop-on-file-row'
 
-export const skip = 1
+export const skip = ['webkit']
 
 export const test: Test = async ({ expect, Explorer, FileSystem, Locator, Workspace }) => {
   // arrange
