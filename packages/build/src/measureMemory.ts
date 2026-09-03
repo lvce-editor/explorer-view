4a9f3b51d94531e986ad9b2432d02ef8d0d03d6b
Le Vivilet <levivilet@pm.me>
2026-09-03 16:16:32 +0000

    fix: update explorer memory threshold

--- packages/build/src/measureMemory.ts
+++ packages/build/src/measureMemory.ts
@@ -2,7 +2,7 @@ import { measureMemory } from '@lvce-editor/measure-memory'
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-const threshold = 642_000
+const threshold = 643_000
 
 const instantiations = 200_000
 
