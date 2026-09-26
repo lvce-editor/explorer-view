82561833b49688e0c66098719f70be6810ec9dac
Le Vivilet <levivilet@pm.me>
2026-09-26 07:57:58 -0700

    test: cover context menu cut for multiple selected files (#1903)

---
+++ packages/e2e/src/viewlet.explorer-context-menu-cut-multiple-files.ts
@@ -0,0 +1,35 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.explorer-context-menu-cut-multiple-files'
+
+export const skip = ['webkit']
+
+export const test: Test = async ({ ClipBoard, ContextMenu, expect, Explorer, FileSystem, KeyBoard, Locator, Workspace }) => {
+  // arrange
+  await ClipBoard.enableMemoryClipBoard()
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.setFiles([
+    { content: 'content 1', uri: `${tmpDir}/file1.txt` },
+    { content: 'content 2', uri: `${tmpDir}/file2.txt` },
+    { content: 'content 3', uri: `${tmpDir}/file3.txt` },
+    { content: 'content 4', uri: `${tmpDir}/file4.txt` },
+  ])
+  await Workspace.setPath(tmpDir)
+  await Explorer.selectIndices([0, 1, 2])
+
+  // act
+  await Explorer.openContextMenu(1)
+  await ContextMenu.selectItem('Cut')
+
+  // assert
+  const cutLabels = Locator('.TreeItem .Label.LabelCut')
+  const unselectedLabel = Locator('.TreeItem[data-index="3"] .Label')
+  await expect(cutLabels).toHaveCount(3)
+  await expect(unselectedLabel).toHaveClass('Label')
+
+  // act - cancel the cut operation
+  await KeyBoard.press('Escape')
+
+  // assert
+  await expect(cutLabels).toHaveCount(0)
+}
